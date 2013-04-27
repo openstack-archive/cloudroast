c37855ab96ca06252f3e59f3139bab025ef9227d
Daryl Walleck <daryl.walleck@rackspace.com>
2013-04-27 01:26:32 -0500

    Expanded positive/negative coverage for delete server
    * Added pos/neg tests for deleted servers
    * Added proper tags for tests
    
    Change-Id: I2c49b0239ec1dc1481ef934cce90601868463d92

--- test_repo/compute/functional/servers/test_delete_server.py
+++ test_repo/compute/functional/servers/test_delete_server.py
@@ -28,10 +28,81 @@ class ServersTest(ComputeFixture):
         cls.resp = cls.servers_client.delete_server(cls.server.id)
         cls.server_behaviors.wait_for_server_to_be_deleted(cls.server.id)
 
+    @tags(type='smoke', net='no')
     def test_delete_server_response(self):
+        """The response code for a delete server request should be 204."""
         self.assertEqual(204, self.resp.status_code)
 
+    @tags(type='smoke', net='no')
+    def test_deleted_server_not_listed(self):
+        """A deleted server should not be included in the list of servers."""
+        servers = self.servers_client.list_servers().entity
+        self.assertNotIn(self.server, servers)
+
+        servers = self.servers_client.list_servers_with_detail().entity
+        self.assertNotIn(self.server, servers)
+
+    @tags(type='positive', net='no')
+    def test_deleted_server_listed_with_changes_since(self):
+        """
+        A deleted server should be included in the list of servers if
+        the changes-since parameter is a time before the server was deleted
+        """
+
+        servers = self.servers_client.list_servers(
+            changes_since=self.server.created).entity
+        self.assertTrue(
+            any([s for s in servers if s.id == self.server.id]))
+
+        servers = self.servers_client.list_servers_with_detail(
+            changes_since=self.server.created).entity
+        self.assertTrue(
+            any([s for s in servers if s.id == self.server.id]))
+
+    @tags(type='negative', net='no')
     def test_get_deleted_server_fails(self):
+        """A get server request for a deleted server should fail."""
         with self.assertRaises(ItemNotFound):
             self.servers_client.get_server(self.server.id)
 
+    @tags(type='negative', net='no')
+    def test_update_deleted_server_fails(self):
+        """An update server request for a deleted server should fail."""
+        with self.assertRaises(ItemNotFound):
+            self.servers_client.update_server(self.server.id, name='newname')
+
+    @tags(type='negative', net='no')
+    def test_delete_deleted_server_fails(self):
+        """A delete server request for a deleted server should fail."""
+        with self.assertRaises(ItemNotFound):
+            self.servers_client.delete_server(self.server.id)
+
+    @tags(type='negative', net='no')
+    def test_change_password_for_deleted_server_fails(self):
+        """A change password request for a deleted server should fail."""
+        with self.assertRaises(ItemNotFound):
+            self.servers_client.change_password(self.server.id, 'newP@ssw0rd')
+
+    @tags(type='negative', net='no')
+    def test_reboot_deleted_server_fails(self):
+        """A reboot request for a deleted server should fail."""
+        with self.assertRaises(ItemNotFound):
+            self.servers_client.reboot(self.server.id, 'HARD')
+
+    @tags(type='negative', net='no')
+    def test_rebuild_deleted_server_fails(self):
+        """A rebuild request for a deleted server should fail."""
+        with self.assertRaises(ItemNotFound):
+            self.servers_client.rebuild(self.server.id, self.image_ref_alt)
+
+    @tags(type='negative', net='no')
+    def test_resize_deleted_server_fails(self):
+        """A resize request for a deleted server should fail."""
+        with self.assertRaises(ItemNotFound):
+            self.servers_client.resize(self.server.id, self.flavor_ref_alt)
+
+    @tags(type='negative', net='no')
+    def test_create_image_for_deleted_server_fails(self):
+        """A create image request for a deleted server should fail."""
+        with self.assertRaises(ItemNotFound):
+            self.servers_client.create_image(self.server.id, 'backup')
