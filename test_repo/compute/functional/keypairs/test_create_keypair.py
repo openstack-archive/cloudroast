dc89b570cf269f39319ad8a48b715a3af4f76896
Daryl Walleck <daryl.walleck@rackspace.com>
2013-06-11 13:52:55 -0500

    Adds first basic test for testing of keypair auth
    * Adds test to create keypairs test that use keys for auth
    
    Change-Id: Ia4659c6ab640659348ce85e32310c63dfcf953c2

--- test_repo/compute/functional/keypairs/test_create_keypair.py
+++ test_repo/compute/functional/keypairs/test_create_keypair.py
@@ -17,6 +17,7 @@ limitations under the License.
 from cafe.drivers.unittest.decorators import tags
 from cloudcafe.compute.common.datagen import rand_name
 from cloudcafe.compute.common.exceptions import ActionInProgress
+from cloudcafe.compute.common.types import InstanceAuthStrategies
 from test_repo.compute.fixtures import ComputeFixture
 
 
@@ -27,24 +28,32 @@ class CreateKeypairTest(ComputeFixture):
         super(CreateKeypairTest, cls).setUpClass()
         cls.name = rand_name("key")
         cls.create_resp = cls.keypairs_client.create_keypair(cls.name)
-        cls.keypair = cls.create_resp.entity
-        cls.resources.add(cls.keypair.name,
+        cls.keypair = cls.keypairs_client.get_keypair(cls.name).entity
+        cls.resources.add(cls.name,
                           cls.keypairs_client.delete_keypair)
 
     @tags(type='positive', net='no')
     def test_create_keypair_response(self):
         """Verify the response code and contents are correct"""
 
+        # Get the keypair from the original response
+        keypair = self.create_resp.entity
+
         self.assertEqual(self.create_resp.status_code, 200)
-        self.assertEqual(self.keypair.name, self.name)
-        self.assertIsNotNone(self.keypair.public_key)
-        self.assertIsNotNone(self.keypair.fingerprint)
+        self.assertEqual(keypair.name, self.name)
+        self.assertIsNotNone(keypair.public_key)
+        self.assertIsNotNone(keypair.fingerprint)
 
     @tags(type='positive', net='no')
     def test_created_keypair_listed(self):
         """Verify the new key appears in list of keypairs"""
+
         keypairs_list = self.keypairs_client.list_keypairs().entity
-        self.assertIn(self.keypair,keypairs_list)
+
+        # Format of keypairs in list differs from the normal keypair model
+        # Just check for a keypair by name
+        self.assertTrue(any([key for key in keypairs_list
+                             if key.name == self.keypair.name]))
 
     @tags(type='negative', net='no')
     def test_cannot_create_duplicate_keypair(self):
@@ -59,9 +68,13 @@ class CreateKeypairTest(ComputeFixture):
             key_name=self.name).entity
         self.resources.add(server.id, self.servers_client.delete_server)
 
+        # Get the keypair from the original response
+        keypair = self.create_resp.entity
+
         # Verify the authorized_keys file was generated
         remote_client = self.server_behaviors.get_remote_instance_client(
-            server, self.servers_config)
+            server, self.servers_config, key=keypair.private_key,
+            auth_strategy=InstanceAuthStrategies.KEY)
         self.assertTrue(remote_client.is_file_present(
             '~/.ssh/authorized_keys'))
 
