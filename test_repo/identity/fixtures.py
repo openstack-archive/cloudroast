36325009dace9bba0d60436431435ef90d75be1f
Charles Kimpolo <clmkimpo@thoughtworks.com>
2013-06-13 18:28:50 +0200

    Tenants API Role Tests Parity
    1. Added an admin package to Identity for tests parity.
    2. Added a role test to admin package.
    3. Refactored fixtures as per role test parity requirements.
    4. Completed testing role creation functionality.
    5. Completed testing role deletion functionality.
    6. Added assign role to a tenant user test.
    7. Completed testing role functionality on tenants-api client.
    
    Change-Id: I5ceb4563373c0864703b0053e3673c865f32b0e5

--- test_repo/identity/fixtures.py
+++ test_repo/identity/fixtures.py
@@ -13,13 +13,14 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import json
 
 from cafe.drivers.unittest.fixtures import BaseTestFixture
-from cloudcafe.identity.config import IdentityTokenConfig
+from cloudcafe.auth.config import UserConfig
+from cloudcafe.identity.config import \
+    IdentityTokenConfig, IdentityExtensionConfig, IdentityTenantConfig
+from cloudcafe.identity.v2_0.extensions_api.client import ExtensionsAPI_Client
 from cloudcafe.identity.v2_0.tenants_api.client import TenantsAPI_Client
 from cloudcafe.identity.v2_0.tokens_api.client import TokenAPI_Client
-from cloudcafe.identity.v2_0.tokens_api.models.responses.access import Access
 
 
 class BaseIdentityAdminTest(BaseTestFixture):
@@ -27,6 +28,9 @@ class BaseIdentityAdminTest(BaseTestFixture):
     def setUpClass(cls):
         super(BaseIdentityAdminTest, cls).setUpClass()
         cls.token_config = IdentityTokenConfig()
+        cls.user_config = UserConfig()
+        cls.extension_config = IdentityExtensionConfig()
+        cls.tenant_config = IdentityTenantConfig()
         cls.endpoint_url = cls.token_config.authentication_endpoint
         cls.serialize_format = cls.token_config.serialize_format
         cls.deserialize_format = cls.token_config.deserialize_format
@@ -36,21 +40,62 @@ class BaseIdentityAdminTest(BaseTestFixture):
             deserialize_format=cls.deserialize_format,
             serialize_format=cls.serialize_format)
 
-        cls.auth_response = cls.token_client.authenticate(
+        cls.admin_auth_response = cls.token_client.authenticate(
             username=cls.token_config.username,
             password=cls.token_config.password,
             tenant_name=cls.token_config.tenant_name)
+        cls.access_data = cls.admin_auth_response.entity
+        cls.admin_token = cls.access_data.token
+        cls.admin = cls.access_data.user
 
-        cls.access_dict = json.loads(cls.auth_response.content).get('access')
-        cls.access_data = Access._dict_to_obj(cls.access_dict)
-        cls.token = cls.access_data.token
-        cls.user = cls.access_data.user
+        cls.demo_auth_response = cls.token_client.authenticate(
+            username=cls.user_config.username,
+            password=cls.user_config.password,
+            tenant_name=cls.user_config.tenant_name)
+        cls.demo_access_data = cls.demo_auth_response.entity
+        cls.demo_token = cls.demo_access_data.token
+        cls.demo_user = cls.demo_access_data.user
 
         cls.tenant_client = TenantsAPI_Client(
             url=cls.endpoint_url,
-            auth_token=cls.token.id_,
+            auth_token=cls.admin_token.id_,
             deserialize_format=cls.deserialize_format,
-            serialize_format=cls.serialize_format)
+            serialize_format=cls.serialize_format,
+            tenant_admin=cls.tenant_config.tenants_api_admin)
+
+        cls.demo_tenant_client = TenantsAPI_Client(
+            url=cls.endpoint_url,
+            auth_token=cls.demo_token.id_,
+            deserialize_format=cls.deserialize_format,
+            serialize_format=cls.serialize_format,
+            tenant_admin=cls.tenant_config.tenants_api_admin)
+
+        cls.missing_token_tenant_client = TenantsAPI_Client(
+            url=cls.endpoint_url,
+            auth_token=None,
+            deserialize_format=cls.deserialize_format,
+            serialize_format=cls.serialize_format,
+            tenant_admin=cls.tenant_config.tenants_api_admin)
+
+        cls.extension_client = ExtensionsAPI_Client(
+            url=cls.endpoint_url,
+            auth_token=cls.admin_token.id_,
+            serialized_format=cls.deserialize_format,
+            deserialized_format=cls.deserialize_format,
+            extensions_admin=cls.extension_config.extensions_api_admin)
+
+        cls.demo_extension_client = ExtensionsAPI_Client(
+            url=cls.endpoint_url,
+            auth_token=cls.demo_token.id_,
+            serialized_format=cls.deserialize_format,
+            deserialized_format=cls.deserialize_format,
+            extensions_admin=cls.extension_config.extensions_api_admin)
+
+        cls.missing_token_client = ExtensionsAPI_Client(
+            url=cls.endpoint_url,
+            serialized_format=cls.deserialize_format,
+            deserialized_format=cls.deserialize_format,
+            extensions_admin=cls.extension_config.extensions_api_admin)
 
     @classmethod
     def tearDownClass(cls):
