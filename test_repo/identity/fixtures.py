53e5b838b99370cde9cb425f8b366163fbca5673
Charles Kimpolo <clmkimpo@thoughtworks.com>
2013-06-21 11:39:54 +0200

    Identity Tests Parity Fixtures
    1. Refactored identity-api fixtures
    2. Provided a better access of tests data.
    
    Change-Id: Ibe366f7b501a1463bb353953c48dea3e2baf03da

--- test_repo/identity/fixtures.py
+++ test_repo/identity/fixtures.py
@@ -13,41 +13,42 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import json
 
 from cafe.drivers.unittest.fixtures import BaseTestFixture
-from cloudcafe.auth.config import UserConfig
-from cloudcafe.auth.provider import AuthProvider
-from cloudcafe.identity.config import IdentityEndpointConfig, \
-    IdentityTokenConfig
+from cloudcafe.identity.config import IdentityTokenConfig
 from cloudcafe.identity.v2_0.tenants_api.client import TenantsAPI_Client
 from cloudcafe.identity.v2_0.tokens_api.client import TokenAPI_Client
+from cloudcafe.identity.v2_0.tokens_api.models.responses.access import Access
 
 
 class BaseIdentityAdminTest(BaseTestFixture):
     @classmethod
     def setUpClass(cls):
-        cls.identity_config = IdentityEndpointConfig()
+        super(BaseIdentityAdminTest, cls).setUpClass()
         cls.token_config = IdentityTokenConfig()
-        cls.user_config = UserConfig()
-        cls.auth = AuthProvider()
-
-        cls.tenant_id = cls.user_config.tenant_id
+        cls.endpoint_url = cls.token_config.authentication_endpoint
         cls.serialize_format = cls.token_config.serialize_format
         cls.deserialize_format = cls.token_config.deserialize_format
 
-        cls.access_data = cls.auth.get_access_data()
-        cls.auth_token = cls.access_data.token
-        cls.endpoint_url = cls.identity_config.endpoint_url
-
         cls.token_client = TokenAPI_Client(
             url=cls.endpoint_url,
-            auth_token=cls.auth_token,
             deserialize_format=cls.deserialize_format,
             serialize_format=cls.serialize_format)
 
+        cls.auth_response = cls.token_client.authenticate(
+            username=cls.token_config.username,
+            password=cls.token_config.password,
+            tenant_name=cls.token_config.tenant_name)
+
+        cls.access_dict = json.loads(cls.auth_response.content).get('access')
+        cls.access_data = Access._dict_to_obj(cls.access_dict)
+        cls.token = cls.access_data.token
+        cls.user = cls.access_data.user
+
         cls.tenant_client = TenantsAPI_Client(
             url=cls.endpoint_url,
-            auth_token=cls.auth_token,
+            auth_token=cls.token.id_,
             deserialize_format=cls.deserialize_format,
             serialize_format=cls.serialize_format)
 
