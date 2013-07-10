b71220a4424ee5ba84983da01cdd979a7ad760f8
Daryl Walleck <daryl.walleck@RACKSPACE.COM>
2013-07-09 21:09:11 -0500

    Resolves pep8/pyflakes issues in compute extensions tests
    Change-Id: I6cbb475d692ed8483fbfcfe0feeaee91cd8ad1ff

--- test_repo/compute/extensions/security_groups/test_security_groups.py
+++ test_repo/compute/extensions/security_groups/test_security_groups.py
@@ -22,7 +22,7 @@ class SecurityGroupTest(ComputeFixture):
 
     @tags(type='positive', net='no')
     def test_create_delete_security_group(self):
-        resp = self.sec_groups_client.create_security_group(name='test',
-                                                            description='test group')
+        resp = self.sec_groups_client.create_security_group(
+            name='test', description='test group')
         group = resp.entity
         self.sec_groups_client.delete_security_group(group.id)
