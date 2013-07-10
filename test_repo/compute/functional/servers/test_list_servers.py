02a44fa43fa4e564912458723eb2549d6155d1ed
Daryl Walleck <daryl.walleck@RACKSPACE.COM>
2013-07-09 22:44:17 -0500

    Refactorings for the list servers tests
    * Replaced assertTrue's with assertIn/assertNotIn
    * Fixed pep8 issues
    * Removed detailed messages for assertIn/NotIn as
      these assertions provide very detailed output
    
    Change-Id: I8fc2d9b86f932cff3bb837a2bd07c63f8f0492c6

--- test_repo/compute/functional/servers/test_list_servers.py
+++ test_repo/compute/functional/servers/test_list_servers.py
@@ -14,8 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import unittest2 as unittest
-
 from cafe.drivers.unittest.decorators import tags
 from test_repo.compute.fixtures import ComputeFixture
 
@@ -64,44 +62,38 @@ class ServerListTest(ComputeFixture):
     def test_list_servers(self):
         """All servers should be returned"""
         list_servers_response = self.servers_client.list_servers()
-        list_servers = list_servers_response.entity
+        servers_list = list_servers_response.entity
         self.assertEqual(200, list_servers_response.status_code)
-        self.assertTrue(self.server.min_details() in
-                        list_servers, msg="Server with id %s was not found in the list" % self.server.id)
-        self.assertTrue(self.server_second.min_details() in
-                        list_servers, msg="Server with id %s was not found in the list" % self.server_second.id)
-        self.assertTrue(self.server_third.min_details() in
-                        list_servers, msg="Server with id %s was not found in the list" % self.server_third.id)
+        self.assertIn(self.server.min_details(), servers_list)
+        self.assertIn(self.server_second.min_details(), servers_list)
+        self.assertIn(self.server_third.min_details(), servers_list)
 
     @tags(type='smoke', net='no')
     def test_list_servers_with_detail(self):
         """Return a detailed list of all servers"""
-        list_servers_detail_response = self.servers_client.list_servers_with_detail()
-        list_servers_detail = list_servers_detail_response.entity
-        self.assertEqual(200, list_servers_detail_response.status_code)
-        servers_lists = []
+        list_response = self.servers_client.list_servers_with_detail()
+        list_servers_detail = list_response.entity
+        self.assertEqual(200, list_response.status_code)
+        servers_list = []
         for i in list_servers_detail:
-            servers_lists.append(i.id)
-        self.assertTrue(self.server.id in servers_lists,
-                        msg="Server with id %s was not found in the list" % self.server.id)
-        self.assertTrue(self.server_second.id in servers_lists,
-                        msg="Server with id %s was not found in the list" % self.server_second.id)
-        self.assertTrue(self.server_third.id in servers_lists,
-                        msg="Server with id %s was not found in the list" % self.server_third.id)
+            servers_list.append(i.id)
+        self.assertIn(self.server.id, servers_list)
+        self.assertIn(self.server_second.id, servers_list)
+        self.assertIn(self.server_third.id, servers_list)
 
     @tags(type='positive', net='no')
     def test_list_server_details_using_marker(self):
         """The list of servers should start from the provided marker"""
-        list_server_detail_response = self.servers_client.list_servers_with_detail()
-        list_server_detail = list_server_detail_response.entity
+        list_response = self.servers_client.list_servers_with_detail()
+        list_server_detail = list_response.entity
         first_server = list_server_detail[0]
 
-        # Verify the list of servers doesn't contain the server used as a marker
+        # Verify the servers doesn't contain the server used as a marker
         params = first_server.id
-        filtered_servers = self.servers_client.list_servers_with_detail(marker=params)
+        filtered_servers = self.servers_client.list_servers_with_detail(
+            marker=params)
         self.assertEqual(200, filtered_servers.status_code)
-        self.assertTrue(first_server not in filtered_servers.entity,
-                        msg="The server id used as marker found in the server list")
+        self.assertNotIn(first_server, filtered_servers.entity)
 
     @tags(type='positive', net='no')
     def test_list_servers_using_marker(self):
@@ -110,65 +102,59 @@ class ServerListTest(ComputeFixture):
         list_server_info = list_server_info_response.entity
         first_server = list_server_info[0]
 
-        # Verify the list of servers doesn't contain the server used as a marker
+        # Verify the servers doesn't contain the server used as a marker
         params = first_server.id
-        filtered_servers = self.servers_client.list_servers(marker=params)
+        filtered_servers = self.servers_client.list_servers(
+            marker=params)
         self.assertEqual(200, filtered_servers.status_code)
-        self.assertTrue(first_server not in filtered_servers.entity,
-                        msg="The server id used as marker found in the server list")
+        self.assertNotIn(first_server, filtered_servers.entity)
 
     @tags(type='positive', net='no')
     def test_list_server_with_detail_limit_results(self):
-        """Verify only the expected number of results (with full details) are returned"""
+        """Verify the expected number of servers are returned with details"""
         limit = 1
         params = limit
-        server_with_limit = self.servers_client.list_servers_with_detail(limit=params)
-        self.assertEqual(limit, len(server_with_limit.entity),
-                         msg="The number of servers returned (%s) was more than the limit (%s)" % (len(server_with_limit.entity), limit))
+        response = self.servers_client.list_servers_with_detail(limit=params)
+        servers = response.entity
+        self.assertEqual(
+            limit, len(response.entity),
+            msg="({0}) servers returned. Expected {1} servers.".format(
+                len(servers), limit))
 
     @tags(type='positive', net='no')
     def test_list_servers_filter_by_image(self):
         """Filter the list of servers by image"""
         params = self.image_ref
         list_servers_response = self.servers_client.list_servers(image=params)
-        list_servers = list_servers_response.entity
+        servers_list = list_servers_response.entity
         self.assertEqual(200, list_servers_response.status_code)
 
-        self.assertTrue(self.server.min_details() in
-                        list_servers, msg="Server with id %s was not found in the list" % self.server.image.id)
-        self.assertTrue(self.server_second.min_details() in
-                        list_servers, msg="Server with id %s was not found in the list" % self.server_second.image.id)
-        self.assertTrue(self.server_third.min_details() not in
-                        list_servers, msg="Server with id %s and image id %s was found in the list filtered by image id %s" % (self.server_third.id, self.server_third.image.id, self.image_ref))
+        self.assertIn(self.server.min_details(), servers_list)
+        self.assertIn(self.server_second.min_details(), servers_list)
+        self.assertNotIn(self.server_third.min_details(), servers_list)
 
     @tags(type='positive', net='no')
     def test_list_servers_filter_by_flavor(self):
         """Filter the list of servers by flavor"""
         params = self.flavor_ref_alt
         list_servers_response = self.servers_client.list_servers(flavor=params)
-        list_servers = list_servers_response.entity
+        servers_list = list_servers_response.entity
         self.assertEqual(200, list_servers_response.status_code)
 
-        self.assertTrue(self.server.min_details() not in list_servers,
-                        msg="Server with id %s and flavor id %s was found in the list filtered by flavor id %s" % (self.server.id, self.server.flavor.id, self.flavor_ref_alt))
-        self.assertTrue(self.server_second.min_details() not in list_servers,
-                        msg="Server with id %s and flavor id %s was found in the list filtered by flavor id %s" % (self.server_second.id, self.server_second.flavor.id, self.flavor_ref_alt))
-        self.assertTrue(self.server_third.min_details() in list_servers,
-                        msg="Server with id %s was not found in the list" % self.server_third.id)
+        self.assertNotIn(self.server.min_details(), servers_list)
+        self.assertNotIn(self.server_second.min_details(), servers_list)
+        self.assertIn(self.server_third.min_details(), servers_list)
 
     @tags(type='positive', net='no')
     def test_list_servers_filter_by_server_name(self):
         """ Filter the list of servers by name """
         params = self.server.name
         list_servers_response = self.servers_client.list_servers(name=params)
-        list_servers = list_servers_response.entity
+        servers_list = list_servers_response.entity
         self.assertEqual(200, list_servers_response.status_code)
-        self.assertTrue(self.server.min_details() in list_servers,
-                        msg="Server with id %s was not found in the list" % self.server.id)
-        self.assertTrue(self.server_second.min_details() not in list_servers,
-                        msg="Server with id %s and name %s was found in the list filtered by name %s" % (self.server.id, self.server_second.name, self.server.name))
-        self.assertTrue(self.server_third.min_details() not in list_servers,
-                        msg="Server with id %s and name %s was found in the list filtered by name %s" % (self.server_third.id, self.server_third.name, self.server.name))
+        self.assertIn(self.server.min_details(), servers_list)
+        self.assertNotIn(self.server_second.min_details(), servers_list)
+        self.assertNotIn(self.server_third.min_details(), servers_list)
 
     @tags(type='positive', net='no')
     def test_list_servers_filter_by_server_status(self):
@@ -177,12 +163,9 @@ class ServerListTest(ComputeFixture):
         list_servers_response = self.servers_client.list_servers(status=params)
         list_servers = list_servers_response.entity
         self.assertEqual(200, list_servers_response.status_code)
-        self.assertTrue(self.server.min_details() in
-                        list_servers, msg="Server with id %s was not found in the list" % self.server.id)
-        self.assertTrue(self.server_second.min_details() in
-                        list_servers, msg="Server with id %s was not found in the list" % self.server_second.id)
-        self.assertTrue(self.server_third.min_details() in
-                        list_servers, msg="Server with id %s was not found in the list" % self.server_third.id)
+        self.assertIn(self.server.min_details(), list_servers)
+        self.assertIn(self.server_second.min_details(), list_servers)
+        self.assertIn(self.server_third.min_details(), list_servers)
 
     @tags(type='positive', net='no')
     def test_list_servers_filter_by_changes_since(self):
@@ -194,92 +177,79 @@ class ServerListTest(ComputeFixture):
         servers_ids_list = []
         for i in servers.entity:
             servers_ids_list.append(i.id)
-        self.assertTrue(self.server.id not in servers_ids_list,
-                        msg="Server with id %s was found in the list" % self.server.id)
-        self.assertTrue(self.server_second.id in servers_ids_list,
-                        msg="Server with id %s was not found in the list" % self.server_second.id)
-        self.assertTrue(self.server_third.id in servers_ids_list,
-                        msg="Server with id %s was not found in the list" % self.server_third.id)
+        self.assertNotIn(self.server.id, servers_ids_list)
+        self.assertIn(self.server_second.id, servers_ids_list)
+        self.assertIn(self.server_third.id, servers_ids_list)
 
     @tags(type='positive', net='no')
     def test_list_servers_detailed_filter_by_image(self):
         """Filter the detailed list of servers by image"""
         params = self.image_ref
-        servers_response = self.servers_client.list_servers_with_detail(image=params)
-        self.assertEqual(200, servers_response.status_code)
+        list_response = self.servers_client.list_servers_with_detail(
+            image=params)
+        self.assertEqual(200, list_response.status_code)
         servers_list = []
-        for i in servers_response.entity:
+        for i in list_response.entity:
             servers_list.append(i.id)
-        self.assertTrue(self.server.id in servers_list,
-                        msg="Server with id %s was not found in the list" % self.server.id)
-        self.assertTrue(self.server_second.id in servers_list,
-                        msg="Server with id %s was not found in the list" % self.server_second.id)
-        self.assertTrue(self.server_third.id not in servers_list,
-                        msg="Server with id %s and image id %s was found in the list filtered by image id %s" % (self.server_third.id, self.server_third.image.id, self.image_ref))
+        self.assertIn(self.server.id, servers_list)
+        self.assertIn(self.server_second.id, servers_list)
+        self.assertNotIn(self.server_third.id, servers_list)
 
     @tags(type='positive', net='no')
     def test_list_servers_detailed_filter_by_flavor(self):
         """Filter the detailed list of servers by flavor"""
         params = self.flavor_ref_alt
-        filtered_servers_response = self.servers_client.list_servers_with_detail(flavor=params)
-        filtered_servers = filtered_servers_response.entity
-        self.assertEqual(200, filtered_servers_response.status_code)
+        list_response = self.servers_client.list_servers_with_detail(
+            flavor=params)
+        filtered_servers = list_response.entity
+        self.assertEqual(200, list_response.status_code)
 
-        self.assertTrue(self.server not in filtered_servers,
-                        msg="Server with id %s and flavor id %s was found in the list filtered by flavor id %s" % (self.server.id, self.server.flavor.id, self.flavor_ref_alt))
-        self.assertTrue(self.server_second not in filtered_servers,
-                        msg="Server with id %s and flavor id %s was found in the list filtered by flavor id %s" % (self.server_second.id, self.server.flavor.id, self.flavor_ref_alt))
-        self.assertTrue(self.server_third in filtered_servers,
-                        msg="Server with id %s was not found in the list" % self.server_third.id,)
+        self.assertNotIn(self.server, filtered_servers)
+        self.assertNotIn(self.server_second, filtered_servers)
+        self.assertIn(self.server_third, filtered_servers)
 
     @tags(type='positive', net='no')
     def test_list_servers_detailed_filter_by_server_name(self):
         """Filter the detailed list of servers by server name"""
         params = self.server.name
-        filtered_servers_response = self.servers_client.list_servers_with_detail(name=params)
-        filtered_servers = filtered_servers_response.entity
-        self.assertEqual(200, filtered_servers_response.status_code)
+        list_response = self.servers_client.list_servers_with_detail(
+            name=params)
+        filtered_servers = list_response.entity
+        self.assertEqual(200, list_response.status_code)
 
-        self.assertTrue(self.server in filtered_servers,
-                        msg="Server with id %s was not found in the list" % self.server.id)
-        self.assertTrue(self.server_second not in filtered_servers,
-                        msg="Server with id %s and name %s was found in the list filtered by name %s" % (self.server_second.id, self.server_second.name, self.server.name))
-        self.assertTrue(self.server_third not in filtered_servers,
-                        msg="Server with id %s and name %s was found in the list filtered by name %s" % (self.server_third.id, self.server_third.name, self.server.name))
+        self.assertIn(self.server, filtered_servers)
+        self.assertNotIn(self.server_second, filtered_servers)
+        self.assertNotIn(self.server_third, filtered_servers)
 
     @tags(type='positive', net='no')
     def test_list_servers_detailed_filter_by_server_status(self):
         """Filter the detailed list of servers by server status"""
         params = 'active'
-        filtered_servers_response = self.servers_client.list_servers_with_detail(status=params)
-        filtered_servers = filtered_servers_response.entity
-        self.assertEqual(200, filtered_servers_response.status_code)
+        list_response = self.servers_client.list_servers_with_detail(
+            status=params)
+        filtered_servers = list_response.entity
+        self.assertEqual(200, list_response.status_code)
         servers_list = []
         for i in filtered_servers:
             servers_list.append(i.id)
-        self.assertTrue(self.server.id in servers_list,
-                        msg="Server with id %s was not found in the list" % self.server.id)
-        self.assertTrue(self.server_second.id in servers_list,
-                        msg="Server with id %s was not found in the list" % self.server_second.id)
-        self.assertTrue(self.server_third.id in servers_list,
-                        msg="Server with id %s was not found in the list" % self.server_third.id)
+        self.assertIn(self.server.id, servers_list)
+        self.assertIn(self.server_second.id, servers_list)
+        self.assertIn(self.server_third.id, servers_list)
 
     @tags(type='positive', net='no')
     def test_list_servers_detailed_filter_by_changes_since(self):
-        """Create a filter for the server with the second server created date"""
+        """Filter the detailed servers list with the changes-since filter"""
         change_time = self.server_second.created
         params = change_time
 
         # Filter the detailed list of servers by changes-since
-        filtered_servers_response = self.servers_client.list_servers_with_detail(changes_since=params)
-        filtered_servers = filtered_servers_response.entity
-        self.assertEqual(200, filtered_servers_response.status_code)
+        list_response = self.servers_client.list_servers_with_detail(
+            changes_since=params)
+        filtered_servers = list_response.entity
+        self.assertEqual(200, list_response.status_code)
         servers_list = []
         for i in filtered_servers:
             servers_list.append(i.id)
-        self.assertTrue(self.server.id not in servers_list,
-                        msg="Server with id %s was found in the list" % self.server.id)
-        self.assertTrue(self.server_second.id in servers_list,
-                        msg="Server with id %s was not found in the list" % self.server_second.id)
-        self.assertTrue(self.server_third.id in servers_list,
-                        msg="Server with id %s was not found in the list" % self.server_third.id)
+        self.assertNotIn(self.server.id, servers_list)
+        self.assertIn(self.server_second.id, servers_list)
+        self.assertIn(self.server_third.id, servers_list)
