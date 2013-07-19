18d67bb0381046992b034399e4aa4d060dd98dd0
Nandhini Devi Kaliaperumal <nandhini.kaliaperumal@rackspace.com>
2013-07-19 15:41:48 -0500

    Created separate fixture for images v2 api tests and added get_schema tests
    Change-Id: Ic8db7895787cb9344796e9ff82c89a84348d1603

---
+++ cloudroast/images/v2/functional/test_get_images_schema.py
@@ -0,0 +1,54 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from cafe.drivers.unittest.decorators import tags
+from cloudroast.images.v2.fixtures import ImagesV2Fixture
+
+
+class GetImagesSchemaTest(ImagesV2Fixture):
+    """
+        Test the retrieval of schema details for images
+    """
+
+    @tags(type='smoke')
+    def test_get_image_schema(self):
+        """ Get schema that represents an image entity. """
+
+        """
+        1) Get image schema
+        2) Verify the schema displays correctly with all elements
+        """
+
+        response = self.api_client.get_image_schema()
+        image_schema = response.entity
+        self.assertEqual(200, response.status_code)
+        """@TODO: Add check to verify all expected elements are
+        returned in the image schema response"""
+
+    @tags(type='smoke')
+    def test_get_images_schema(self):
+        """ Get schema that represents an images entity. """
+
+        """
+        1) Get images schema
+        2) Verify the schema displays correctly with all elements
+        """
+
+        response = self.api_client.get_images_schema()
+        image_schema = response.entity
+        self.assertEqual(200, response.status_code)
+        """@TODO: Add check to verify all expected elements are
+        returned in the images schema response"""
