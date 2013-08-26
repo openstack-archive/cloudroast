a1dfc45dccf7d0fee7d698083065301acf1505de
Jose Idar <jose.idar@rackspace.com>
2013-08-26 18:39:42 -0500

    WORK IN PROGRESS - DO NOT MERGE - Engine Config Overhaul
    Change-Id: If32781cd4e13a06d202bc8795999ac7bdbf685df

--- setup.py
+++ setup.py
@@ -16,18 +16,43 @@ limitations under the License.
 
 import os
 import sys
-
 import cloudroast
+
 try:
     from setuptools import setup, find_packages
+    from setuptools.command.install import install as _install
 except ImportError:
     from distutils.core import setup, find_packages
+    from distutils.command.install import install as _install
+
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
 
 requires = open('pip-requires').readlines()
+
+
+#Post-install engine configuration
+def _post_install(dir):
+    print('\n'.join(["\t\t   (----) (----)--)",
+                     "\t\t    (--(----)  (----) --)",
+                     "\t\t  (----) (--(----) (----)",
+                     "\t\t  -----------------------",
+                     "\t\t  \                     /",
+                     "\t\t   \                   /",
+                     "\t\t    \_________________/",
+                     "\t\t     === CloudRoast ===",
+                     "\t\t= A CloudCAFE Test Repository ="]))
+
+
+#cmdclass hook allows setup to make post install call
+class install(_install):
+    def run(self):
+        _install.run(self)
+        self.execute(
+            _post_install, (self.install_lib,),
+            msg="Running post install tasks...")
 
 setup(
     name='cloudroast',
@@ -46,7 +71,6 @@ setup(
     install_requires=requires,
     license=open('LICENSE').read(),
     zip_safe=False,
-    #https://the-hitchhikers-guide-to-packaging.readthedocs.org/en/latest/specification.html
     classifiers=(
         'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
@@ -55,33 +79,5 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-    )
-)
-
-''' @todo: need to clean this up or do it with puppet/chef '''
-# Default Config Options
-root_dir = "{0}/.cloudcafe".format(os.path.expanduser("~"))
-config_dir = "{0}/configs".format(root_dir)
-
-# Build Default directories
-if(os.path.exists("{0}/engine.config".format(config_dir)) == False):
-    raise Exception("Core CAFE Engine configuration not found")
-else:
-    # Copy over the default configurations
-    if(os.path.exists("~install")):
-        os.remove("~install")
-        # Report
-        print('\n'.join(["\t\t   (----) (----)--)",
-                         "\t\t    (--(----)  (----) --)",
-                         "\t\t  (----) (--(----) (----)",
-                         "\t\t  -----------------------",
-                         "\t\t  \                     /",
-                         "\t\t   \                   /",
-                         "\t\t    \_________________/",
-                         "\t\t     === CloudRoast ===",
-                         "\t\t= A CloudCAFE Test Repository ="]))
-    else:
-        # State file
-        temp = open("~install", "w")
-        temp.close()
+        'Programming Language :: Python :: 2.7',),
+    cmdclass={'install': install})
