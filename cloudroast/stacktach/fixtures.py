0aaa526a316964ec023a60b2ea33dbc6f83e5d70
Priyanka Agrawal <priyanka.agrawal@RACKSPACE.COM>
2013-09-17 16:58:35 +0530

    Added the folder skeleton for stacktach tests. Added stacktach fixture file - StackTachFixture and StackTachDBFixture
    Change-Id: I97fb1a9732e8f01fc44d4c85095f9ba14ba448ab

---
+++ cloudroast/stacktach/fixtures.py
@@ -0,0 +1,63 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from cafe.drivers.unittest.fixtures import BaseTestFixture
+from cloudcafe.stacktach.config import StacktachConfig, MiscConfig
+from cloudcafe.stacktach.stacky_api.client import StackTachClient
+from cloudcafe.stacktach.stacktach_db_api.client import StackTachDBClient
+from cloudcafe.stacktach.stacktach_db_api.behaviors import StackTachDBBehavior
+
+
+class StackTachFixture(BaseTestFixture):
+    """
+    @summary: Fixture for any StackTach test.
+    """
+    @classmethod
+    def setUpClass(cls):
+        super(StackTachFixture, cls).setUpClass()
+        cls.misc_config = MiscConfig()
+        cls.stacktach_config = StacktachConfig()
+        cls.event_id = cls.stacktach_config.event_id
+        cls.days_passed = cls.stacktach_config.days_passed
+
+        cls.url = cls.stacktach_config.url
+        cls.serializer = cls.misc_config.serializer
+        cls.deserializer = cls.misc_config.deserializer
+
+        cls.stacktach_client = StackTachClient(cls.url, cls.serializer,
+                                               cls.deserializer)
+
+
+class StackTachDBFixture(BaseTestFixture):
+    """
+    @summary: Fixture for any StackTachDB test.
+    """
+    @classmethod
+    def setUpClass(cls):
+        super(StackTachDBFixture, cls).setUpClass()
+        cls.misc_config = MiscConfig()
+        cls.stacktach_config = StacktachConfig()
+        cls.event_id = cls.stacktach_config.event_id
+        cls.days_passed = cls.stacktach_config.days_passed
+
+        cls.url = cls.stacktach_config.db_url
+        cls.serializer = cls.misc_config.serializer
+        cls.deserializer = cls.misc_config.deserializer
+
+        cls.stacktach_dbclient = StackTachDBClient(cls.url, cls.serializer,
+                                                   cls.deserializer)
+        cls.stacktach_db_behavior = StackTachDBBehavior(cls.stacktach_dbclient,
+                                                        cls.stacktach_config)
