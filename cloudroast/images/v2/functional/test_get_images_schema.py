54879885f02e5017f9f5664e5140fa62bf3c995f
Charles Kimpolo <clmkimpo@thoughtworks.com>
2013-10-01 16:48:27 +0530

    GET v2.0/schemas/image(s)
    Get schema that represents an image(s) entity.
    
    Tested get image(s) schema
    Added explicit assertion messages for all assertions as per reviewer comments.
    
    Reordered the imports in the fixtures.
    Moved MSG constant in the fixtures as per reviewer comment.
    
    Change-Id: I431031b9ef7c30bfdcbe77e6445279bcfa2445af

--- cloudroast/images/v2/functional/test_get_images_schema.py
+++ cloudroast/images/v2/functional/test_get_images_schema.py
@@ -15,36 +15,37 @@ limitations under the License.
 """
 
 from cafe.drivers.unittest.decorators import tags
-from cloudroast.images.v2.fixtures import ImagesV2Fixture
+
+from cloudroast.images.v2.fixtures import ImagesV2Fixture, MSG
 
 
 class GetImagesSchemaTest(ImagesV2Fixture):
-    """
-        Test the retrieval of schema details for images
-    """
+    """ Test the retrieval of schema details for images. """
 
     @tags(type='smoke')
     def test_get_image_schema(self):
-        """ Get schema that represents an image entity. """
-
         """
-        1) Get image schema
-        2) Verify the response status code is 200
+        Get schema that represents an image entity.
+
+        1. Get image schema
+        2. Verify the response status code is 200
         """
 
         response = self.api_client.get_image_schema()
-        image_schema = response.entity
-        self.assertEqual(200, response.status_code)
+
+        self.assertEqual(response.status_code, 200,
+                         MSG.format('status_code', 200, response.status_code))
 
     @tags(type='smoke')
     def test_get_images_schema(self):
-        """ Get schema that represents an images entity. """
-
         """
-        1) Get images schema
-        2) Verify the response status code is 200
+        Get schema that represents an images entity.
+
+        1. Get images schema
+        2. Verify the response status code is 200
         """
 
         response = self.api_client.get_images_schema()
-        image_schema = response.entity
-        self.assertEqual(200, response.status_code)
+
+        self.assertEqual(response.status_code, 200,
+                         MSG.format('status_code', 200, response.status_code))
