e89e9a3f94502827ccb7d4359b61dce8642006d6
Charles Kimpolo <clmkimpo@thoughtworks.com>
2013-10-07 15:45:41 +0200

    POST v2.0/images
    Register a VM image with minimum compulsory properties - Smoke Test.
    
    Tested register image with compulsory properties.
    Reordered the import as per reviewer comment.
    Added explicit assertion messages for all assertions as per reviewer comments.
    
    Reordered the imports in 'test_post_images.py'
    Moved the constant MSG to fixtures.
    Added explicit assertion messages for all assertions as per reviewer comments.
    
    Moved the msg constant to fixtures setUpClass.
    Fixed line continuation and imports issues.
    
    Change-Id: If68e321e7bcc4b4d1b41ad1e9442306d71a10006

--- cloudroast/images/v2/fixtures.py
+++ cloudroast/images/v2/fixtures.py
@@ -15,9 +15,10 @@ limitations under the License.
 """
 
 from cafe.drivers.unittest.fixtures import BaseTestFixture
-from cloudcafe.images.config import ImagesConfig
 from cloudcafe.auth.provider import AuthProvider
+from cloudcafe.common.constants import Messages
 from cloudcafe.common.resources import ResourcePool
+from cloudcafe.images.config import ImagesConfig
 from cloudcafe.images.v2.client import ImageClient as ImagesV2Client
 
 
@@ -25,6 +26,7 @@ class ImagesV2Fixture(BaseTestFixture):
     """
     @summary: Base fixture for Images V2 API tests
     """
+
     @classmethod
     def setUpClass(cls):
         super(ImagesV2Fixture, cls).setUpClass()
@@ -39,6 +41,8 @@ class ImagesV2Fixture(BaseTestFixture):
         cls.api_client = ImagesV2Client(cls.images_endpoint,
                                         access_data.token.id_,
                                         'json', 'json')
+
+        cls.msg = Messages.ASSERT_MSG
 
     @classmethod
     def tearDownClass(cls):
