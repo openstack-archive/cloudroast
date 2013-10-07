af5b3092915179ea2d68047f05a51d26c6f62a8d
Charles Kimpolo <clmkimpo@thoughtworks.com>
2013-10-07 16:06:47 +0200

    PATCH v2.0/images
    Update a specified image's property - Smoke Test.
    
    Tested update the name of an image.
    Refactored fixtures.
    Addressed reviewer comment - removed unused rest client in fixtures.
    
    Reordered the import as per reviewer comment.
    Added explicit assertion messages for all assertions as per reviewer comments.
    Added a short description for each of the methods in the fixtures.
    Made some small changes in the fixtures.
    
    Added explicit assertion messages for other assertions as per reviewer comment.
    Moved the constant MSG to fixtures.
    
    Change-Id: Iec757d087a0696416ef8e5668a498440bf07d802

---
+++ cloudroast/images/v2/functional/test_patch_image.py
@@ -0,0 +1,52 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from cafe.drivers.unittest.decorators import tags
+
+from cloudcafe.common.tools.datagen import rand_name
+
+from cloudroast.images.v2.fixtures import ImagesV2Fixture, MSG
+
+
+class PatchImageTest(ImagesV2Fixture):
+
+    @tags(type='smoke')
+    def test_update_image_name(self):
+        """Update an image's name property.
+
+        1. Create an image
+        2. Update the name of an image
+        3. Verify response code is 200
+        4. Verify response is an image with correct properties.
+        """
+
+        image_id = self.register_basic_image()
+        updated_image_name = rand_name("updated_name_")
+        response = self.api_client.update_image(
+            image_id=image_id,
+            replace={"name": updated_image_name})
+
+        updated_image = response.entity
+
+        self.assertEqual(
+            response.status_code, 200,
+            MSG.format('status_code', 200, response.status_code))
+        self.assertEqual(
+            updated_image.name, updated_image_name,
+            MSG.format('name', updated_image_name, updated_image.name))
+        self.assertEqual(
+            updated_image.id_, image_id,
+            MSG.format('id', image_id, updated_image.id_))
