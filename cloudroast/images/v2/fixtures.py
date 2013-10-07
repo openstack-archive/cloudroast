fd4c01163c08adfd0a23bd9c057509c342d008cc
Charles Kimpolo <clmkimpo@thoughtworks.com>
2013-10-07 15:45:41 +0200

    POST v2.0/images
    Register a VM image with minimum compulsory properties - Smoke Test.
    
    Tested register image with compulsory properties.
    Reordered the import as per reviewer comment.
    Added explicit assertion messages for all assertions as per reviewer comments.
    
    Reordered the imports in 'test_post_images.py'
    Moved the constant MSG to fixtures.
    Added explicit assertion messages for all assertions as per reviewer comments.
    
    Moved the msg constant to fixtures setUpClass.
    Fixed line continuation and imports issues.
    
    Removed the custom assertion message.
    
    Change-Id: If68e321e7bcc4b4d1b41ad1e9442306d71a10006

--- cloudroast/images/v2/fixtures.py
+++ cloudroast/images/v2/fixtures.py
@@ -15,9 +15,9 @@ limitations under the License.
 """
 
 from cafe.drivers.unittest.fixtures import BaseTestFixture
-from cloudcafe.images.config import ImagesConfig
 from cloudcafe.auth.provider import AuthProvider
 from cloudcafe.common.resources import ResourcePool
+from cloudcafe.images.config import ImagesConfig
 from cloudcafe.images.v2.client import ImageClient as ImagesV2Client
 
 
@@ -25,6 +25,7 @@ class ImagesV2Fixture(BaseTestFixture):
     """
     @summary: Base fixture for Images V2 API tests
     """
+
     @classmethod
     def setUpClass(cls):
         super(ImagesV2Fixture, cls).setUpClass()
---
+++ cloudroast/images/v2/functional/test_post_images.py
@@ -0,0 +1,46 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from cafe.drivers.unittest.decorators import tags
+from cloudcafe.common.tools.datagen import rand_name
+from cloudroast.images.v2.fixtures import ImagesV2Fixture
+
+
+class PostImagesTest(ImagesV2Fixture):
+    """Test creation/registration of images."""
+
+    @tags(type='smoke')
+    def test_register_image_with_compulsory_properties(self):
+        """
+        Register a VM image with minimum compulsory properties.
+
+        1. Register a new image.
+        2. Verify the response code is 201
+        3. Verify the model returned has correct properties.
+        """
+
+        image_name = rand_name('test_image_')
+        response = self.api_client.create_image(name=image_name,
+                                                container_format='bare',
+                                                disk_format='raw')
+
+        self.assertEqual(response.status_code, 201)
+
+        image = response.entity
+
+        self.assertEqual(image.name, image_name)
+        self.assertEqual(image.disk_format, 'raw')
+        self.assertEqual(image.container_format, 'bare')
