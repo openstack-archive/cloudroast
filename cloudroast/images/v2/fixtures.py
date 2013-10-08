76aba301e19ccc925717fe2f45e43c4fd6008166
Charles Kimpolo <clmkimpo@thoughtworks.com>
2013-10-08 11:50:26 +0200

    PUT v2.0/images/{image_id}/file.
    Register and upload new image.
    
    Tested upload image with an image file.
    
    Added explicit assertion messages for all assertions as per reviewer comments.
    Added a short description for each of the methods in the fixtures.
    Made some small changes in the fixtures.
    
    Reordered imports in the fixtures as per reviewer comment.
    Moved MSG constant to fixtures.
    Fixed line continuation issues.
    
    Moved the msg constant to fixtures setUpClass.
    Fixed line continuation and import issues.
    
    Removed the custom assertion message.
    
    Change-Id: Ifc963fb8086b393e69b58b5591d701a6e8e35ba2

--- cloudroast/images/v2/fixtures.py
+++ cloudroast/images/v2/fixtures.py
@@ -15,9 +15,10 @@ limitations under the License.
 """
 
 from cafe.drivers.unittest.fixtures import BaseTestFixture
-from cloudcafe.images.config import ImagesConfig
 from cloudcafe.auth.provider import AuthProvider
 from cloudcafe.common.resources import ResourcePool
+from cloudcafe.common.tools.datagen import rand_name
+from cloudcafe.images.config import ImagesConfig
 from cloudcafe.images.v2.client import ImageClient as ImagesV2Client
 
 
@@ -25,6 +26,7 @@ class ImagesV2Fixture(BaseTestFixture):
     """
     @summary: Base fixture for Images V2 API tests
     """
+
     @classmethod
     def setUpClass(cls):
         super(ImagesV2Fixture, cls).setUpClass()
@@ -44,3 +46,39 @@ class ImagesV2Fixture(BaseTestFixture):
     def tearDownClass(cls):
         super(ImagesV2Fixture, cls).tearDownClass()
         cls.resources.release()
+
+    @classmethod
+    def register_basic_image(cls):
+        """Register a standard image and return its id."""
+        response = cls.api_client.create_image(
+            name=rand_name('basic_image_'),
+            container_format='bare',
+            disk_format='raw')
+
+        image = response.entity
+
+        cls.resources.add(cls.api_client.delete_image, image.id_)
+
+        return image.id_
+
+    @classmethod
+    def register_private_image(cls):
+        """Register a private image and return its id."""
+        response = cls.api_client.create_image(
+            name=rand_name('private_image_'),
+            visibility='private',
+            container_format='bare',
+            disk_format='raw')
+
+        image = response.entity
+
+        cls.resources.add(cls.api_client.delete_image, image.id_)
+
+        return image.id_
+
+    @classmethod
+    def get_member_ids(cls, image_id):
+        """Get a list of ids for all available members."""
+        response = cls.api_client.list_members(image_id)
+
+        return [member.member_id for member in response.entity]
