53b8d3e496841858bfa30562ff2d02ce5e827388
Charles Kimpolo <clmkimpo@thoughtworks.com>
2013-10-08 15:54:30 +0200

    DELETE v2.0/images/{image_id}/members/{member_id}
    Delete a member from an image.
    
    Tested delete a valid member from a valid image.
    Refactored fixtures.
    
    Added explicit assertion messages for all assertions as per reviewer comments.
    Added a short description for each of the methods in the fixtures.
    
    Moved MSG constant to fixtures.
    Fixed line continuation issues.
    
    Moved msg constant to fixtures setUpClass.
    Fixed line continuation and import issues.
    
    Removed the custom assertion message.
    
    Change-Id: Ic7a6d5a6545531dbac1c15ef9cec66ea81aa7df9

---
+++ cloudroast/images/v2/functional/test_delete_members.py
@@ -0,0 +1,45 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from cafe.drivers.unittest.decorators import tags
+from cloudcafe.common.tools.datagen import rand_name
+from cloudroast.images.v2.fixtures import ImagesV2Fixture
+
+
+class DeleteImageMemberTest(ImagesV2Fixture):
+    @tags(type='smoke')
+    def test_delete_member_from_image(self):
+        """ Delete a member from an image.
+
+        1. Create a private image
+        2. Add a member to the image
+        3. Delete the added member from the image
+        4. Verify the response code is 204
+        5. Verify that the deleted member is not in the list of image members.
+        """
+
+        image_id = self.register_private_image()
+        member_id = rand_name('member_id_')
+        response = self.api_client.add_member(image_id=image_id,
+                                              member_id=member_id)
+        self.assertEqual(response.status_code, 200)
+
+        response = self.api_client.delete_member(image_id=image_id,
+                                                 member_id=member_id)
+        self.assertEqual(response.status_code, 204)
+
+        member_ids = self.get_member_ids(image_id=image_id)
+        self.assertNotIn(member_id, member_ids)
