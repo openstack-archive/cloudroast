955c75d9d366dbd6b5595c10fa1008ac8633b65b
Charles Kimpolo <clmkimpo@thoughtworks.com>
2013-10-08 11:50:26 +0200

    PUT v2.0/images/{image_id}/file.
    Register and upload new image.
    
    Tested upload image with an image file.
    
    Added explicit assertion messages for all assertions as per reviewer comments.
    Added a short description for each of the methods in the fixtures.
    Made some small changes in the fixtures.
    
    Reordered imports in the fixtures as per reviewer comment.
    Moved MSG constant to fixtures.
    Fixed line continuation issues.
    
    Moved the msg constant to fixtures setUpClass.
    Fixed line continuation and import issues.
    
    Removed the custom assertion message.
    Added a new line.
    
    Change-Id: Ifc963fb8086b393e69b58b5591d701a6e8e35ba2

---
+++ cloudroast/images/v2/functional/test_put_image.py
@@ -0,0 +1,40 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import cStringIO as StringIO
+
+from cafe.drivers.unittest.decorators import tags
+from cloudroast.images.v2.fixtures import ImagesV2Fixture
+
+
+class PutImageTest(ImagesV2Fixture):
+
+    @tags(type='smoke')
+    def test_upload_image(self):
+        """
+        Register and upload new image.
+
+        1. Register new image
+        2. Update image with image file
+        3. Verify response code is 204
+        """
+
+        image_id = self.register_basic_image()
+        image_data = StringIO.StringIO(('*' * 1024))
+
+        response = self.api_client.store_raw_image_data(image_id, image_data)
+
+        self.assertEqual(response.status_code, 204)
