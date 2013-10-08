b86b5c41a0e20e2ea7596f0395a6c120068a1c45
Charles Kimpolo <clmkimpo@thoughtworks.com>
2013-10-08 12:16:08 +0200

    POST v2/images/{image_id}/tags/{tag}
    Add tag to an image.
    
    Tested adding a valid tag to a valid image.
    Refactored fixtures.
    
    Added explicit assertion messages for all assertions as per reviewer comments.
    Added a short description for each of the methods in the fixtures.
    
    Change-Id: Ife9274cd4899b9bf796eac08cab0118c39efa37e

---
+++ cloudroast/images/v2/functional/test_post_tags.py
@@ -0,0 +1,47 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
+from cafe.drivers.unittest.decorators import tags
+
+from cloudcafe.common.tools.datagen import rand_name
+from cloudroast.images.v2.fixtures import ImagesV2Fixture
+
+
+class PostImageTagsTest(ImagesV2Fixture):
+
+    @tags(type='smoke')
+    def test_add_tag_to_image(self):
+        """
+        Add tag to an image.
+
+        1. Create an image
+        2. Add a tag to existing image
+        3. Verify the response code is 204
+        4. Verify that the added tag is in the list of image tags.
+        """
+
+        image_id = self.register_basic_image()
+        tag = rand_name('tag_')
+
+        response = self.api_client.add_tag(image_id, tag)
+        self.assertEqual(
+            response.status_code, 204,
+            self.msg.format('status_code', 204, response.status_code))
+
+        response = self.api_client.get_image(image_id)
+        image = response.entity
+        self.assertIn(tag, image.tags)
