0159c016dbb5e1e7bc2324a2a6f9247ccf24df42
ivo5307 <ivo.vasev@RACKSPACE.COM>
2013-10-23 15:26:50 -0500

    Cloud Init Tests All
    Change-Id: I74dffa09d5e5760bce3389cd60ad86e1f687fcd6

---
+++ cloudroast/compute/functional/cloudinit/test_ci_boothook.py
@@ -0,0 +1,74 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import base64
+
+from cafe.drivers.unittest.decorators import tags
+from cloudcafe.common.tools.datagen import rand_name
+from cloudroast.compute.fixtures import ComputeFixture
+
+
+class CloudInitBoothookTest(ComputeFixture):
+
+    @classmethod
+    def setUpClass(cls):
+        super(CloudInitBoothookTest, cls).setUpClass()
+        init_st = cls.config_drive_behaviors.read_cloud_init_from_config_drive(
+            cls.cloud_init_config.boothook_script)
+        cls.user_data_contents = init_st
+        user_data = base64.b64encode(cls.user_data_contents)
+        cls.key = cls.keypairs_client.create_keypair(rand_name("key")).entity
+        cls.resources.add(cls.key.name,
+                          cls.keypairs_client.delete_keypair)
+        cls.server_response = cls.server_behaviors.create_active_server(
+            config_drive=True,
+            key_name=cls.key.name,
+            user_data=user_data)
+        cls.server = cls.server_response.entity
+        cls.user_data_filepath = '{0}/openstack/latest/user_data'.format(
+            cls.config_drive_config.base_path_to_mount)
+        cls.resources.add(cls.server.id,
+                          cls.servers_client.delete_server)
+
+    @tags(type='smoke', net='yes')
+    def test_bothook_input_format(self):
+        """Verify the Cloud Boothook Input Format is working as expected"""
+        message = "Expected {0} to be {1}, was {2}."
+        self.assertEqual(200, self.server_response.status_code)
+        self.config_drive_behaviors.mount_config_drive(
+            server=self.server, servers_config=self.servers_config,
+            key=self.key.private_key,
+            source_path=self.config_drive_config.mount_source_path,
+            destination_path=self.config_drive_config.base_path_to_mount)
+        remote_client = self.server_behaviors.get_remote_instance_client(
+            self.server, self.servers_config, key=self.key.private_key)
+        instanse_user_data = remote_client.get_file_details(
+            filepath=self.user_data_filepath)
+        self.assertEqual(instanse_user_data.content,
+                         self.user_data_contents,
+                         msg=message.format('Configuration do not match',
+                                            instanse_user_data.content,
+                                            self.user_data_contents))
+        boothook_file_present = remote_client.is_file_present(
+            filepath="/var/lib/cloud/instances/{0}/boothooks/part-001".format(
+                self.server.id))
+        self.assertTrue(boothook_file_present,
+                        msg="Boothook file present on the instance")
+        boothook = remote_client.get_file_details(
+            "/var/lib/cloud/instances/{0}/boothooks/part-001".format(
+                self.server.id)).content
+        self.assertIn('hello_boothook!', boothook, 
+                      msg="boothook script is processed")
