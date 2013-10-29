dc8090cdb850c94f9de3761c4cdee9d570c627b3
Dave Barrett <dave.barrett@racksapce.com>
2013-10-29 09:40:23 -0500

    Resize integration tests for config drive  * Switched branch and added additional test case
    Change-Id: I7491a59f51b89a2af42b8a3c2f3fae88d5f804d4

---
+++ cloudroast/compute/functional/config_drive/test_config_drive_create_image.py
@@ -0,0 +1,176 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import base64
+
+from cafe.drivers.unittest.decorators import tags
+from cloudcafe.common.tools.datagen import rand_name
+from cloudroast.compute.fixtures import CreateServerFixture
+from cloudcafe.compute.common.types import NovaImageStatusTypes
+
+
+class CreateImageTest(CreateServerFixture):
+
+    @classmethod
+    def setUpClass(cls):
+        super(CreateImageTest, cls).setUpClass()
+
+        # Set variables
+        cls.image_name = rand_name('image')
+        cls.metadata = {'meta_key_1': 'meta_value_1',
+                        'meta_key_2': 'meta_value_2'}
+        cls.file_contents = 'This is a config drive test file.'
+        files = [{'path': '/test.txt', 'contents': base64.b64encode(
+            cls.file_contents)}]
+        cls.user_data_contents = "My user data"
+        user_data = base64.b64encode(cls.user_data_contents)
+        cls.key = cls.keypairs_client.create_keypair(rand_name("key")).entity
+        cls.resources.add(cls.key.name,
+                          cls.keypairs_client.delete_keypair)
+
+        # Build server
+        response = cls.server_behaviors.create_active_server(
+            config_drive=True,
+            personality=files,
+            user_data=user_data,
+            metadata=cls.metadata,
+            key_name=cls.key.name)
+        cls.server = response.entity
+        cls.user_data_filepath = '{0}/openstack/latest/user_data'.format(
+            cls.config_drive_config.base_path_to_mount)
+        remote_client = cls.server_behaviors.get_remote_instance_client(
+            cls.server, cls.servers_config, key=cls.key.private_key)
+
+        # Mount config drive
+        cls.config_drive_behaviors.mount_config_drive(
+            server=cls.server, servers_config=cls.servers_config,
+            key=cls.key.private_key,
+            source_path=cls.config_drive_config.mount_source_path,
+            destination_path=cls.config_drive_config.base_path_to_mount)
+        cls.user_data = remote_client.get_file_details(
+            file_path=cls.user_data_filepath).content
+        cls.kb_size = remote_client.get_directory_details(
+            cls.config_drive_config.base_path_to_mount)
+        cls.openstack_meta_pre = (
+            cls.config_drive_behaviors.get_openstack_metadata(
+                cls.server, cls.servers_config, key=cls.key.private_key,
+                filepath=cls.config_drive_config.openstack_meta_filepath))
+
+        # Create Image
+        server_id = cls.server.id
+        cls.image_response = cls.servers_client.create_image(
+            server_id, cls.image_name, metadata=cls.metadata)
+        cls.image_id = cls.parse_image_id(cls.image_response)
+        cls.resources.add(cls.image_id, cls.images_client.delete_image)
+        cls.image_behaviors.wait_for_image_status(
+            cls.image_id, NovaImageStatusTypes.ACTIVE)
+        cls.image = cls.images_client.get_image(cls.image_id).entity
+
+    @tags(type='positive', net='no')
+    def test_can_create_server_from_image(self):
+        """Verify that a new server can be created from the image.
+           And that the config drive is mounted on the new image"""
+        remote_client = self.server_behaviors.get_remote_instance_client(
+            self.server, self.servers_config, key=self.key.private_key)
+
+        # Mount config drive
+        self.user_data_filepath = '{0}/openstack/latest/user_data'.format(
+            self.config_drive_config.base_path_to_mount)
+        remote_client = self.server_behaviors.get_remote_instance_client(
+            self.server, self.servers_config, key=self.key.private_key)
+        self.config_drive_behaviors.mount_config_drive(
+            server=self.server, servers_config=self.servers_config,
+            key=self.key.private_key,
+            source_path=self.config_drive_config.mount_source_path,
+            destination_path=self.config_drive_config.base_path_to_mount)
+        self.resources.add(self.server.id, self.servers_client.delete_server)
+
+        # set variables
+        # verify thatthe directory exists after the reboot and remount
+        self.dir_openstack_content_present = (
+            remote_client.is_directory_present(
+                directory_path='{0}/openstack/content'.format(
+                    self.config_drive_config.base_path_to_mount)))
+        self.assertTrue(
+            self.dir_openstack_content_present,
+            msg="Directory Openstack is not present")
+
+    def test_create_server_openstack_metadata(self):
+        """Verify that a new server can be created from the image.
+           And that the config drive is mounted on the new image"""
+
+        # Mount config drive
+        self.user_data_filepath = '{0}/openstack/latest/user_data'.format(
+            self.config_drive_config.base_path_to_mount)
+        self.config_drive_behaviors.mount_config_drive(
+            server=self.server, servers_config=self.servers_config,
+            key=self.key.private_key,
+            source_path=self.config_drive_config.mount_source_path,
+            destination_path=self.config_drive_config.base_path_to_mount)
+        self.resources.add(self.server.id, self.servers_client.delete_server)
+
+        #set variables
+        # Verify that the metadata before the rebbot matchs the
+        # metadata after the reboot
+        message = "Expected {0} to be {1}, was {2}."
+        self.openstack_meta_after = (
+            self.config_drive_behaviors.get_openstack_metadata(
+                self.server, self.servers_config, key=self.key.private_key,
+                filepath=self.config_drive_config.openstack_meta_filepath))
+        self.assertEqual(
+            self.openstack_meta_after,
+            self.openstack_meta_pre,
+            msg="Meta data des not match")
+        openstack_meta = self.openstack_meta_after
+        self.assertEqual(
+            self.server.admin_pass,
+            openstack_meta.admin_pass,
+            msg=message.format(
+                'Password mismatch',
+                self.server.admin_pass,
+                openstack_meta.admin_pass))
+        self.assertIsNotNone(
+            openstack_meta.availability_zone,
+            msg="availability_zone was not set in the response")
+        self.assertIsNotNone(
+            openstack_meta.hostname,
+            msg="hostname was not set in the response")
+        self.assertIsNotNone(
+            openstack_meta.launch_index,
+            msg="launch_index was not set in the response")
+        self.assertEqual(
+            self.server.name,
+            openstack_meta.name,
+            msg=message.format(
+                'server name',
+                self.server.name,
+                openstack_meta.name))
+        self.assertEqual(openstack_meta.meta.get('meta_key_1'), 'meta_value_1')
+        self.assertEqual(openstack_meta.meta.get('meta_key_2'), 'meta_value_2')
+        self.assertEqual(
+            self.key.public_key,
+            getattr(openstack_meta.public_keys, self.key.name),
+            msg=message.format(
+                'key do not match',
+                self.key.public_key,
+                openstack_meta.public_keys))
+        self.assertEqual(
+            self.server.id,
+            openstack_meta.uuid,
+            msg=message.format(
+                'server id does not match',
+                self.server.id,
+                openstack_meta.uuid))
---
+++ cloudroast/compute/functional/config_drive/test_config_drive_resize_confirm.py
@@ -0,0 +1,180 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import base64
+
+from cafe.drivers.unittest.decorators import tags
+from cloudcafe.common.tools.datagen import rand_name
+from cloudroast.compute.fixtures import ComputeFixture
+from cloudcafe.compute.common.types import NovaServerStatusTypes
+
+
+class ResizeServerUpConfirmTests(ComputeFixture):
+
+    @classmethod
+    def setUpClass(cls):
+        super(ResizeServerUpConfirmTests, cls).setUpClass()
+
+        # set variables
+        cls.metadata = {'meta_key_1': 'meta_value_1',
+                        'meta_key_2': 'meta_value_2'}
+        cls.file_contents = 'This is a config drive test file.'
+        files = [{'path': '/test.txt', 'contents': base64.b64encode(
+            cls.file_contents)}]
+        cls.user_data_contents = "My user data"
+        user_data = base64.b64encode(cls.user_data_contents)
+
+        cls.key = cls.keypairs_client.create_keypair(rand_name("key")).entity
+        cls.resources.add(
+            cls.key.name,
+            cls.keypairs_client.delete_keypair)
+        # build server
+        server_response = cls.server_behaviors.create_active_server(
+            config_drive=True,
+            personality=files,
+            user_data=user_data,
+            flavor_ref=cls.flavor_ref,
+            metadata=cls.metadata,
+            key_name=cls.key.name)
+        server_to_resize = server_response.entity
+        cls.resources.add(
+            server_to_resize.id,
+            cls.servers_client.delete_server)
+        cls.server_behaviors.wait_for_server_status(
+            server_to_resize.id, NovaServerStatusTypes.ACTIVE)
+
+        cls.server = server_response.entity
+        cls.user_data_filepath = '{0}/openstack/latest/user_data'.format(
+            cls.config_drive_config.base_path_to_mount)
+
+        remote_client = cls.server_behaviors.get_remote_instance_client(
+            cls.server, cls.servers_config, key=cls.key.private_key)
+
+        # mount drive
+        cls.config_drive_behaviors.mount_config_drive(
+            server=cls.server, servers_config=cls.servers_config,
+            key=cls.key.private_key,
+            source_path=cls.config_drive_config.mount_source_path,
+            destination_path=cls.config_drive_config.base_path_to_mount)
+        cls.user_data_pre = remote_client.get_file_details(
+            file_path=cls.user_data_filepath).content
+        cls.kb_size_pre = remote_client.get_directory_details(
+            cls.config_drive_config.base_path_to_mount)
+
+        # resize server and confirm
+        cls.resize_resp = cls.servers_client.resize(
+            server_to_resize.id, cls.flavor_ref_alt)
+        cls.server_behaviors.wait_for_server_status(
+            server_to_resize.id, NovaServerStatusTypes.VERIFY_RESIZE)
+        cls.confirm_resize_resp = cls.servers_client.confirm_resize(
+            server_to_resize.id)
+
+        cls.server_behaviors.wait_for_server_status(
+            server_to_resize.id, NovaServerStatusTypes.ACTIVE)
+        resized_server_response = cls.servers_client.get_server(
+            server_to_resize.id)
+
+        cls.server = resized_server_response.entity
+        cls.server.admin_pass = server_to_resize.admin_pass
+        cls.resized_flavor = cls.flavors_client.get_flavor_details(
+            cls.flavor_ref_alt).entity
+
+        remote_client = cls.server_behaviors.get_remote_instance_client(
+            cls.server, cls.servers_config, key=cls.key.private_key)
+
+        #mount drive
+        cls.config_drive_behaviors.mount_config_drive(
+            server=cls.server, servers_config=cls.servers_config,
+            key=cls.key.private_key,
+            source_path=cls.config_drive_config.mount_source_path,
+            destination_path=cls.config_drive_config.base_path_to_mount)
+
+        cls.user_data_after = remote_client.get_file_details(
+            file_path=cls.user_data_filepath).content
+        cls.kb_size_after = remote_client.get_directory_details(
+            cls.config_drive_config.base_path_to_mount)
+
+        #set variables
+        cls.dir_openstack_content_present = remote_client.is_directory_present(
+            directory_path='{0}/openstack/content'.format(
+                cls.config_drive_config.base_path_to_mount))
+        cls.openstack_meta = cls.config_drive_behaviors.get_openstack_metadata(
+            cls.server, cls.servers_config, key=cls.key.private_key,
+            filepath=cls.config_drive_config.openstack_meta_filepath)
+
+    def test_verify_user_data(self):
+        self.assertEqual(
+            self.user_data_after, self.user_data_pre,
+            msg="User data different")
+
+    def test_verify_tolerance(self):
+        self.assertGreaterEqual(
+            self.kb_size_after.size, self.config_drive_config.min_size,
+            msg='New image is not less than tolerance')
+        self.assertLessEqual(
+            self.kb_size_after.size, self.config_drive_config.max_size,
+            msg='New image is not more than tolerance')
+
+    def test_directorty_present_after_resize(self):
+        self.assertFalse(
+            self.dir_openstack_content_present,
+            msg="Directory Openstack is not present")
+
+    def test_openstack_metadata(self):
+        message = "Expected {0} to be {1}, was {2}."
+        self.openstack_meta_after = (
+            self.config_drive_behaviors.get_openstack_metadata(
+                self.server,
+                self.servers_config,
+                key=self.key.private_key,
+                filepath=self.config_drive_config.openstack_meta_filepath))
+        openstack_meta = self.openstack_meta_after
+        self.assertIsNotNone(
+            openstack_meta.availability_zone,
+            msg="availability_zone was not set in the response")
+        self.assertIsNotNone(
+            openstack_meta.hostname,
+            msg="hostname was not set in the response")
+        self.assertIsNotNone(
+            openstack_meta.launch_index,
+            msg="launch_index was not set in the response")
+        self.assertEqual(
+            self.server.name,
+            openstack_meta.name,
+            msg=message.format(
+                'server name',
+                self.server.name,
+                openstack_meta.name))
+        self.assertEqual(openstack_meta.meta.get('meta_key_1'), 'meta_value_1')
+        self.assertEqual(openstack_meta.meta.get('meta_key_2'), 'meta_value_2')
+        self.assertEqual(
+            self.key.public_key,
+            getattr(openstack_meta.public_keys, self.key.name),
+            msg=message.format(
+                'key do not match',
+                self.key.public_key,
+                openstack_meta.public_keys))
+        self.assertEqual(
+            self.server.id,
+            openstack_meta.uuid,
+            msg=message.format(
+                'server id does not match',
+                self.server.id,
+                openstack_meta.uuid))
+
+    @tags(type='smoke', net='no')
+    def test_server_properties_after_resize(self):
+        self.assertNotEqual(self.flavor_ref, self.server.flavor.id)
