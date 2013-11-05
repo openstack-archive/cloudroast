f33520d3a5e12002bfe76d8db35ef77481b8d916
ivo5307 <ivo.vasev@RACKSPACE.COM>
2013-11-05 11:07:04 -0600

    Clooud Init Actions Test Case
       * Rebuild case
       * Typo fixes
    
    Change-Id: I7cc920178fdaf7c50a063314e17196a9e12f7a90

---
+++ cloudroast/compute/functional/cloudinit/test_ci_actions.py
@@ -0,0 +1,84 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import base64
+
+from cafe.drivers.unittest.decorators import tags
+from cloudcafe.common.tools.datagen import rand_name
+from cloudcafe.compute.common.types import NovaServerStatusTypes
+from cloudroast.compute.fixtures import ComputeFixture
+
+
+class CloudInitActionsTest(ComputeFixture):
+
+    @classmethod
+    def setUpClass(cls):
+        super(CloudInitActionsTest, cls).setUpClass()
+        init_st = cls.config_drive_behaviors.read_cloud_init_for_config_drive(
+            cls.cloud_init_config.user_data_script)
+        cls.user_data_contents = init_st
+        user_data = base64.b64encode(cls.user_data_contents)
+        cls.key = cls.keypairs_client.create_keypair(rand_name("key")).entity
+        cls.resources.add(cls.key.name,
+                          cls.keypairs_client.delete_keypair)
+        active_server_response = cls.server_behaviors.create_active_server(
+            config_drive=True,
+            user_data=user_data,
+            key_name=cls.key.name)
+        cls.server = active_server_response.entity
+        cls.resources.add(cls.server.id,
+                          cls.servers_client.delete_server)
+        cls.server_response = cls.servers_client.rebuild(
+            cls.server.id, cls.image_ref_alt, config_drive=True,
+            key_name=cls.key.name, user_data=user_data)
+        cls.server_after_rebuild = cls.server_response.entity
+        cls.server_behaviors.wait_for_server_status(
+            cls.server.id, NovaServerStatusTypes.ACTIVE)
+        cls.user_data_filepath = '{0}/openstack/latest/user_data'.format(
+            cls.config_drive_config.base_path_to_mount)
+
+    @tags(type='smoke', net='yes')
+    def test_user_data_input_format_on_rebuild(self):
+        """Verify the User Data Script Input Format is working on rebuild"""
+        message = "Expected {0} to be {1}, was {2}."
+        self.assertEqual(202, self.server_response.status_code)
+        self.config_drive_behaviors.mount_config_drive(
+            server=self.server_after_rebuild,
+            servers_config=self.servers_config,
+            key=self.key.private_key,
+            source_path=self.config_drive_config.mount_source_path,
+            destination_path=self.config_drive_config.base_path_to_mount)
+        remote_client = self.server_behaviors.get_remote_instance_client(
+            self.server_after_rebuild, self.servers_config,
+            key=self.key.private_key)
+        instance_user_data = remote_client.get_file_details(
+            self.user_data_filepath)
+        self.assertEqual(instance_user_data.content,
+                         self.user_data_contents,
+                         msg=message.format('Script does not match',
+                                            instance_user_data.content,
+                                            self.user_data_contents))
+        dir_script_present = remote_client.is_directory_present(
+            directory_path=self.cloud_init_config.user_data_created_directory)
+        self.assertTrue(dir_script_present,
+                        msg="Directory that was created by the script was "
+                        "not present")
+        file_script_present = remote_client.is_file_present(
+            '{0}/time.txt'.format(
+                self.cloud_init_config.user_data_created_directory))
+        self.assertTrue(file_script_present,
+                        msg="File that was created by the script was "
+                        "not present")
