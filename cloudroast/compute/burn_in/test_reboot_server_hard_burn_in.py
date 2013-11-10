b71145363775a8ba7f1651f679df044648dce10c
Daryl Walleck <daryl.walleck@RACKSPACE.COM>
2013-11-10 14:57:52 -0600

    Adds basic compute burn in tests
    * Adds burn in tests for primary server actions
    
    Change-Id: I9e6cbd1fd168d22d91b30024f44c91d616967843

---
+++ cloudroast/compute/burn_in/test_reboot_server_hard_burn_in.py
@@ -0,0 +1,32 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from cloudroast.compute.fixtures import ComputeFixture
+from cloudcafe.compute.common.types import NovaServerRebootTypes
+
+
+class RebootServerHardBurnIn(ComputeFixture):
+
+    @classmethod
+    def setUpClass(cls):
+        super(RebootServerHardBurnIn, cls).setUpClass()
+        resp = cls.server_behaviors.create_active_server()
+        cls.server = resp.entity
+        cls.resources.add(cls.server.id, cls.servers_client.delete_server)
+
+    def test_reboot_server_hard(self):
+        self.server_behaviors.reboot_and_await(
+            self.server.id, NovaServerRebootTypes.HARD)
---
+++ cloudroast/compute/burn_in/test_resize_server_burn_in.py
@@ -0,0 +1,51 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import unittest2 as unittest
+from unittest2.suite import TestSuite
+
+from cloudcafe.compute.flavors_api.config import FlavorsConfig
+from cloudroast.compute.fixtures import ComputeFixture
+
+flavors_config = FlavorsConfig()
+resize_enabled = flavors_config.resize_enabled
+
+
+def load_tests(loader, standard_tests, pattern):
+    suite = TestSuite()
+    suite.addTest(ResizeServerBurnIn("test_resize_server"))
+    suite.addTest(ResizeServerBurnIn("test_resize_server_confirm"))
+    return suite
+
+
+@unittest.skipUnless(
+    resize_enabled, 'Resize not enabled for this flavor class.')
+class ResizeServerBurnIn(ComputeFixture):
+
+    @classmethod
+    def setUpClass(cls):
+        super(ResizeServerBurnIn, cls).setUpClass()
+        resp = cls.server_behaviors.create_active_server()
+        cls.server = resp.entity
+        cls.resources.add(cls.server.id, cls.servers_client.delete_server)
+
+    def test_resize_server(self):
+        self.server_behaviors.resize_and_await(
+            self.server.id, self.flavor_ref_alt)
+
+    def test_resize_server_confirm(self):
+        self.servers_client.confirm_resize(self.server.id)
+        self.server_behaviors.wait_for_server_status(self.server.id, 'ACTIVE')
