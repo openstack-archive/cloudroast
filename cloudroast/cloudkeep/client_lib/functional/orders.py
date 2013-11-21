3b5b03cdd420f01f24c3b8ddd01760864739328c
John Vrbanac <john.vrbanac@rackspace.com>
2013-11-21 17:49:28 -0600

    Updating all client lib tests
    * Fixing Order API smoke tests
    * Fixing Orders functional tests
    * Fixing Secrets API smoke tests
    * Fixing Secrets API functional tests
    
    Change-Id: Id75842640e486be594453938e6b97074b6065a91

--- cloudroast/cloudkeep/client_lib/functional/orders.py
+++ cloudroast/cloudkeep/client_lib/functional/orders.py
@@ -13,9 +13,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import unittest2
+from unittest import skip
+from requests.exceptions import MissingSchema
 
-from barbicanclient.common.exceptions import ClientException
+from barbicanclient.client import HTTPClientError as ClientException
 from cafe.drivers.unittest.decorators import tags
 from cloudcafe.cloudkeep.common.responses import CloudkeepResponse
 from cloudcafe.cloudkeep.common.states import SecretsStates
@@ -25,277 +26,109 @@ from cloudroast.cloudkeep.client_lib.fixtures import OrdersFixture, \
 
 class OrdersAPI(OrdersFixture):
 
-    @tags(type='positive')
-    def test_create_order_w_only_mime_type(self):
-        """Covers creating order with only mime type. Should raise a
-        ClientException.
-        """
-        self.assertRaises(ClientException,
-                          self.cl_behaviors.create_order,
-                          mime_type=self.config.mime_type)
+    @tags(type='negative')
+    def test_create_order_w_only_content_type(self):
+        self.assertRaises(
+            ClientException, self.cl_behaviors.create_order,
+            payload_content_type=self.config.payload_content_type)
 
     @tags(type='negative')
     def test_create_order_w_null_values(self):
-        """Covers creating order with all null values. Should raise a
-        ClientException.
-        """
         self.assertRaises(ClientException, self.cl_behaviors.create_order)
 
     @tags(type='positive')
     def test_create_order_w_null_name(self):
-        """Covers creating order without a null name."""
         order = self.cl_behaviors.create_order(
             name=None,
-            mime_type=self.config.mime_type,
+            payload_content_type=self.config.payload_content_type,
             algorithm=self.config.algorithm,
             bit_length=self.config.bit_length,
             mode=self.config.mode)
         self.assertIsNotNone(order)
-
-    @tags(type='positive')
-    def test_create_order_w_null_name_checking_name(self):
-        """Covers creating order with a null name, checking that the
-        created secret's name matches the secret's ID.
-        """
-        order = self.cl_behaviors.create_order(
-            name=None,
-            mime_type=self.config.mime_type,
-            algorithm=self.config.algorithm,
-            bit_length=self.config.bit_length,
-            mode=self.config.mode)
-        secret_id = CloudkeepResponse.get_id_from_ref(order.secret_ref)
-        self.assertEqual(order.secret['name'], secret_id,
-                         "Name did not match secret ID")
+        self.assertIn('http', order)
 
     @tags(type='positive')
     def test_create_order_w_empty_name(self):
         """Covers creating order without an empty name."""
         order = self.cl_behaviors.create_order(
             name='',
-            mime_type=self.config.mime_type,
+            payload_content_type=self.config.payload_content_type,
             algorithm=self.config.algorithm,
             bit_length=self.config.bit_length,
             mode=self.config.mode)
-        self.assertIsNotNone(order)
 
-    @tags(type='positive')
-    def test_create_order_w_empty_name_checking_name(self):
-        """Covers creating order with an empty name, checking that the
-        created secret's name matches the secret's ID."""
-        order = self.cl_behaviors.create_order(
-            name='',
-            mime_type=self.config.mime_type,
-            algorithm=self.config.algorithm,
-            bit_length=self.config.bit_length,
-            mode=self.config.mode)
-        secret_id = CloudkeepResponse.get_id_from_ref(order.secret_ref)
-        self.assertEqual(order.secret['name'], secret_id,
-                         "Name did not match secret ID")
+        self.assertIsNotNone(order)
+        self.assertIn('http', order)
 
     @tags(type='negative')
     def test_create_order_w_invalid_mime_type(self):
-        """Covers creating order with an invalid mime type.
-        Should raise a ClientException.
-        """
         self.assertRaises(ClientException,
                           self.cl_behaviors.create_order_overriding_cfg,
-                          mime_type='crypto/boom')
+                          payload_content_type='crypto/boom')
 
     @tags(type='negative')
     def test_create_order_w_invalid_bit_length(self):
-        """Covers creating order with a bit length that is not an integer.
-        Should raise a ClientException.
-        """
         self.assertRaises(ClientException,
                           self.cl_behaviors.create_order_overriding_cfg,
                           bit_length='not-an-int')
 
     @tags(type='negative')
     def test_create_order_w_negative_bit_length(self):
-        """Covers creating order with a negative bit length.
-        Should raise a ClientException.
-        """
         self.assertRaises(ClientException,
                           self.cl_behaviors.create_order_overriding_cfg,
                           bit_length=-1)
 
     @tags(type='positive')
-    def test_create_order_checking_metadata(self):
-        """Covers creating order and checking metadata of secret created.
-        Assumes that order status is active and not pending.
-        """
-        order = self.cl_behaviors.create_order_from_config()
-        secret = order.secret
+    def test_create_order_checking_secret_metadata(self):
+        """ Assumes that the order status is active and not pending."""
+        order_ref = self.cl_behaviors.create_order_from_config()
+        order = self.cl_client.get_order(order_ref)
 
-        self.assertEqual(secret['name'], self.config.name)
-        self.assertEqual(secret['mime_type'], self.config.mime_type)
-        self.assertEqual(secret['mode'], self.config.mode)
-        self.assertEqual(secret['algorithm'], self.config.algorithm)
-        self.assertEqual(secret['bit_length'], self.config.bit_length)
+        # Retrieve automatically created secret metadata
+        secret_id = CloudkeepResponse.get_id_from_ref(order.secret_ref)
+        secret = self.secrets_client.get_secret(secret_id).entity
+
+        self.assertIsNotNone(secret)
+        self.assertEqual(secret.name, self.config.name)
+        self.assertEqual(secret.mode, self.config.mode)
+        self.assertEqual(secret.algorithm, self.config.algorithm)
+        self.assertEqual(secret.bit_length, self.config.bit_length)
+        self.assertEqual(secret.content_types.get('default'),
+                         self.config.payload_content_type)
 
     @tags(type='negative')
-    def test_delete_nonexistent_order_by_href(self):
-        """Covers deleting an order that does not exist by href.
-        Should raise a ClientException.
-        """
-        self.assertRaises(ClientException,
+    def test_delete_nonexistent_order(self):
+        self.assertRaises(MissingSchema,
                           self.cl_behaviors.delete_order,
                           'not-an-href')
 
     @tags(type='negative')
-    def test_delete_nonexistent_order_by_id(self):
-        """Covers deleting an order that does not exist by id.
-        Should raise a ClientException.
-        """
-        self.assertRaises(ClientException,
-                          self.cl_behaviors.delete_order_by_id,
-                          'not-an-id')
-
-    @tags(type='negative')
-    def test_get_nonexistent_order_by_href(self):
-        """Covers getting an order that does not exist by href.
-        Should raise a ClientException.
-        """
-        self.assertRaises(ClientException,
+    def test_get_nonexistent_order(self):
+        self.assertRaises(MissingSchema,
                           self.cl_client.get_order,
                           'not-an-href')
 
-    @tags(type='negative')
-    def test_get_nonexistent_order_by_id(self):
-        """Covers deleting an order that does not exist by id.
-        Should raise a ClientException.
-        """
-        self.assertRaises(ClientException,
-                          self.cl_client.get_order_by_id,
-                          'not-an-id')
-
+    @skip('Not implemented')
     @tags(type='positive')
-    def test_get_order_by_href_checking_metadata(self):
-        """Covers getting an order by href and checking the secret
-        metadata. Assumes that order status is active and not pending.
-        """
-        resp = self.barb_behaviors.create_order_from_config()
-        self.assertEqual(resp.status_code, 202,
-                         'Barbican returned unexpected response code')
-
-        order = self.cl_client.get_order(resp.ref)
-        secret_metadata = order.secret
-
-        self.assertEqual(secret_metadata['name'], self.config.name)
-        self.assertEqual(secret_metadata['mime_type'], self.config.mime_type)
-        self.assertEqual(secret_metadata['algorithm'], self.config.algorithm)
-        self.assertEqual(secret_metadata['bit_length'], self.config.bit_length)
-        self.assertEqual(secret_metadata['mode'],
-                         self.config.mode)
-
-    @tags(type='positive')
-    def test_get_order_by_id_checking_metadata(self):
-        """Covers getting an order by id and checking the secret
-        metadata. Compares to the values of the initial creation.
-        Assumes that order status is active and not pending.
-        """
-        resp = self.barb_behaviors.create_order_from_config()
-        self.assertEqual(resp.status_code, 202,
-                         'Barbican returned unexpected response code')
-
-        order = self.cl_client.get_order_by_id(resp.id)
-        secret_metadata = order.secret
-
-        self.assertEqual(secret_metadata['name'], self.config.name)
-        self.assertEqual(secret_metadata['mime_type'], self.config.mime_type)
-        self.assertEqual(secret_metadata['algorithm'], self.config.algorithm)
-        self.assertEqual(secret_metadata['bit_length'], self.config.bit_length)
-        self.assertEqual(secret_metadata['mode'],
-                         self.config.mode)
-
-    @tags(type='positive')
-    def test_get_order_w_expiration_by_href(self):
-        """Covers getting an order that created a secret with an expiration
-        by href. Assumes that order status is active and not pending.
-        """
+    def test_get_order_w_expiration(self):
+        """ Assumes that order status is active and not pending."""
         resp = self.barb_behaviors.create_order_from_config(
             use_expiration=True)
-        self.assertEqual(resp.status_code, 202,
-                         'Barbican returned unexpected response code')
+        self.assertEqual(resp.status_code, 202)
 
         order_ref = resp.ref
         order = self.cl_client.get_order(href=order_ref)
         secret = order.secret
         self.assertIsNotNone(secret['expiration'])
 
-    @tags(type='positive')
-    def test_get_order_w_expiration_by_id(self):
-        """Covers getting an order that created a secret with an expiration
-        by id. Assumes that order status is active and not pending.
-        """
-        resp = self.barb_behaviors.create_order_from_config(
-            use_expiration=True)
-        self.assertEqual(resp.status_code, 202,
-                         'Barbican returned unexpected response code')
-
-        order_id = resp.id
-        order = self.cl_client.get_order_by_id(order_id=order_id)
-        secret = order.secret
-        self.assertIsNotNone(secret['expiration'])
-
-    @tags(type='positive')
-    def test_order_get_secret_checking_metadata(self):
-        """Covers getting a secret using the Order object function and
-        checking the metadata of the secret. Assumes that order status
-        is active and not pending.
-        """
-        resp = self.barb_behaviors.create_order_from_config()
-        self.assertEqual(resp.status_code, 202,
-                         'Barbican returned unexpected response code')
-
-        order = self.cl_client.get_order_by_id(resp.id)
-        secret = order.get_secret()
-
-        self.assertEqual(secret.status, SecretsStates.ACTIVE)
-        self.assertEqual(secret.name, self.config.name)
-        self.assertEqual(secret.mime_type, self.config.mime_type)
-        self.assertEqual(secret.algorithm, self.config.algorithm)
-        self.assertEqual(secret.bit_length, self.config.bit_length)
-        self.assertEqual(secret.mode, self.config.mode)
-
-
 class OrdersPagingAPI(OrdersPagingFixture):
 
     @tags(type='positive')
     def test_list_orders_limit_and_offset(self):
-        """Covers using the limit and offset attribute of listing orders."""
         # First set of orders
-        list_resp = self.cl_client.list_orders(limit=10, offset=0)
-        order_group1 = list_resp[0]
+        order_group1 = self.cl_client.list_orders(limit=10, offset=0)
 
         # Second set of orders
-        list_resp = self.cl_client.list_orders(limit=10, offset=10)
-        order_group2 = list_resp[0]
-
-        self._check_for_duplicates(group1=order_group1, group2=order_group2)
-
-    @tags(type='positive')
-    def test_list_orders_next(self):
-        """Covers using next reference for listing orders."""
-        # First set of orders
-        order_group1, prev_ref, next_ref = self.cl_client.list_orders(
-            limit=10, offset=0)
-
-        # Next set of orders
-        list_resp = self.cl_client.list_orders_by_href(href=next_ref)
-        order_group2 = list_resp[0]
-
-        self._check_for_duplicates(group1=order_group1, group2=order_group2)
-
-    @tags(type='positive')
-    def test_list_orders_previous(self):
-        """Covers using next reference for listing orders."""
-        # First set of orders
-        order_group1, prev_ref, next_ref = self.cl_client.list_orders(
-            limit=10, offset=10)
-
-        # Previous set of orders
-        list_resp = self.cl_client.list_orders_by_href(href=prev_ref)
-        order_group2 = list_resp[0]
+        order_group2 = self.cl_client.list_orders(limit=10, offset=10)
 
         self._check_for_duplicates(group1=order_group1, group2=order_group2)
--- cloudroast/cloudkeep/client_lib/smoke/secrets.py
+++ cloudroast/cloudkeep/client_lib/smoke/secrets.py
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import unittest2
+from binascii import a2b_base64 as base64
 
 from cafe.drivers.unittest.decorators import tags
 from cloudcafe.cloudkeep.common.states import SecretsStates
@@ -24,151 +24,54 @@ class SecretsAPI(SecretsFixture):
 
     @tags(type='positive')
     def test_create_secret(self):
-        """Covers creating a secret with the barbicanclient library.
-        Includes creating a secret with an expiration.
-        - Reported in python-barbicanclient GitHub Issue #9
-        """
-        try:
-            secret = self.cl_behaviors.create_secret_from_config(
-                use_expiration=True)
-        except TypeError, error:
-            self.fail("Creation failed with TypeError: {0}".format(error))
-
-        resp = self.barb_client.get_secret(secret.id)
-        self.assertEqual(resp.status_code, 200,
-                         'Barbican returned unexpected response code')
-
-    @tags(type='positive')
-    def test_create_secret_wout_expiration(self):
-        """Covers creating a secret without expiration with
-        barbicanclient library.
-        """
         resps = self.cl_behaviors.create_and_check_secret()
-        self.assertEqual(resps.get_status_code, 200,
-                         'Barbican returned unexpected response code')
+        get_response = resps.get_resp
+
+         # Check to see if we received a string with http
+        self.assertIn('http', resps.entity)
+        self.assertEqual(get_response.status_code, 200)
 
     @tags(type='positive')
-    def test_get_secret_by_href(self):
-        """Covers getting a secret by href with barbicanclient library."""
-        resp = self.barb_behaviors.create_secret_from_config(
-            use_expiration=False)
+    def test_delete_secret(self):
+        # Creating sample secret directly with API
+        resp = self.barb_behaviors.create_secret_from_config()
+        self.assertEqual(resp.status_code, 201)
 
-        secret = self.cl_client.get_secret(href=resp.ref)
-        self.assertIsNotNone(secret)
-
-    @tags(type='positive')
-    def test_get_secret_by_id(self):
-        """Covers getting a secret by id with barbicanclient library."""
-        resp = self.barb_behaviors.create_secret_from_config(
-            use_expiration=False)
-        self.assertEqual(resp.status_code, 201,
-                         'Barbican returned unexpected response code')
-        secret = self.cl_client.get_secret_by_id(secret_id=resp.id)
-        self.assertIsNotNone(secret)
-
-    @tags(type='positive')
-    def test_delete_secret_by_href(self):
-        """Covers deleting a secret by href with barbicanclient library."""
-        resp = self.barb_behaviors.create_secret_from_config(
-            use_expiration=False)
-        self.assertEqual(resp.status_code, 201,
-                         'Barbican returned unexpected response code')
-
+        # Delete using client lib
         self.cl_behaviors.delete_secret(secret_ref=resp.ref)
-        # Deleting here because using two different behaviors
-        self.barb_behaviors.remove_from_created_secrets(
-            secret_id=resp.id)
 
-        get_resp = self.barb_client.get_secret(secret_id=resp.id)
-        self.assertEqual(get_resp.status_code, 404,
-                         'Should have failed with 404')
+        # Removing from Barbican API behavior as well.
+        self.barb_behaviors.remove_from_created_secrets(secret_id=resp.id)
+
+        get_response = self.barb_client.get_secret(secret_id=resp.id)
+        self.assertEqual(get_response.status_code, 404)
 
     @tags(type='positive')
-    def test_delete_secret_by_id(self):
-        """Covers deleting a secret by id with barbicanclient library."""
-        resp = self.barb_behaviors.create_secret_from_config(
-            use_expiration=False)
-        self.assertEqual(resp.status_code, 201,
-                         'Barbican returned unexpected response code')
+    def test_get_secret_metadata(self):
+        resp = self.barb_behaviors.create_secret_from_config()
+        secret_meta = self.cl_client.get_secret(href=resp.ref)
 
-        self.cl_behaviors.delete_secret_by_id(secret_id=resp.id)
-        # Deleting here because using two different behaviors
-        self.barb_behaviors.remove_from_created_secrets(
-            secret_id=resp.id)
+        self.assertIsNotNone(secret_meta)
+        self.assertEqual(secret_meta.status, SecretsStates.ACTIVE)
+        self.assertEqual(secret_meta.name, self.config.name)
+        self.assertEqual(secret_meta.mode, self.config.mode)
+        self.assertEqual(secret_meta.algorithm, self.config.algorithm)
+        self.assertEqual(secret_meta.bit_length, self.config.bit_length)
 
-        get_resp = self.barb_client.get_secret(secret_id=resp.id)
-        self.assertEqual(get_resp.status_code, 404,
-                         'Should have failed with 404')
+    @tags(type='positive')
+    def test_get_secret_raw(self):
+        resp = self.barb_behaviors.create_secret_from_config()
+        self.assertEqual(resp.status_code, 201)
+
+        raw_secret = self.cl_client.get_raw_secret(
+            href=resp.ref, content_type=self.config.payload_content_type)
+
+        self.assertEqual(raw_secret, base64(self.config.payload))
 
     @tags(type='positive')
     def test_list_secrets(self):
-        """Covers listing secrets with barbicanclient library."""
-        resp = self.barb_behaviors.create_secret_from_config(
-            use_expiration=False)
-        self.assertEqual(resp.status_code, 201,
-                         'Barbican returned unexpected response code')
+        resp = self.barb_behaviors.create_secret_from_config()
+        self.assertEqual(resp.status_code, 201)
 
-        list_tuple = self.cl_client.list_secrets(limit=10, offset=0)
-        secrets = list_tuple[0]
-        self.assertGreater(len(secrets), 0)
-
-    @tags(type='positive')
-    def test_list_secrets_by_href(self):
-        """Covers listing secrets by href with barbicanclient library."""
-        resp = self.barb_behaviors.create_secret_from_config(
-            use_expiration=False)
-        self.assertEqual(resp.status_code, 201,
-                         'Barbican returned unexpected response code')
-
-        list_tuple = self.cl_client.list_secrets_by_href()
-        secrets = list_tuple[0]
-        self.assertGreater(len(secrets), 0)
-
-    @tags(type='positive')
-    def test_create_secret_metadata(self):
-        """Covers creating a secret with barbicanclient library and checking
-        the metadata of the secret.
-        """
-        secret = self.cl_behaviors.create_secret_from_config(
-            use_expiration=False)
-
-        resp = self.barb_client.get_secret(secret.id)
-        metadata = resp.entity
-
-        self.assertEqual(resp.status_code, 200,
-                         'Returned unexpected response code')
-        self.assertEqual(metadata.status, SecretsStates.ACTIVE)
-        self.assertEqual(metadata.name, self.config.name)
-        self.assertEqual(metadata.mode, self.config.mode)
-        self.assertEqual(metadata.algorithm, self.config.algorithm)
-        self.assertEqual(metadata.bit_length, self.config.bit_length)
-
-    @tags(type='positive')
-    def test_get_raw_secret_by_href(self):
-        """Covers getting the secret payload by href with
-        barbicanclient library.
-        """
-        resp = self.barb_behaviors.create_secret_from_config(
-            use_expiration=False)
-        self.assertEqual(resp.status_code, 201,
-                         'Barbican returned unexpected response code')
-
-        raw_secret = self.cl_client.get_raw_secret(
-            resp.ref, self.config.mime_type)
-
-        self.assertEqual(raw_secret, self.config.plain_text)
-
-    @tags(type='positive')
-    def test_get_raw_secret_by_id(self):
-        """Covers getting the secret payload by id with
-        barbicanclient library.
-        """
-        resp = self.barb_behaviors.create_secret_from_config(
-            use_expiration=False)
-        self.assertEqual(resp.status_code, 201,
-                         'Barbican returned unexpected response code')
-
-        raw_secret = self.cl_client.get_raw_secret_by_id(
-            resp.id, self.config.mime_type)
-
-        self.assertEqual(raw_secret, self.config.plain_text)
+        secrets = self.cl_client.list_secrets(limit=1, offset=0)
+        self.assertEqual(len(secrets), 1)
