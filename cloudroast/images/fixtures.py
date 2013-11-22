42fa8747d613de3ee87b77e8ba731549e9a081dc
Luke Wollney <luke.wollney@rackspace.com>
2013-11-22 15:14:16 -0600

    Update images fixtures and remove test modules
    Change-Id: I4258d87247e6dc4ffe4e36ea02f2f178c4d28642

--- cloudroast/images/fixtures.py
+++ cloudroast/images/fixtures.py
@@ -15,18 +15,83 @@ limitations under the License.
 """
 
 from cafe.drivers.unittest.fixtures import BaseTestFixture
-from cloudcafe.images.config import ImagesConfig
+from cloudcafe.auth.config import UserAuthConfig, UserConfig
+from cloudcafe.auth.provider import AuthProvider
 from cloudcafe.common.resources import ResourcePool
+from cloudcafe.images.config import \
+    AltUserConfig, ImagesConfig, MarshallingConfig
+from cloudcafe.images.v2.behaviors import ImagesBehaviors
+from cloudcafe.images.v2.client import ImagesClient
 
 
 class ImagesFixture(BaseTestFixture):
+    """@summary: Fixture for images v2 api"""
+
     @classmethod
     def setUpClass(cls):
         super(ImagesFixture, cls).setUpClass()
-        cls.config = ImagesConfig()
+        cls.images_config = ImagesConfig()
+        cls.marshalling = MarshallingConfig()
+        cls.endpoint_config = UserAuthConfig()
+        cls.user_config = UserConfig()
+        cls.alt_user_config = AltUserConfig()
         cls.resources = ResourcePool()
+        cls.serialize_format = cls.marshalling.serializer
+        cls.deserialize_format = cls.marshalling.deserializer
+        # TODO: Remove once import/export functionality is implemented
+        internal_url = cls.images_config.internal_url
+        cls.access_data = AuthProvider.get_access_data(cls.endpoint_config,
+                                                       cls.user_config)
+        # If authentication fails, fail immediately
+        if cls.access_data is None:
+            cls.assertClassSetupFailure('Authentication failed')
+        cls.alt_access_data = AuthProvider.get_access_data(cls.endpoint_config,
+                                                           cls.alt_user_config)
+        # If authentication fails, fail immediately
+        if cls.alt_access_data is None:
+            cls.assertClassSetupFailure('Authentication failed')
+        images_service = cls.access_data.get_service(
+            cls.images_config.endpoint_name)
+        public_url_check = \
+            images_service.get_endpoint(cls.images_config.region)
+        # If endpoint validation fails, fail immediately
+        if public_url_check is None:
+            cls.assertClassSetupFailure('Endpoint validation failed')
+        cls.url = \
+            images_service.get_endpoint(cls.images_config.region).public_url
+        # If a url override was provided, use it instead
+        if cls.images_config.override_url:
+            cls.url = cls.images_config.override_url
+        cls.images_client = cls.generate_images_client(
+            cls.access_data, internal_url)
+        cls.alt_images_client = cls.generate_images_client(
+            cls.alt_access_data, internal_url)
+        cls.images_behavior = ImagesBehaviors(
+            images_client=cls.images_client, images_config=cls.images_config)
+        cls.alt_images_behavior = ImagesBehaviors(
+            images_client=cls.alt_images_client,
+            images_config=cls.images_config)
+        cls.image_schema_json = (
+            open(cls.images_config.image_schema_json).read().rstrip())
+        cls.images_schema_json = (
+            open(cls.images_config.images_schema_json).read().rstrip())
 
     @classmethod
     def tearDownClass(cls):
-        cls.resources.release()
         super(ImagesFixture, cls).tearDownClass()
+        cls.resources.release()
+
+    @classmethod
+    def generate_images_client(self, auth_data, internal_url=None):
+        """@summary: Returns new images client for requested auth data"""
+
+        if internal_url is not None:
+            url = internal_url
+        else:
+            url = self.url
+
+        client_args = {'base_url': url,
+                       'auth_token': auth_data.token.id_,
+                       'serialize_format': self.serialize_format,
+                       'deserialize_format': self.deserialize_format}
+        return ImagesClient(**client_args)
