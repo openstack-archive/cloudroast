d2d6909957e6e8429feb1eb14d485de09c89ee1c
Luke Wollney <luke.wollney@rackspace.com>
2013-11-22 15:27:37 -0600

    Add get image members and get images tests
    *Refactored get image members test module including updating tests to reflect cloud cafe changes and aesthetic changes
    *Refactored get images test module including updating tests to reflect cloud cafe changes and aesthetic changes
    
    Change-Id: Idf6c2f7775fa179127cadb5b54e9352508ad6b6d

--- cloudroast/images/v2/functional/test_get_image_members.py
+++ cloudroast/images/v2/functional/test_get_image_members.py
@@ -15,97 +15,39 @@ limitations under the License.
 """
 
 from cafe.drivers.unittest.decorators import tags
-from cloudcafe.common.tools.datagen import rand_name
 from cloudcafe.images.common.types import ImageVisibility
-from cloudroast.images.v2.fixtures import ImagesV2Fixture
+from cloudroast.images.fixtures import ImagesFixture
 
 
-class GetImageMembersTest(ImagesV2Fixture):
+class TestGetImageMembers(ImagesFixture):
 
     @tags(type='smoke')
-    def test_get_members_of_image(self):
-        """ Get the members of an image.
+    def test_happy_path_get_image_members(self):
+        """
+        @summary: Happy path - Get image members
 
-        1. Create an image
-        2. Add a member (member1) to the image
-        3. Verify the response code is 200
-        4. Add another member (member2) to the image
-        5. Verify the response code is 200
-        6. Get members list for an image
-        7. Verify the response code is 200
-        8. Verify the response body contains member1 and member2
+        1) Create image
+        2) Add image member
+        3) Verify that the response code is 200
+        4) Get image members
+        5) Verify that the response code is 200
+        6) Verify that the response contains the correct properties for the
+        member
         """
 
-        image_id = self.images_behavior.register_private_image()
-
-        member1_name = rand_name("member_")
-        response = self.api_client.add_member(image_id, member1_name)
-        member1 = response.entity
-        self.assertEqual(response.status_code, 200)
-
-        member2_name = rand_name("member_")
-        response = self.api_client.add_member(image_id, member2_name)
-        member2 = response.entity
-        self.assertEqual(response.status_code, 200)
-
-        response = self.api_client.list_members(image_id)
-        members = response.entity
-
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(member1, members)
-        self.assertIn(member2, members)
-
-    @tags(type='negative')
-    def test_get_members_using_invalid_image_id(self):
-        """ Get the members of an image using an invalid image id.
-
-        1. Try get members list for an image using invalid image id
-        2. Verify the response code is 404
-        """
-        image_id = 'invalid_image_id'
-        response = self.api_client.list_members(image_id)
-        self.assertEquals(response.status_code, 404)
-
-    @tags(type='negative')
-    def test_get_members_for_deleted_image(self):
-        """ Get the members of a deleted image.
-
-        1. Try get members list for a deleted image
-        2. Verify the response code is 404
-        """
-        response = self.api_client.create_image()
-        self.assertEquals(response.status_code, 201)
-
-        image_id = response.entity.id_
-        response = self.api_client.delete_image(image_id)
-
-        self.assertEquals(response.status_code, 204)
-
-        response = self.api_client.list_members(image_id)
-        self.assertEquals(response.status_code, 404)
-
-    @tags(type='negative')
-    def test_get_members_using_a_blank_image_id(self):
-        """ Get the members using a blank image id.
-
-        1. Try get members list using a blank image id
-        2. Verify the response code is 404
-        """
-        image_id = ''
-        response = self.api_client.list_members(image_id)
-        self.assertEquals(response.status_code, 404)
-
-    @tags(type='negative')
-    def test_get_members_for_a_private_image_as_non_admin(self):
-        """ Get the members of an image.
-
-        1. Try get members list for an image
-        2. Verify the response code is 404
-        """
-        response = self.admin_api_client.create_image(
+        member_id = self.alt_user_config.tenant_id
+        image = self.images_behavior.create_new_image(
             visibility=ImageVisibility.PRIVATE)
-        image_id = response.entity.id_
-        self.assertEquals(response.status_code, 201)
-
-        response = self.api_client.list_members(image_id)
-        self.assertEquals(response.status_code, 404)
+        response = self.images_client.add_member(image.id_, member_id)
+        self.assertEqual(response.status_code, 200)
+        member = response.entity
+        response = self.images_client.list_members(image.id_)
+        self.assertEqual(response.status_code, 200)
+        members = response.entity
+        self.assertEqual(len(members), 1)
+        self.assertEqual(members[0].created_at, member.created_at)
+        self.assertEqual(members[0].image_id, member.image_id)
+        self.assertEqual(members[0].member_id, member.member_id)
+        self.assertEqual(members[0].schema, member.schema)
+        self.assertEqual(members[0].status, member.status)
+        self.assertEqual(members[0].updated_at, member.updated_at)
