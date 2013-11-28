f462fb3805b9bac9c90183d712e268dd3342b7be
Charles Kimpolo <clmkimpo@thoughtworks.com>
2013-11-28 14:42:49 +0200

    Images API v2.0 One To Many Image Sharing
    Implements one to many image sharing test:
     - added a new test scenario 'one to many image sharing'
     - refactored image fixtures
    
    Change-Id: I2d828a575d7489e806c2f39ad76455f5830264b8

---
+++ cloudroast/images/v2/functional/test_one_to_many_image_sharing.py
@@ -0,0 +1,104 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from cloudcafe.images.common.types import ImageMemberStatus
+from cloudroast.images.fixtures import ImagesFixture
+
+
+class OneToManyImageSharingTest(ImagesFixture):
+
+    def test_one_to_many_image_sharing(self):
+        """
+        @summary: One to many image sharing
+
+        1. Register an image (admin_image) as an admin tenant
+        2. Verify that tenant A cannot access the image
+        3. Verify that tenant B cannot access the image
+        4. Add tenant A as a member of the image
+        5. Verify that tenant A can now access the image
+        6. Add tenant B as a member of the image
+        7. Verify that tenant B can now access the image
+        8. Update tenant A membership status to 'Accepted' for the image
+        9. Verify that tenant A can now see the image in their list
+        10. Update tenant B membership status to 'Accepted' for the image
+        11. Verify that tenant B can now see the image in their list
+        12. List all members of the image
+        13. Verify that tenant A belongs to the image members list
+        14. Verify that tenant B belongs to the image members list
+        """
+
+        admin_image = self.admin_images_behavior.create_new_image()
+        tenant_A_id = self.access_data.token.tenant.id_
+        tenant_B_id = self.alt_access_data.token.tenant.id_
+
+        response = self.images_client.get_image(image_id=admin_image.id_)
+        self.assertEqual(response.status_code, 404)
+
+        response = self.alt_images_client.get_image(image_id=admin_image.id_)
+        self.assertEqual(response.status_code, 404)
+
+        response = self.admin_images_client.add_member(
+            image_id=admin_image.id_, member_id=tenant_A_id)
+        self.assertEqual(response.status_code, 200)
+        member = response.entity
+        self.assertEqual(member.member_id, tenant_A_id)
+        self.assertEqual(member.status, ImageMemberStatus.PENDING)
+
+        response = self.images_client.get_image(image_id=admin_image.id_)
+        self.assertEqual(response.status_code, 200)
+        image = response.entity
+        self.assertEqual(image, admin_image)
+
+        response = self.admin_images_client.add_member(
+            image_id=admin_image.id_, member_id=tenant_B_id)
+        self.assertEqual(response.status_code, 200)
+        member = response.entity
+        self.assertEqual(member.member_id, tenant_B_id)
+        self.assertEqual(member.status, ImageMemberStatus.PENDING)
+
+        response = self.alt_images_client.get_image(image_id=admin_image.id_)
+        self.assertEqual(response.status_code, 200)
+        image = response.entity
+        self.assertEqual(image, admin_image)
+
+        response = self.images_client.update_member(
+            image_id=admin_image.id_, member_id=tenant_A_id,
+            status=ImageMemberStatus.ACCEPTED)
+        member = response.entity
+        self.assertEqual(member.member_id, tenant_A_id)
+        self.assertEqual(member.status, ImageMemberStatus.ACCEPTED)
+
+        response = self.images_client.list_images()
+        self.assertEqual(response.status_code, 200)
+        images = response.entity
+        self.assertIn(admin_image, images)
+
+        response = self.alt_images_client.update_member(
+            image_id=admin_image.id_, member_id=tenant_B_id,
+            status=ImageMemberStatus.ACCEPTED)
+        member = response.entity
+        self.assertEqual(member.member_id, tenant_B_id)
+        self.assertEqual(member.status, ImageMemberStatus.ACCEPTED)
+
+        response = self.alt_images_client.list_images()
+        self.assertEqual(response.status_code, 200)
+        images = response.entity
+        self.assertIn(admin_image, images)
+
+        members_ids = self.admin_images_behavior.get_member_ids(
+            image_id=admin_image.id_)
+        self.assertIn(tenant_A_id, members_ids)
+        self.assertIn(tenant_B_id, members_ids)
