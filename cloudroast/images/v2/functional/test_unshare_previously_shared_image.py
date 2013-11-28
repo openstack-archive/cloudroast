13a49ed103e48f305219e39a3fcc809477ca2be1
Charles Kimpolo <clmkimpo@thoughtworks.com>
2013-11-28 17:07:29 +0200

    Image API v2.0 Unshare Previously Shared Image
    Implements unshare previously shared image test:
     - added a new test scenario - unshare previously shared
     image between two users A and B.
    
    Change-Id: I9b9a4e3f29a728d341c32ac0b1fe1a4cfa957b3b

---
+++ cloudroast/images/v2/functional/test_unshare_previously_shared_image.py
@@ -0,0 +1,78 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from cloudcafe.images.common.types import ImageMemberStatus
+from cloudroast.images.fixtures import ImagesFixture
+
+
+class UnsharePreviouslySharedImageTest(ImagesFixture):
+
+    def test_unshare_previously_shared_image(self):
+        """@summary: Unshare previously shared image between two users A and B
+
+        1. Register an image as user A
+        2. Verify that user B cannot access the image
+        3. Add user B as a member of the image
+        4. Verify that user B can now access the image
+        5. Update user B membership status to 'accepted' for image
+        6. Verify that the image belongs to user B list of images
+        7. Remove user B as a member of the image
+        8. Verify that the list of members for the image is empty
+        9. Verify that the image does not belong to user B list of images
+        """
+
+        image = self.images_behavior.create_new_image()
+        tenant_B_id = self.alt_access_data.token.tenant.id_
+
+        response = self.alt_images_client.get_image(image_id=image.id_)
+        self.assertEqual(response.status_code, 404)
+
+        response = self.images_client.add_member(image_id=image.id_,
+                                                 member_id=tenant_B_id)
+        self.assertEqual(response.status_code, 200)
+        member = response.entity
+        self.assertEqual(member.member_id, tenant_B_id)
+        self.assertEqual(member.status, ImageMemberStatus.PENDING)
+
+        response = self.alt_images_client.get_image(image_id=image.id_)
+        self.assertEqual(response.status_code, 200)
+        image_A = response.entity
+        self.assertEqual(image_A, image)
+
+        response = self.alt_images_client.update_member(
+            image_id=image.id_, member_id=tenant_B_id,
+            status=ImageMemberStatus.ACCEPTED)
+        self.assertEqual(response.status_code, 200)
+        updated_member = response.entity
+        self.assertEqual(updated_member.member_id, tenant_B_id)
+        self.assertEqual(updated_member.status, ImageMemberStatus.ACCEPTED)
+
+        response = self.alt_images_client.list_images()
+        self.assertEqual(response.status_code, 200)
+        images = response.entity
+        self.assertIn(image, images)
+
+        response = self.images_client.delete_member(image_id=image.id_,
+                                                    member_id=tenant_B_id)
+        self.assertEqual(response.status_code, 204)
+
+        members_ids = self.images_behavior.get_member_ids(image_id=image.id_)
+        self.assertEqual(members_ids, [])
+
+        response = self.alt_images_client.list_images()
+        self.assertEqual(response.status_code, 200)
+        images = response.entity
+        self.assertNotIn(image, images)
