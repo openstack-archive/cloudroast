34e2eda4fd13b78be280c876dd082c05bc1eda7f
John Vrbanac <john.vrbanac@rackspace.com>
2013-12-13 09:56:42 -0600

    Removing invalid pairing and status tests
    The pairing functionality was removed from Meniscus. As a result, we are
    removing all pairing
    
    Change-Id: I376a8fe79182bad2d2c596f371cc98307553a103

--- cloudroast/meniscus/smoke/status.py
+++ cloudroast/meniscus/smoke/status.py
@@ -13,64 +13,11 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import unittest
 from cloudroast.meniscus.fixtures import StatusFixture
 
 
 class TestStatus(StatusFixture):
 
-    def setUp(self):
-        super(TestStatus, self).setUp()
-        self.pairing_resp = self.pairing_behaviors.pair_worker_from_config()
-        self.assertEqual(self.pairing_resp.status_code, 202)
-        self.pairing_info = self.pairing_resp.entity
-
-    def get_worker_entity(self):
-        worker_id = self.pairing_info.worker_id
-        resp = self.status_client.get_worker_status(worker_id)
-
-        self.assertEqual(resp.status_code, 200)
-        return resp.entity
-
-    def test_update_worker_status(self):
-        response = self.pairing_client.update_status(
-            worker_id=self.pairing_info.worker_id,
-            worker_token=self.pairing_info.worker_token,
-            status='online')
-
-        self.assertEqual(response.status_code, 200)
-
-    def test_update_worker_load_average(self):
-        resp = self.status_behaviors.update_status_from_config(
-            worker_id=self.pairing_info.worker_id,
-            worker_token=self.pairing_info.worker_token,
-            one=0.1,
-            five=0.2,
-            fifteen=0.3)
-
-        self.assertEqual(resp.status_code, 200)
-
-    def test_update_worker_disk_usage(self):
-        total, used = 100000, 30000
-        disk = [{'device': '/dev/sda1/', 'total': total, 'used': used}]
-
-        resp = self.status_behaviors.update_status_from_config(
-            worker_id=self.pairing_info.worker_id,
-            worker_token=self.pairing_info.worker_token,
-            disks=disk)
-
-        self.assertEqual(resp.status_code, 200)
-
-        worker = self.get_worker_entity()
-        partition = worker.system_info.disk_usage[0]
-        self.assertEqual(partition.total, total)
-        self.assertEqual(partition.used, used)
-
-    def test_get_worker_status(self):
-        worker = self.get_worker_entity()
-        self.assertEqual(worker.hostname, self.pairing_config.hostname)
-
-    @unittest.skip('GitHub #316')
     def test_get_worker_statuses(self):
         resp = self.status_client.get_all_worker_statuses()
         self.assertEqual(resp.status_code, 200)
