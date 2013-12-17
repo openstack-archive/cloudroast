3183611c99d4b4ae5b5fa0e9b13d43ab3d68777f
Carlos 'sn1p3r' Martinez <carlos.martinez@rackspace.com>
2013-12-17 16:04:01 -0600

    Subject: Add asseritions to obj test
    * added an assertion to check the x-container-object-count of a HEAD request
    * added an assertion to check the number of objects in the body of a container
      listing response
    
    Change-Id: I81a1ba0c166a280fd4df5ea4b48cbfad91a4e45f

--- cloudroast/objectstorage/regression/object/object_test.py
+++ cloudroast/objectstorage/regression/object/object_test.py
@@ -21,7 +21,6 @@ from cloudroast.objectstorage.fixtures import ObjectStorageFixture
 from cloudcafe.common.tools.check_dict import get_value
 
 CONTAINER_NAME = 'object_regression'
-EXPIRER_RUN_INTERVAL = 300
 
 
 class ObjectRegressionTest(ObjectStorageFixture):
@@ -32,6 +31,8 @@ class ObjectRegressionTest(ObjectStorageFixture):
         cls.base_container_name = CONTAINER_NAME
         cls.default_obj_name = cls.behaviors.VALID_OBJECT_NAME
         cls.default_obj_data = cls.behaviors.VALID_OBJECT_DATA
+        cls.expirer_run_interval = \
+            cls.objectstorage_api_config.object_expirer_run_interval
 
     @unittest.skipUnless(get_value('slow') == 'true', 'test is slow')
     def test_object_deletion_with_x_delete_at(self):
@@ -84,14 +85,30 @@ class ObjectRegressionTest(ObjectStorageFixture):
             404, resp.status_code,
             'Object should be deleted after X-Delete-At.')
 
-        sleep(EXPIRER_RUN_INTERVAL)
+        sleep(self.expirer_run_interval)
 
-        resp = self.client.list_objects(container_name)
+        get_response = self.client.list_objects(
+            container_name,
+            params={'format': 'json'})
 
         self.assertEqual(
-            '0', resp.headers.get('x-container-object-count'),
+            204, get_response.status_code,
+            'No content should be returned for the request.')
+
+        get_count = int(get_response.headers.get('x-container-object-count'))
+
+        self.assertEqual(
+            '0', get_count,
             'No objects should be listed in the container.')
 
         self.assertEqual(
-            204, resp.status_code,
-            'No content should be returned for the request.')
+            '0', len(get_response.entity),
+            'No objects should be listed in the container.')
+
+        head_response = self.client.get_container_metadata(container_name)
+
+        head_count = int(head_response.headers.get('x-container-object-count'))
+
+        self.assertEqual(
+            '0', head_count,
+            'No objects should be listed in the container.')
