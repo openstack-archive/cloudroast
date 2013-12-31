5492f2db4856c4526a9885aa795e68a2800d3d6c
Charles Kimpolo <clmkimpo@thoughtworks.com>
2013-12-31 13:49:44 +0200

    Images API v2.0 Update Image Postive Test
    Adds a new test scenario in 'test_update_image_positive.py' module.
    
    Change-Id: I86a769bd856601a619fa5c9d424ad792c4818a64

--- cloudroast/images/v2/functional/test_update_image_positive.py
+++ cloudroast/images/v2/functional/test_update_image_positive.py
@@ -17,6 +17,8 @@ limitations under the License.
 from cafe.drivers.unittest.decorators import tags
 from cloudcafe.common.tools.datagen import rand_name
 from cloudroast.images.fixtures import ImagesFixture
+from cloudcafe.images.common.types import ImageVisibility, \
+    ImageContainerFormat, ImageDiskFormat
 
 
 class TestUpdateImagePositive(ImagesFixture):
@@ -99,3 +101,42 @@ class TestUpdateImagePositive(ImagesFixture):
         for prop, prop_val in updated_image.additional_properties.iteritems():
             if prop == new_prop:
                 self.assertEqual(prop_val, updated_new_prop_value)
+
+    @tags(type='positive', regression='true')
+    def test_update_image_with_data_of_another_image(self):
+        """
+        @summary: Update image with data of another image (update ignores
+        duplication)
+
+        1. Create an image (first_image)
+        2. Create another image (second_image)
+        3. Update first_image with data of second_image
+        4. Verify that response code is 200
+        5. Verify that the response contains an image entity with
+        correct properties
+        """
+
+        first_image = self.images_behavior.create_new_image()
+        second_image = self.images_behavior.create_new_image(
+            name=rand_name("second_image"),
+            visibility=ImageVisibility.PUBLIC,
+            container_format=ImageContainerFormat.AMI,
+            disk_format=ImageDiskFormat.ISO, tags=[rand_name("tag")])
+
+        response = self.images_client.update_image(
+            image_id=first_image.id_,
+            replace={"name": second_image.name,
+                     "visibility": second_image.visibility,
+                     "container_format": second_image.container_format,
+                     "disk_format": second_image.disk_format,
+                     "tags": second_image.tags})
+        self.assertEqual(response.status_code, 200)
+
+        updated_image = response.entity
+        self.assertEqual(updated_image.id_, first_image.id_)
+        self.assertEqual(updated_image.name, second_image.name)
+        self.assertEqual(updated_image.visibility, second_image.visibility)
+        self.assertEqual(updated_image.container_format,
+                         second_image.container_format)
+        self.assertEqual(updated_image.disk_format, second_image.disk_format)
+        self.assertEqual(updated_image.tags, second_image.tags)
