7b12045403fb02d3576928cb392650a8f92fe746
Luke Wollney <luke.wollney@RACKSPACE.COM>
2014-03-06 14:14:27 -0600

    Update create delete update image tests
    * Updated create_image tests
    - Updated tests due to behavior, fixture and config changes
    - Updated variables for better clarity
    * Updated create_image_negative tests
    - Updated tests due to behavior, fixture and config changes
    - Updated method call reference
    - Split public validation test into two tests
    - Updated image_names to a list comprehension
    * Updated delete_image tests
    - Updated tests due to behavior, fixture and config changes
    * Updated delete_image_negative tests
    - Updated tests due to behavior, fixture and config changes
    * Updated delete_image_positive tests
    - Updated tests due to behavior, fixture and config changes
    - Updated method call reference
    - Updated skipped test to account for superuser config
    * Updated update_image tests
    - Updated tests due to behavior, fixture and config changes
    * Updated update_image_negative tests
    - Updated tests due to behavior, fixture and config changes
    - Updated method call reference
    - Moved the second test for public validation here
    - Updated image creations
    - Update image_names to a list comprehension
    * Updated update_image_positive tests
    - Updated tests due to behavior, fixture and config changes
    
    Change-Id: I1590dbf244c433a68d8a9f4eb75f34a2a5c6f0c0

--- cloudroast/images/v2/functional/test_delete_image_positive.py
+++ cloudroast/images/v2/functional/test_delete_image_positive.py
@@ -14,11 +14,16 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import StringIO
+import cStringIO as StringIO
+import unittest2 as unittest
 
 from cafe.drivers.unittest.decorators import tags
 from cloudcafe.images.common.types import ImageVisibility, ImageStatus
+from cloudcafe.images.config import ImagesConfig
 from cloudroast.images.fixtures import ImagesFixture
+
+images_config = ImagesConfig()
+superuser = images_config.superuser
 
 
 class TestDeleteImagePositive(ImagesFixture):
@@ -36,21 +41,23 @@ class TestDeleteImagePositive(ImagesFixture):
         6) Verify that the response code is 404
         """
 
-        member_id = self.alt_user_config.tenant_id
-        image = self.images_behavior.create_new_image(
-            visibility=ImageVisibility.PRIVATE)
+        member_id = self.alt_tenant_id
+        image = self.images_behavior.create_image_via_task()
         self.images_client.add_member(image.id_, member_id)
         response = self.images_client.delete_image(image.id_)
         self.assertEqual(response.status_code, 204)
         response = self.alt_images_client.get_image(image.id_)
         self.assertEqual(response.status_code, 404)
 
+    @unittest.skipIf(superuser is None,
+                     ('The superuser property is None, test can only be '
+                      'executed for user with correct privileges'))
     @tags(type='positive', regression='true')
     def test_delete_image_that_is_public(self):
         """
         @summary: Delete image that is public
 
-        1) Create image that is public
+        1) Create image that is public as glance superuser
         2) Delete image as alternate tenant that is not a member of the image
         3) Verify that the response code is 403
         4) Get image
@@ -59,7 +66,7 @@ class TestDeleteImagePositive(ImagesFixture):
         7) Verify that the image's status has not changed
         """
 
-        image = self.images_behavior.create_new_image(
+        image = self.images_behavior.create_image_via_task(
             visibility=ImageVisibility.PUBLIC)
         response = self.alt_images_client.delete_image(image.id_)
         self.assertEqual(response.status_code, 403)
@@ -75,16 +82,16 @@ class TestDeleteImagePositive(ImagesFixture):
         """
         @summary: Delete image with binary data
 
-        1. Create image
-        2. Upload image file
-        3. Verify that the response is 204
-        4. Get active image
-        5. Verify that the response contains an image entity
+        1) Create image
+        2) Store image file
+        3) Verify that the response is 204
+        4) Get active image
+        5) Verify that the response contains an image entity
         with correct properties
-        6. Delete active image
-        7. Verify that the response code is 204
-        8. Get deleted image
-        9. Verify that the response code is 404
+        6) Delete active image
+        7) Verify that the response code is 204
+        8) Get deleted image
+        9) Verify that the response code is 404
         """
 
         file_data = StringIO.StringIO(('*' * 1024))
