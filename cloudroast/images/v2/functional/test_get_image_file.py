09e87421034d705fcc9ea73186cdb8c385e98ac6
Luke Wollney <luke.wollney@RACKSPACE.COM>
2014-03-06 13:47:52 -0600

    Update get image file tests
    *Updated get_image_file
    -Updated tests due to behavior, fixture and config changes
    *Updated get_image_file_negative
    -Updated tests due to behavior, fixture and config changes
    
    Change-Id: I64ca5f3bb358335dc1495cb1343f421f8c32c052

--- cloudroast/images/v2/functional/test_get_image_file.py
+++ cloudroast/images/v2/functional/test_get_image_file.py
@@ -14,48 +14,53 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import StringIO
-
 from cafe.drivers.unittest.decorators import tags
-from cloudcafe.images.common.types import ImageMemberStatus
+from cloudcafe.images.common.types import (
+    ImageContainerFormat, ImageDiskFormat, ImageMemberStatus)
 from cloudroast.images.fixtures import ImagesFixture
 
 
 class TestGetImageFile(ImagesFixture):
-
-    @classmethod
-    def setUpClass(cls):
-        cls.image = cls.images_behavior.create_new_image()
 
     @tags(type='positive', regression='true')
     def test_get_image_file(self):
         """
         @summary: Get image file
 
-        1) Create import task to import new image containing data file
+        1) Create new image containing data file
         2) Get image file
         3) Verify that the response code is 200
         4) Verify that the image file contains the correct data
         """
 
-        task = self.images_behavior.create_new_task()
-        response = self.images_client.get_image_file(task.result.image_id)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content, self.test_file)
-        response = self.images_client.store_image_file(
-            image_id=self.image.id_, file_data=self.file_data)
-        self.assertEqual(response.status_code, 204)
-        response = self.images_client.get_image_file(self.image.id_)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content, self.image_data)
+        file_data = self.test_file
 
-    @tags(type='postitive', regression='true')
+        response = self.images_client.create_image(
+            container_format=ImageContainerFormat.BARE,
+            disk_format=ImageDiskFormat.RAW)
+        self.assertEqual(response.status_code, 201)
+        image = response.entity
+
+        response = self.images_client.store_image_file(image.id_, file_data)
+        self.assertEqual(response.status_code, 204)
+
+        response = self.images_client.get_image_file(image.id_)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, file_data)
+        response = self.images_client.store_image_file(
+            image_id=image.id_, file_data=file_data)
+        self.assertEqual(response.status_code, 204)
+        response = self.images_client.get_image_file(image.id_)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, file_data)
+
+    @tags(type='positive', regression='true')
     def test_get_image_file_as_a_member_of_the_image(self):
         """
         @summary: Get image file as a member of the image
 
         1. Create image as tenant
-        2. Upload image file as tenant
+        2. Store image file as tenant
         3. Verify that the response code is 204
         4. Add alternative tenant as a member of the image
         5. Verify that alternative tenant belongs to image members list
@@ -64,10 +69,10 @@ class TestGetImageFile(ImagesFixture):
         8. Verify that the response content is the same as image data
         """
 
-        alt_tenant_id = self.alt_access_data.token.tenant.id_
-        file_data = StringIO.StringIO(self.image_data)
-        image = self.images_behavior.create_new_image()
-        response = self.images_client.  store_image_file(
+        alt_tenant_id = self.alt_tenant_id
+        file_data = self.test_file
+        image = self.images_behavior.create_image_via_task()
+        response = self.images_client.store_image_file(
             image_id=image.id_, file_data=file_data)
         self.assertEqual(response.status_code, 204)
 
@@ -83,4 +88,4 @@ class TestGetImageFile(ImagesFixture):
 
         response = self.alt_images_client.get_image_file(image_id=image.id_)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content, self.image_data)
+        self.assertEqual(response.content, file_data)
