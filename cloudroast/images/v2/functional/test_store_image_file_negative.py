a4ff1a98f772fefa4f589ae19db26b47f37ab3a0
Luke Wollney <luke.wollney@RACKSPACE.COM>
2014-03-06 13:36:18 -0600

    Update store image file tests
    *Removed upload_image_file module
    -Updated and moved tests to store_image_file module
    *Removed upload_image_file_negative
    -Updated and moved tests to store_image_file_negative module
    *Updated store_image_file module
    *Added store_image_file_negative module
    
    Change-Id: I8cb20c4c15493487876c7b23649e2235e6e1ae4f

--- cloudroast/images/v2/functional/test_store_image_file_negative.py
+++ cloudroast/images/v2/functional/test_store_image_file_negative.py
@@ -14,38 +14,39 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import StringIO
+import cStringIO as StringIO
+import unittest2 as unittest
 
 from cafe.drivers.unittest.decorators import tags
-from cloudcafe.images.common.types import ImageStatus
+from cloudcafe.images.common.types import (
+    ImageContainerFormat, ImageDiskFormat, ImageStatus)
 from cloudroast.images.fixtures import ImagesFixture
 
 
-class UploadImageFileNegativeTest(ImagesFixture):
+class StoreImageFileNegativeTest(ImagesFixture):
 
     @classmethod
     def setUpClass(cls):
-        super(UploadImageFileNegativeTest, cls).setUpClass()
-        cls.image = cls.images_behavior.create_new_image()
+        super(StoreImageFileNegativeTest, cls).setUpClass()
+        response = cls.images_client.create_image(
+            container_format=ImageContainerFormat.BARE,
+            disk_format=ImageDiskFormat.RAW)
+        cls.image = response.entity
+        cls.resources.add(cls.image.id_, cls.images_client.delete_image)
         cls.data_size = 1024
         cls.file_data = StringIO.StringIO("*" * cls.data_size)
 
-    @classmethod
-    def tearDownClass(cls):
-        super(UploadImageFileNegativeTest, cls).tearDownClass()
-        cls.images_behavior.resources.release()
-
     @tags(type='negative', regression='true')
-    def test_upload_image_file_with_invalid_content_type(self):
+    def test_store_image_file_with_invalid_content_type(self):
         """
-        @summary: Upload image file with invalid content type
+        @summary: Store image file with invalid content type
 
-        1. Create an image
-        2. Verify that the image status is 'queued'
-        3. Verify that the image size is None
-        4. Verify that the image checksum is None
-        5. Upload image file with invalid content type
-        6. Verify response code is 415
+        1) Using a previously created image, verify that the image status is
+        'queued'
+        2) Verify that the image size is None
+        3) Verify that the image checksum is None
+        4) Store image file with invalid content type
+        5) Verify response code is 415
         """
 
         self.assertEqual(self.image.status, ImageStatus.QUEUED)
@@ -58,13 +59,12 @@ class UploadImageFileNegativeTest(ImagesFixture):
         self.assertEqual(response.status_code, 415)
 
     @tags(type='negative', regression='true')
-    def test_upload_image_file_with_blank_image_id(self):
+    def test_store_image_file_with_blank_image_id(self):
         """
-        @summary: Upload image file with blank image id
+        @summary: Store image file with blank image id
 
-        1. Create an image
-        2. Upload image file with blank image id
-        3. Verify response code is 404
+        1) Store image file with blank image id
+        2) Verify response code is 404
         """
 
         response = self.images_client.store_image_file(
@@ -72,35 +72,35 @@ class UploadImageFileNegativeTest(ImagesFixture):
         self.assertEqual(response.status_code, 404)
 
     @tags(type='negative', regression='true')
-    def test_upload_image_file_with_invalid_image_id(self):
+    def test_store_image_file_with_invalid_image_id(self):
         """
-        @summary: Upload image file with invalid image id
+        @summary: Store image file with invalid image id
 
-        1. Create an image
-        2. Upload image file with invalid image id
-        3. Verify response code is 404
+        1) Store image file with invalid image id
+        2) Verify response code is 404
         """
 
         response = self.images_client.store_image_file(
             image_id="invalid_id", file_data=self.file_data)
         self.assertEqual(response.status_code, 404)
 
+    @unittest.skip('Bug, Redmine #3556')
     @tags(type='negative', regression='true')
-    def test_upload_image_file_with_duplicate_data(self):
+    def test_store_image_file_with_duplicate_data(self):
         """
-        @summary: Upload image file with duplicate data
+        @summary: Store image file with duplicate data
 
-        1. Create an image
-        2. Verify that the image status is 'queued'
-        3. Verify that the image size is None
-        4. Verify that the image checksum is None
-        5. Upload image file
-        6. Verify response code is 204
-        7. Get the image
-        8. Verify that the response contains an image entity with the correct
+        1) Using a previously created image, verify that the image status is
+        'queued'
+        2) Verify that the image size is None
+        3) Verify that the image checksum is None
+        4) Store image file
+        5) Verify response code is 204
+        6) Get the image
+        7) Verify that the response contains an image entity with the correct
         properties
-        9. Try to upload image file with duplicate data
-        10. Verify response code is 409
+        8) Try to store image file with duplicate data
+        9) Verify response code is 409
         """
 
         self.assertEqual(self.image.status, ImageStatus.QUEUED)
@@ -115,14 +115,13 @@ class UploadImageFileNegativeTest(ImagesFixture):
 
         response = self.images_client.get_image(image_id=self.image.id_)
         self.assertEqual(response.status_code, 200)
-        uploaded_image = response.entity
-        self.assertEqual(uploaded_image.id_, self.image.id_)
-        self.assertEqual(uploaded_image.status, ImageStatus.ACTIVE)
-        self.assertEqual(uploaded_image.size, self.data_size)
-        self.assertIsNotNone(uploaded_image.checksum)
+        updated_image = response.entity
+        self.assertEqual(updated_image.id_, self.image.id_)
+        self.assertEqual(updated_image.status, ImageStatus.ACTIVE)
+        self.assertEqual(updated_image.size, self.data_size)
+        self.assertIsNotNone(updated_image.checksum)
 
-        duplicate_file = StringIO.StringIO("*" * self.data_size)
         response = self.images_client.store_image_file(
             image_id=self.image.id_,
-            file_data=duplicate_file)
+            file_data=file_data)
         self.assertEqual(response.status_code, 409)
