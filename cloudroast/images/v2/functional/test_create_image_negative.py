d30e8059d5f3f3d92c79626bddf05c693f0c14c5
Luke Wollney <luke.wollney@RACKSPACE.COM>
2014-03-06 14:14:27 -0600

    Update create delete update image tests
    * Updated create_image tests
    - Updated tests due to behavior, fixture and config changes
    - Updated variables for better clarity
    - Updated import order
    * Updated create_image_negative tests
    - Updated tests due to behavior, fixture and config changes
    - Updated method call reference
    - Split public validation test into two tests
    - Updated image_names to a list comprehension
    - Updated import order
    - Updated test name
    - Added skipif tags for certain tests
    - Updated create_public_image test to account for different env
    * Updated delete_image tests
    - Updated tests due to behavior, fixture and config changes
    - Updated import order
    * Updated delete_image_negative tests
    - Updated tests due to behavior, fixture and config changes
    - Updated import order
    - Updated test step wording
    * Updated delete_image_positive tests
    - Updated tests due to behavior, fixture and config changes
    - Updated method call reference
    - Updated skipped test to account for superuser config
    - Updated to check for False instead of None for superuser config
    - Updated import order
    * Updated update_image tests
    - Updated tests due to behavior, fixture and config changes
    - Updated import order
    * Updated update_image_negative tests
    - Updated tests due to behavior, fixture and config changes
    - Updated method call reference
    - Moved the second test for public validation here
    - Updated image creations
    - Update image_names to a list comprehension
    - Updated imported order
    - Updated test name
    * Updated update_image_positive tests
    - Updated tests due to behavior, fixture and config changes
    - Updated import order
    
    Change-Id: I1590dbf244c433a68d8a9f4eb75f34a2a5c6f0c0

--- cloudroast/images/v2/functional/test_create_image_negative.py
+++ cloudroast/images/v2/functional/test_create_image_negative.py
@@ -14,8 +14,18 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+import unittest2 as unittest
+
 from cafe.drivers.unittest.decorators import tags
+from cloudcafe.common.tools.datagen import rand_name
+from cloudcafe.images.common.types import (
+    ImageContainerFormat, ImageDiskFormat, ImageVisibility)
+from cloudcafe.images.config import ImagesConfig
+
 from cloudroast.images.fixtures import ImagesFixture
+
+images_config = ImagesConfig()
+allow_post_images = images_config.allow_post_images
 
 
 class TestCreateImageNegative(ImagesFixture):
@@ -44,3 +54,41 @@ class TestCreateImageNegative(ImagesFixture):
         response = self.images_client.create_image(
             container_format='unacceptable')
         self.assertEqual(response.status_code, 400)
+
+    @unittest.skipIf(allow_post_images is False,
+                     ('The allow_post_images property is False, test can only '
+                      'be executed against endpoint with correct access'))
+    @tags(type='negative', regression='true')
+    def test_create_public_image(self):
+        """
+        @summary: Create a public image by setting the visibility property to
+        public
+
+        1) Create image setting the visibility property to 'public'
+        2) List images accounting for pagination
+        3) If the allow_create_update_public_images property is true, verify
+        that the response code is 201, else verify that the response code is
+        403
+        4) If the allow_create_update_public_images property is true, verify
+        that the image is present in the list, else verify that the image is
+        not present in the list
+        """
+
+        image_name = rand_name('image')
+        allow_public_images = images_config.allow_create_update_public_images
+
+        response = self.images_client.create_image(
+            container_format=ImageContainerFormat.ARI,
+            disk_format=ImageDiskFormat.AKI, name=image_name,
+            visibility=ImageVisibility.PUBLIC)
+
+        images = self.images_behavior.list_images_pagination()
+        image_names = [image.name for image in images]
+
+        if allow_public_images is True:
+            self.assertEqual(response.status_code, 201)
+            self.assertIn(image_name, image_names)
+
+        else:
+            self.assertEqual(response.status_code, 403)
+            self.assertNotIn(image_name, image_names)
