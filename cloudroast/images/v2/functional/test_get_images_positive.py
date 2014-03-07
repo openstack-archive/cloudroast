6a643bd9e52febc85ee11ce286b29937ace8e434
Luke <luke.wollney@rackspace.com>
2014-03-07 01:17:24 -0600

    Update get images tests
    *Updated get_images module
    -Updated tests due to behavior, fixture and config changes
    -Moved image creation to setup class
    -Updated test steps
    *Updated get_images_positive module
    -Updated tests due to behavior, fixture and config changes
    -Added compare_images_glance_nova test
    -Removed check for active server status
    *Updated get_images_filter module:
    -Updated tests due to behavior, fixture and config changes
    -Updated test steps
    *Updated get_images_sort module
    -Updated tests due to behavior, fixture and config changes
    
    Change-Id: If46d6804912f5544b50ff56dbf336d603a045ed1

--- cloudroast/images/v2/functional/test_get_images_positive.py
+++ cloudroast/images/v2/functional/test_get_images_positive.py
@@ -16,17 +16,18 @@ limitations under the License.
 
 from cafe.drivers.unittest.decorators import tags
 from cloudcafe.common.tools.datagen import rand_name
-from cloudroast.images.fixtures import ImagesFixture
+from cloudcafe.images.common.types import ImageVisibility
+from cloudroast.images.fixtures import ComputeIntegrationFixture
 
 
-class TestGetImages(ImagesFixture):
+class TestGetImages(ComputeIntegrationFixture):
 
     @classmethod
     def setUpClass(cls):
         super(TestGetImages, cls).setUpClass()
         cls.image_name = rand_name('get_image')
-        cls.images = cls.images_behavior.create_new_images(
-            count=2, name=cls.image_name)
+        cls.images = cls.images_behavior.create_images_via_task(
+            count=2, image_properties={'name': cls.image_name})
 
     @tags(type='positive', regression='true')
     def test_get_images_using_marker_pagination(self):
@@ -44,7 +45,7 @@ class TestGetImages(ImagesFixture):
         7) Verify that the previously returned image is not in the current list
         """
 
-        owner = self.user_config.tenant_id
+        owner = self.tenant_id
         response = self.images_client.list_images(
             limit=1, name=self.image_name, owner=owner)
         self.assertEqual(response.status_code, 200)
@@ -73,3 +74,57 @@ class TestGetImages(ImagesFixture):
         self.assertEqual(response.status_code, 200)
         images = response.entity
         self.assertLessEqual(len(images), 50)
+
+    @tags(type='positive', regression='true')
+    def test_compare_images_glance_nova(self):
+        """
+        @summary: Compare the list of images returned via  glance and nova and
+        attempt to build a server using one of the images from the list
+
+        1) Get images with a limit of 100 images via glance
+        2) Get images with a limit of 100 images via nova
+        3) Verify that the length of the list of images from glance is the same
+        as the length of the list of images from nova
+        4) Verify that each image name in the list of images from glance is in
+        the list of images from nova
+        5) If the name matches CentOS 6.4 set image_id to that image's id to be
+        used to build a server with
+        6) Build a server and wait for it to become active using an image from
+        the list of images
+        """
+
+        image_id = None
+        test_image_name = self.images_config.test_image_name
+
+        response = self.images_client.list_images(
+            limit=100, visibility=ImageVisibility.PUBLIC)
+        self.assertEqual(response.status_code, 200)
+        glance_images = response.entity
+
+        # Remove test image from glance_images
+        for image in glance_images:
+            if image.name == test_image_name:
+                glance_images.remove(image)
+
+        glance_image_names = [image.name for image in glance_images]
+
+        response = self.compute_images_client.list_images_with_detail(
+            limit=100, image_type='base')
+        self.assertEqual(response.status_code, 200)
+        nova_images = response.entity
+
+        nova_image_names = [image.name for image in nova_images]
+
+        self.assertEqual(len(glance_images), len(nova_images))
+
+        for image in nova_images:
+            if image.name is not None and image.name.lower() == 'centos 6.4':
+                image_id = image.id
+        self.assertIsNotNone(image_id)
+
+        for image_name in glance_image_names:
+            self.assertIn(image_name, nova_image_names)
+
+        response = self.server_behaviors.create_active_server(
+            image_ref=image_id)
+        self.assertEqual(response.status_code, 200)
