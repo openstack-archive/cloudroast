3ad1cee6fd2a62497b56c5993f8f99f21c37a910
ivo5307 <ivo.vasev@RACKSPACE.COM>
2014-04-17 10:41:56 -0500

    Boot from volume V1 and V2 regression tests
        * Divided on V1 and V2 depending on boot from volume version
        * Changed year in inits from 2013 to 2014
    
    Change-Id: I543b98f5aae312fbeeb9e0839773bf4c34537201

--- cloudroast/compute/integration/volumes/boot_from_volume/v1/test_vnc_console.py
+++ cloudroast/compute/integration/volumes/boot_from_volume/v1/test_vnc_console.py
@@ -1,5 +1,5 @@
 """
-Copyright 2014 Rackspace
+Copyright 2013 Rackspace
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,3 +13,16 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
+from cloudroast.compute.fixtures import ServerFromVolumeV1Fixture
+from cloudroast.compute.functional.servers.actions.test_vnc_console import \
+    ServerVncConsoleTests
+
+
+class ServerFromVolumeV1VncConsoleTests(ServerFromVolumeV1Fixture,
+                                        ServerVncConsoleTests):
+
+    @classmethod
+    def setUpClass(cls):
+        super(ServerFromVolumeV1VncConsoleTests, cls).setUpClass()
+        cls.create_server()
