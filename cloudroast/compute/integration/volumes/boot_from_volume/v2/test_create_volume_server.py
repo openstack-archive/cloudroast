09ac77536843707805a28933d0c2dbf093a7349c
ivo5307 <ivo.vasev@RACKSPACE.COM>
2014-05-05 14:17:30 -0500

    Create Server, Delete Server and Negative Test added for Volume Instances
        * Create Instance from Image modifications
        * Create Instance from Volume modifications
        * Negative tests added
    
    Change-Id: I974301400607653939aa6a6ae6c4d932f49f7d12

---
+++ cloudroast/compute/integration/volumes/boot_from_volume/v2/test_create_volume_server.py
@@ -0,0 +1,92 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import base64
+
+from cafe.drivers.unittest.decorators import tags
+from cloudcafe.common.tools.datagen import rand_name
+from cloudcafe.compute.common.types import NovaServerStatusTypes
+from cloudcafe.blockstorage.volumes_api.v1.models import statuses
+
+from cloudroast.compute.fixtures import ServerFromVolumeV2Fixture
+from cloudroast.compute.functional.servers.test_create_server import \
+    CreateServerTest
+
+
+class CreateVolumeServerTest(object):
+
+    @tags(type='smoke', net='no')
+    def test_attach_volume_to_server_from_volume(self):
+        """Verify volume attachment works for servers from volume"""
+        # Create Attach-able Volume
+        self.volume = self.blockstorage_behavior.create_available_volume(
+            display_name='test-volume', size=self.volume_size,
+            volume_type=self.volume_type,
+            timeout=self.volume_status_timeout)
+        self.resources.add(self.volume.id_,
+                           self.blockstorage_client.delete_volume)
+        self.device = '/dev/xvdm'
+        self.mount_directory = '/mnt/test'
+        self.filesystem_type = 'ext3'
+        # Verify that a volume can be attached to a server
+        self.volume_attachments_client.attach_volume(
+            self.server.id, self.volume.id_, device=self.device)
+        self.blockstorage_behavior.wait_for_volume_status(
+            self.volume.id_, statuses.Volume.IN_USE)
+
+
+class ServerFromVolumeV2CreateServerTests(ServerFromVolumeV2Fixture,
+                                          CreateServerTest,
+                                          CreateVolumeServerTest):
+
+    @classmethod
+    def setUpClass(cls):
+        super(ServerFromVolumeV2CreateServerTests, cls).setUpClass()
+        # Initialzing instance name, metadata, files, keys, networking
+        cls.name = rand_name("server")
+        cls.metadata = {'meta_key_1': 'meta_value_1',
+                        'meta_key_2': 'meta_value_2'}
+        networks = None
+        if cls.servers_config.default_network:
+            networks = [{'uuid': cls.servers_config.default_network}]
+        cls.file_contents = 'This is a test file.'
+        if cls.file_injection_enabled:
+            cls.file_contents = 'This is a test file.'
+            files = [{'path': '/test.txt', 'contents': base64.b64encode(
+                cls.file_contents)}]
+        cls.key = cls.keypairs_client.create_keypair(rand_name("key")).entity
+        cls.resources.add(cls.key.name,
+                          cls.keypairs_client.delete_keypair)
+        # Creating block device with snapshot data inside
+        cls.block_data = cls.server_behaviors.create_block_device_mapping_v2(
+            boot_index=0, uuid=cls.image.entity.id,
+            volume_size=cls.volume_size,
+            source_type='image', destination_type='volume',
+            delete_on_termination=True)
+        # Creating Instance from Volume V2
+        cls.create_resp = cls.boot_from_volume_client.create_server(
+            name=cls.name, flavor_ref=cls.flavors_config.primary_flavor,
+            metadata=cls.metadata, personality=files, key_name=cls.key.name,
+            networks=networks, block_device_mapping_v2=cls.block_data)
+        created_server = cls.create_resp.entity
+        cls.resources.add(created_server.id,
+                          cls.servers_client.delete_server)
+        wait_response = cls.server_behaviors.wait_for_server_status(
+            created_server.id, NovaServerStatusTypes.ACTIVE)
+        wait_response.entity.admin_pass = created_server.admin_pass
+        cls.flavor = cls.flavors_client.get_flavor_details(
+            cls.flavor_ref).entity
+        cls.server = wait_response.entity
---
+++ cloudroast/compute/integration/volumes/boot_from_volume/v2/test_delete_server.py
@@ -0,0 +1,73 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from cafe.drivers.unittest.decorators import tags
+
+from cloudcafe.common.tools.datagen import rand_name
+from cloudcafe.compute.common.exceptions import ItemNotFound
+from cloudcafe.compute.common.types import NovaServerStatusTypes
+
+from cloudroast.compute.fixtures import ServerFromVolumeV2Fixture
+from cloudroast.compute.functional.servers.test_delete_server import \
+    DeleteServersTest
+
+
+class DeleteVolumeServersTest(object):
+
+    @tags(type='smoke', net='no')
+    def test_volume_is_deleted(self):
+        """We are expecting non existing volume after server deletion"""
+        with self.assertRaises(ItemNotFound):
+            self.volumes.client.delete_volume(self.volume_id)
+
+    @tags(type='smoke', net='no')
+    def test_volume_is_preserved(self):
+        """Verify Preservation of the volume succeeded after server deletion"""
+        # Creating block device with snapshot data inside
+        block_data = self.server_behaviors.create_block_device_mapping_v2(
+            boot_index=0, uuid=self.image.entity.id,
+            volume_size=self.volume_size,
+            source_type='snapshot', destination_type='volume',
+            delete_on_termination=False)
+        # Creating Instance from Volume V2
+        server_response = self.boot_from_volume_client.create_server(
+            block_device_mapping_v2=block_data,
+            flavor_ref=self.flavors_config.primary_flavor,
+            name=rand_name("server"))
+        # Verify the server reaches active status
+        server = self.server_behaviors.wait_for_server_status(
+            server_response.entity.id, NovaServerStatusTypes.ACTIVE)
+        # Delete the Instance
+        self.servers_client.delete_server(server.id)
+        self.server_behaviors.wait_for_server_to_be_deleted(server.id)
+        # Verify the Volume is preserved
+        volume_resp = self.client.get_volume_info(self.volume_id)
+        self.assertEqual(volume_resp.status_code, 202)
+        # Clean the Volume
+        result = self.volumes.behaviors.delete_volume_confirmed(self.volume_id)
+        self.assertTrue(result)
+
+
+class ServerFromVolumeV2DeleteServerTests(ServerFromVolumeV2Fixture,
+                                          DeleteServersTest,
+                                          DeleteVolumeServersTest):
+
+    @classmethod
+    def setUpClass(cls):
+        super(ServerFromVolumeV2DeleteServerTests, cls).setUpClass()
+        cls.create_server()
+        cls.resp = cls.servers_client.delete_server(cls.server.id)
+        cls.server_behaviors.wait_for_server_to_be_deleted(cls.server.id)
