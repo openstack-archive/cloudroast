d31034a38179b9d151d3d564d12617edc368f278
ivo5307 <ivo.vasev@RACKSPACE.COM>
2014-05-07 10:36:40 -0500

    Minor bug fixes in resize case boot from volume
        * Changes because of snapshot missunderstanding now both cases added
        * Fixes in resize tests
    
    Change-Id: I2fb79d1b15ef380a86638a731528eb764e59aacf

--- cloudroast/compute/integration/volumes/boot_from_volume/v2/images/test_volume_server_from_snapshot.py
+++ cloudroast/compute/integration/volumes/boot_from_volume/v2/images/test_volume_server_from_snapshot.py
@@ -28,20 +28,32 @@ class CreateVolumeServerfromSnapshotTest(ServerFromVolumeV2Fixture):
         super(CreateVolumeServerfromSnapshotTest, cls).setUpClass()
         cls.server = cls.server_behaviors.create_active_server().entity
         cls.image = cls.image_behaviors.create_active_image(cls.server.id)
+        # Create Sample Volume for Snapshot
+        cls.volume = cls.blockstorage_behavior.create_available_volume(
+            size=cls.volume_size,
+            volume_type=cls.volume_type,
+            image_ref=cls.image_ref)
+        # Creating Snapshot
+        cls.snapshot = cls.blockstorage_behavior.create_available_snapshot(
+            volume_id=cls.volume.id_)
         # Clean-up
         cls.resources.add(cls.server.id,
                           cls.servers_client.delete_server)
         cls.resources.add(cls.image.entity.id, cls.images_client.delete_image)
+        cls.resources.add(cls.volume.id_,
+                          cls.blockstorage_client.delete_volume)
+        cls.addClassCleanup(
+            cls.volumes.behaviors.delete_snapshot_confirmed, cls.snapshot.id_)
 
     @tags(type='smoke', net='no')
-    def test_create_volume_server_from_regular_snapshot(self):
+    def test_create_volume_server_from_regular_image_snapshot(self):
         """Verify the response code for a create image request is correct."""
         message = "Expected {0} to be {1}, was {2}."
         # Creating block device with snapshot data inside
         self.block_data = self.server_behaviors.create_block_device_mapping_v2(
             boot_index=0, uuid=self.image.entity.id,
             volume_size=self.volume_size,
-            source_type='snapshot', destination_type='volume',
+            source_type='image', destination_type='volume',
             delete_on_termination=True)
         # Creating Instance from Volume V2
         self.server_response = self.boot_from_volume_client.create_server(
@@ -58,3 +70,23 @@ class CreateVolumeServerfromSnapshotTest(ServerFromVolumeV2Fixture):
         self.assertEqual(self.image.entity.id, self.volume_server.image.id,
                          msg=message.format('image id', self.image.entity.id,
                                             self.volume_server.image.id))
+
+    @tags(type='smoke', net='no')
+    def test_create_volume_server_from_regular_volume_snapshot(self):
+        """Verify the response code for a create image request is correct."""
+        # Creating block device with snapshot data inside
+        self.block_data = self.server_behaviors.create_block_device_mapping_v2(
+            boot_index=0, uuid=self.snapshot.id_,
+            volume_size=self.volume_size,
+            source_type='snapshot', destination_type='volume',
+            delete_on_termination=True)
+        # Creating Instance from Volume V2
+        self.server_response = self.boot_from_volume_client.create_server(
+            block_device_mapping_v2=self.block_data,
+            flavor_ref=self.flavors_config.primary_flavor,
+            name=rand_name("server"))
+        # Verify response code is correct
+        self.assertEqual(self.server_response.status_code, 202)
+        # Verify the server reaches active status
+        self.server_behaviors.wait_for_server_status(
+            self.server_response.entity.id, NovaServerStatusTypes.ACTIVE)
