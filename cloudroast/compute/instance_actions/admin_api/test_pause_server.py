d75a2858bf89cdce23ee8476672ec74a1a98721c
Levi Blackstone <levi.blackstone@RACKSPACE.COM>
2014-07-31 15:58:51 -0500

    Updating compute admin API tests
    * Updated test_pause_server to more thoroughly test the expected behavior
    
    Change-Id: Id1065e63f1f5f91816c1a723b64c2a17ac37e784

--- cloudroast/compute/instance_actions/admin_api/test_pause_server.py
+++ cloudroast/compute/instance_actions/admin_api/test_pause_server.py
@@ -24,13 +24,46 @@ class PauseServerTests(ComputeAdminFixture):
     @classmethod
     def setUpClass(cls):
         super(PauseServerTests, cls).setUpClass()
-        cls.server = cls.server_behaviors.create_active_server().entity
+        response = cls.server_behaviors.create_active_server()
+
+        if not response.ok:
+            raise Exception(
+                "Failed to create server: "
+                "{code} - {reason}".format(code=response.status_code,
+                                           reason=response.reason))
+
+        if response.entity is None:
+            raise Exception(
+                "Response entity not set. "
+                "Response was: {0}".format(response.content))
+
+        cls.server = response.entity
         cls.resources.add(cls.server.id, cls.servers_client.delete_server)
 
     def test_pause_unpause_server(self):
-        self.admin_servers_client.pause_server(self.server.id)
+        response = self.admin_servers_client.pause_server(self.server.id)
+
+        if not response.ok:
+            raise Exception(
+                "Failed to pause server: "
+                "{code} - {reason}".format(code=response.status_code,
+                                           reason=response.reason))
+
         self.admin_server_behaviors.wait_for_server_status(
             self.server.id, ServerStates.PAUSED)
+
+        self.assertFalse(self._can_connect_to_server(),
+                         'Should not be able to connect to a paused server')
+
         self.admin_servers_client.unpause_server(self.server.id)
         self.admin_server_behaviors.wait_for_server_status(
             self.server.id, ServerStates.ACTIVE)
+
+        self.assertTrue(self._can_connect_to_server(),
+                        "Unable to connect to active server after unpausing")
+
+    @classmethod
+    def _can_connect_to_server(cls):
+        remote_client = cls.server_behaviors.get_remote_instance_client(
+            cls.server, cls.servers_config, key=cls.key.private_key)
+        return remote_client.can_authenticate()
