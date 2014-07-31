8d4461ff0932605457bdba01a665ed743f70b928
Levi Blackstone <levi.blackstone@RACKSPACE.COM>
2014-07-31 15:58:51 -0500

    Updating compute admin API tests
    * Updated test_pause_server to more thoroughly test the expected behavior
    
    Change-Id: Id1065e63f1f5f91816c1a723b64c2a17ac37e784

--- cloudroast/compute/instance_actions/admin_api/test_pause_server.py
+++ cloudroast/compute/instance_actions/admin_api/test_pause_server.py
@@ -33,9 +33,22 @@ class PauseServerTests(ComputeAdminFixture):
     def test_pause_unpause_server(self):
         """Verify that a server can be paused and then unpaused successfully"""
 
-        self.admin_servers_client.pause_server(self.server.id)
+        response = self.admin_servers_client.pause_server(self.server.id)
+        self.assertEqual(response.status_code, 202)
+
+        ip = self.server_behaviors.get_public_ip_address(self.server.id)
+        self.server_behaviors.ping_until_unreachable(ip)
+
+        # Should already be paused, but need to check the status
         self.admin_server_behaviors.wait_for_server_status(
             self.server.id, ServerStates.PAUSED)
-        self.admin_servers_client.unpause_server(self.server.id)
+
+        response = self.admin_servers_client.unpause_server(self.server.id)
+        self.assertEqual(response.status_code, 202)
+        
         self.admin_server_behaviors.wait_for_server_status(
             self.server.id, ServerStates.ACTIVE)
+
+        self.assertTrue(self.server_behaviors.get_remote_instance_client(
+            self.server, self.servers_config, key=self.key.private_key),
+            "Unable to connect to active server after unpausing")
