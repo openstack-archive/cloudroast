90737e0fc2469a463a2681ddd72f68f8be901d18
Daryl Walleck <daryl.walleck@rackspace.com>
2014-07-31 14:06:51 -0500

    Modified Compute tests to use dynamic mount points
    Change-Id: I236a771f7002ddbf79038be963d26174935b73dd

--- cloudroast/compute/instance_actions/admin_api/test_server_live_migration.py
+++ cloudroast/compute/instance_actions/admin_api/test_server_live_migration.py
@@ -55,7 +55,7 @@ class LiveMigratationServerTests(ComputeAdminFixture):
         for disk in disks:
             self._format_disk(remote_client=remote_client, disk=disk,
                               disk_format=self.disk_format_type)
-            mount_point = '/mnt/{name}'.format(name=rand_name('disk'))
+            mount_point = remote_client.generate_mountpoint()
             self._mount_disk(remote_client=remote_client, disk=disk,
                              mount_point=mount_point)
             test_directory = '{mount}/test'.format(mount=mount_point)
