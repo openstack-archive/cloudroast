9d5ad4ee7936ed1c1d0e300f3fa84d8d9c4d189f
ivo5307 <ivo.vasev@RACKSPACE.COM>
2014-08-13 08:27:48 -0500

    Snapshots glance and blockstorage changes
    * New tests for V1 boot from volume snapshot scenarios
    * Fixes in V2 snapshots scenarios
    * PEP8 fixes
    
    Change-Id: Ia640085a3ed0e3c3935b6a6d3f19402e5b62a574

--- cloudroast/compute/integration/volumes/boot_from_volume/v1/images/test_volume_server_from_snapshot.py
+++ cloudroast/compute/integration/volumes/boot_from_volume/v1/images/test_volume_server_from_snapshot.py
@@ -17,7 +17,6 @@ limitations under the License.
 from cafe.drivers.unittest.decorators import tags
 from cloudcafe.common.tools.datagen import rand_name
 from cloudcafe.compute.common.types import NovaServerStatusTypes
-from cloudcafe.compute.common.types import SourceTypes, DestinationTypes
 
 from cloudroast.compute.fixtures import ServerFromVolumeV2Fixture
 
@@ -28,18 +27,25 @@ class CreateVolumeServerfromSnapshotTest(ServerFromVolumeV2Fixture):
     def setUpClass(cls):
         super(CreateVolumeServerfromSnapshotTest, cls).setUpClass()
         cls.server = cls.server_behaviors.create_active_server().entity
-        cls.image = cls.image_behaviors.create_active_image(cls.server.id)
-        # Create Sample Volume
-        cls.volume = cls.blockstorage_behavior.create_available_volume(
+        # Creating volume for CBS snapshot scenario
+        cls.volume_sec = cls.blockstorage_behavior.create_available_volume(
             size=cls.volume_size, volume_type=cls.volume_type,
             image_ref=cls.image_ref, timeout=cls.volume_create_timeout)
         # Creating Snapshot
         cls.snapshot = cls.blockstorage_behavior.create_available_snapshot(
-            volume_id=cls.volume.id_)
+            volume_id=cls.volume_sec.id_)
+        # Creating glance image from the server
+        cls.image = cls.image_behaviors.create_active_image(cls.server.id).entity
+        # Create Volume from the Image Snapshot
+        cls.volume = cls.blockstorage_behavior.create_available_volume(
+            size=cls.volume_size, volume_type=cls.volume_type,
+            image_ref=cls.image.id, timeout=cls.volume_create_timeout)
         # Clean-up
         cls.resources.add(cls.server.id, cls.servers_client.delete_server)
-        cls.resources.add(cls.image.entity.id, cls.images_client.delete_image)
+        cls.resources.add(cls.image.id, cls.images_client.delete_image)
         cls.resources.add(cls.volume.id_,
+                          cls.blockstorage_client.delete_volume)
+        cls.resources.add(cls.volume_sec.id_,
                           cls.blockstorage_client.delete_volume)
         cls.addClassCleanup(
             cls.blockstorage_behavior.delete_snapshot_confirmed,
@@ -48,21 +54,20 @@ class CreateVolumeServerfromSnapshotTest(ServerFromVolumeV2Fixture):
     @tags(type='smoke', net='no')
     def test_create_volume_server_from_image_snapshot(self):
         """Verify the creation of volume server from image snapshot"""
-        # Creating block device with snapshot data inside
-        self.block_data = self.server_behaviors.create_block_device_mapping_v2(
-            boot_index=0,
-            uuid=self.image.entity.id,
-            volume_size=self.volume_size,
-            source_type=SourceTypes.IMAGE,
-            destination_type=DestinationTypes.VOLUME,
+        # Creating block device with volume from glance snapshot data inside
+        self.block_data = self.server_behaviors.create_block_device_mapping_v1(
+            volume_id=self.volume.id_,
+            device_name=self.images_config.primary_image_default_device,
+            size=self.volume_size,
+            type='',
             delete_on_termination=True)
-        # Creating Instance from Volume V2
-        self.server_response = self.boot_from_volume_client.create_server(
-            block_device_mapping_v2=self.block_data,
+        # Creating Instance from Volume V1
+        self.server_response = self.server_behaviors.create_active_server(
+            block_device_mapping=self.block_data,
             flavor_ref=self.flavors_config.primary_flavor,
             name=rand_name("server"))
         # Verify response code is correct
-        self.assertEqual(self.server_response.status_code, 202)
+        self.assertEqual(self.server_response.status_code, 200)
         # Verify the server reaches active status
         wait_response = self.server_behaviors.wait_for_server_status(
             self.server_response.entity.id, NovaServerStatusTypes.ACTIVE)
@@ -72,20 +77,19 @@ class CreateVolumeServerfromSnapshotTest(ServerFromVolumeV2Fixture):
     def test_create_volume_server_from_volume_snapshot(self):
         """Verify the creation of volume server from volume snapshot"""
         # Creating block device with snapshot data inside
-        self.block_data = self.server_behaviors.create_block_device_mapping_v2(
-            boot_index=0,
-            uuid=self.snapshot.id_,
-            volume_size=self.volume_size,
-            source_type=SourceTypes.SNAPSHOT,
-            destination_type=DestinationTypes.VOLUME,
+        self.block_data = self.server_behaviors.create_block_device_mapping_v1(
+            volume_id=self.snapshot.id_,
+            device_name=self.images_config.primary_image_default_device,
+            size=self.volume_size,
+            type='snap',
             delete_on_termination=True)
-        # Creating Instance from Volume V2
-        self.server_response = self.boot_from_volume_client.create_server(
-            block_device_mapping_v2=self.block_data,
+        # Creating Instance from Volume V1
+        self.server_response = self.server_behaviors.create_active_server(
+            block_device_mapping=self.block_data,
             flavor_ref=self.flavors_config.primary_flavor,
             name=rand_name("server"))
         # Verify response code is correct
-        self.assertEqual(self.server_response.status_code, 202)
+        self.assertEqual(self.server_response.status_code, 200)
         # Verify the server reaches active status
         self.server_behaviors.wait_for_server_status(
             self.server_response.entity.id, NovaServerStatusTypes.ACTIVE)
