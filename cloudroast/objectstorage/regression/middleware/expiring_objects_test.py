ab75086c183f72a57737f7176a9ec26cc17af514
josh7810 <josh.aldana@RACKSPACE.COM>
2014-08-27 16:30:44 -0500

    Tests for Expiring Objects with Unicode Chars
    * Test coverage for an expiring object
      using x_delete_after where the container
      name is Unicode characters.
    * Test coverage for an expiring object
      using x_delete_at where the container
      name is Unicode characters.
    
    Change-Id: I382a964d4489d7effe7fc2c98ff1ba29015a6fe1

--- cloudroast/objectstorage/regression/middleware/expiring_objects_test.py
+++ cloudroast/objectstorage/regression/middleware/expiring_objects_test.py
@@ -22,6 +22,8 @@ from cloudroast.objectstorage.fixtures import ObjectStorageFixture
 from cloudroast.objectstorage.generators import ObjectDatasetList
 
 CONTAINER_DESCRIPTOR = 'expiring_object_test'
+STATUS_CODE_MSG = ('{method} expected status code {expected}'
+                   ' received status code {received}')
 
 
 @DataDrivenFixture
@@ -76,3 +78,115 @@ class ExpiringObjectTest(ObjectStorageFixture):
         response = self.client.get_object(container_name, object_name)
 
         self.assertEqual(response.status_code, 404)
+
+    @data_driven_test(ObjectDatasetList())
+    def ddtest_object_creation_with_delete_after_with_container_unicode(
+            self, object_type, generate_object):
+        """
+        Scenario:
+            Create a container with a unicode name and then add
+            an expiring object to it using x_delete_after
+
+        Expected Results:
+            The object should be available until the expiration time,
+            then it should be deleted
+        """
+        delete_after = 60
+
+        container_description = unicode(u'\u262D\u2622').encode('utf-8')
+        container_name = self.create_temp_container(
+            descriptor=container_description)
+
+        object_name = self.default_obj_name
+        object_headers = {'X-Delete-After': delete_after}
+        generate_object(container_name,
+                        object_name,
+                        headers=object_headers)
+
+        object_response = self.client.get_object(container_name, object_name)
+
+        method = 'Creating Expiring Object in Unicode Container'
+        expected = 200
+        received = object_response.status_code
+
+        self.assertEqual(
+            expected,
+            received,
+            msg=STATUS_CODE_MSG.format(
+                method=method,
+                expected=expected,
+                received=str(received)))
+
+        # Sleep for period set as X-Delete-After(object expiration)
+        time.sleep(delete_after)
+
+        object_response = self.client.get_object(container_name, object_name)
+
+        method = 'GET on expired object in Unicode Container'
+        expected = 404
+        received = object_response.status_code
+
+        self.assertEqual(
+            expected,
+            received,
+            msg=STATUS_CODE_MSG.format(
+                method=method,
+                expected=expected,
+                received=str(received)))
+
+    @data_driven_test(ObjectDatasetList())
+    def ddtest_object_creation_with_delete_at_with_container_unicode(
+            self, object_type, generate_object):
+        """
+        Scenario:
+            Create a container with a unicode name and then add
+            an expiring object to it using x_delete_at.
+
+        Expected Results:
+            The object should be available until the expiration time,
+            then it should be deleted
+        """
+        start_time = calendar.timegm(time.gmtime())
+        future_time = str(int(start_time + 60))
+
+        container_description = unicode(u'\u262D\u2622').encode('utf-8')
+        container_name = self.create_temp_container(
+            descriptor=container_description)
+
+        object_name = self.default_obj_name
+        object_headers = {'X-Delete-At': future_time}
+        generate_object(container_name,
+                        object_name,
+                        headers=object_headers)
+
+        object_response = self.client.get_object(container_name, object_name)
+
+        method = 'Creating Expiring Object in Unicode Container'
+        expected = 200
+        received = object_response.status_code
+
+        self.assertEqual(
+            expected,
+            received,
+            msg=STATUS_CODE_MSG.format(
+                method=method,
+                expected=expected,
+                received=str(received)))
+
+        # Wait for object to expire using interval from config
+        time.sleep(
+            self.objectstorage_api_config.object_deletion_wait_interval)
+
+        object_response = self.client.get_object(container_name, object_name)
+
+        method = 'GET on expired object in Unicode Container'
+        expected = 404
+        received = object_response.status_code
+
+        self.assertEqual(
+            expected,
+            received,
+            msg=STATUS_CODE_MSG.format(
+                method=method,
+                expected=expected,
+                received=str(received)))
