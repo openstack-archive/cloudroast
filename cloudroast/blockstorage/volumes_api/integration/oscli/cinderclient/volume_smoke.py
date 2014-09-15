1409f68b4329f3537977870ee31294111214cdf8
Jose Idar <jose.idar@rackspace.com>
2014-09-15 16:43:51 -0500

    Blockstorage OS CLI integration test updates
     * Added snapshot and volume and volume-attachment
       smoke tests that use the cinder and nova CLI's.
     * Added integration fixtures that utilize both the
       CLI's and the API's directly.
    
    Change-Id: Id8f60aec852faf36b28cf0a949579dcc65777e68

---
+++ cloudroast/blockstorage/volumes_api/integration/oscli/cinderclient/volume_smoke.py
@@ -0,0 +1,55 @@
+from cafe.drivers.unittest.decorators import \
+    DataDrivenFixture, data_driven_test, tags
+from cloudcafe.blockstorage.datasets import BlockstorageDatasets
+
+from cloudroast.blockstorage.volumes_api.integration.oscli.fixtures \
+    import CinderCLI_IntegrationFixture
+
+
+@DataDrivenFixture
+class CinderCLI_VolumeSmoke(CinderCLI_IntegrationFixture):
+
+    @tags('quicksmoke')
+    @data_driven_test(BlockstorageDatasets.volume_types())
+    def ddtest_create_minimum_sized_volume(
+            self, volume_type_name, volume_type_id):
+        size = self.volumes.behaviors.get_configured_volume_type_property(
+            "min_size", id_=volume_type_id, name=volume_type_name)
+        name = self.random_volume_name()
+        resp = self.cinder.client.create(
+            size=size, volume_type=volume_type_id, display_name=name)
+
+        self.assertIsNotNone(
+            resp.entity, 'Could not parse cinder create response')
+
+        self.assertEquals(
+            resp.return_code, 0, "Cinder command returned an error code.")
+
+        volume = resp.entity
+
+        self.addCleanup(self.cinder.client.delete, volume.id_)
+
+        self.assertEqual(
+            int(size), int(volume.size), "Volume size reported incorrectly")
+        self.assertEqual(
+            name, volume.display_name,
+            "Volume display name reported incorrectly")
+
+    @data_driven_test(BlockstorageDatasets.volume_types())
+    def ddtest_delete_volume_by_id(self, volume_type_name, volume_type_id):
+        # setup
+        size = self.volumes.behaviors.get_configured_volume_type_property(
+            "min_size", id_=volume_type_id, name=volume_type_name)
+        volume = self.volumes.behaviors.create_available_volume(
+            size=size, volume_type=volume_type_id)
+
+        resp = self.cinder.client.delete(volume.id_)
+        self.assertEquals(
+            len(resp.standard_out), 0,
+            "Volume delete returned output on standard error")
+        self.addCleanup(self.cinder.client.delete, volume.id_)
+
+        self.assertTrue(
+            self.volumes.behaviors.delete_volume_confirmed(
+                volume.id_, size, ),
+            "Could not confirm that volume {0} was deleted".format(volume.id_))
---
+++ cloudroast/blockstorage/volumes_api/integration/oscli/novaclient/smoke.py
@@ -0,0 +1,60 @@
+from cafe.drivers.unittest.decorators import \
+    data_driven_test, DataDrivenFixture, tags
+from cloudcafe.blockstorage.datasets import BlockstorageDatasets
+
+from cloudroast.blockstorage.volumes_api.integration.oscli.fixtures \
+    import NovaCLI_IntegrationFixture
+
+volume_type_complete_dataset = BlockstorageDatasets.volume_types()
+
+
+@DataDrivenFixture
+class NovaCLI_IntegrationSmoke(NovaCLI_IntegrationFixture):
+
+    @classmethod
+    def setUpClass(cls):
+        super(NovaCLI_IntegrationSmoke, cls).setUpClass()
+
+        # Create test server
+        cls.test_server = cls.new_server()
+
+    @tags('quicksmoke')
+    @data_driven_test(volume_type_complete_dataset)
+    def ddtest_create_minimum_sized_volume(
+            self, volume_type_name, volume_type_id):
+        size = self.volumes.behaviors.get_configured_volume_type_property(
+            "min_size", id_=volume_type_id, name=volume_type_name)
+        volume = self.new_volume(size=size, volume_type=volume_type_id)
+        assert int(volume.size) == int(size), "Volume was the wrong size"
+
+    @data_driven_test(volume_type_complete_dataset)
+    def ddtest_create_maximum_sized_volume(
+            self, volume_type_name, volume_type_id):
+        size = self.volumes.behaviors.get_configured_volume_type_property(
+            "max_size", id_=volume_type_id, name=volume_type_name)
+        volume = self.new_volume(size=size, volume_type=volume_type_id)
+        assert int(volume.size) == int(size), "Volume was the wrong size"
+
+    @data_driven_test(volume_type_complete_dataset)
+    def ddtest_auto_attach(self, volume_type_name, volume_type_id):
+        volume = self.new_volume(volume_type=volume_type_id)
+        self.verified_volume_attach(self.test_server.id_, volume.id_)
+
+    @data_driven_test(volume_type_complete_dataset)
+    def ddtest_detach(self, volume_type_name, volume_type_id):
+        # Create volume and attach it to the test server
+        volume = self.new_volume(volume_type=volume_type_id)
+        self.verified_volume_attach(
+            self.test_server.id_, volume.id_, add_cleanup=True)
+
+        # Issue detach
+        resp = self.nova.client.volume_detach(self.test_server.id_, volume.id_)
+        assert resp.return_code == 0, (
+            'Unable to detach volume, return code was {0}'.format(
+                resp.return_code))
+
+    @data_driven_test(volume_type_complete_dataset)
+    def ddtest_delete(self, volume_type_name, volume_type_id):
+        volume = self.new_volume(volume_type=volume_type_id)
+        resp = self.nova.client.volume_delete(volume.id_)
+        assert resp.return_code == 0, 'Unable to delete volume'
