e554352618ce1d5489af70a47792d9027d925ee1
Carlos 'Sn1p3r' Martinez <carlos.martinez@rackspace.com>
2014-09-15 16:13:03 -0500

    Add Container Quota Regression Tests
    * added a regression test for container quota count
    * added a regression test for container quota bytes
    * added a test for bad quota headers
    * added a comment for the sleep in the bytes header test
    
    Change-Id: I3effc5417a4784881f8f303365dc90bef34fbf26

--- cloudroast/objectstorage/regression/features/container_quota_test.py
+++ cloudroast/objectstorage/regression/features/container_quota_test.py
@@ -14,7 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 from cloudroast.objectstorage.fixtures import ObjectStorageFixture
+from time import sleep
 
+CONTENT_TYPE_TEXT = 'text/plain; charset=UTF-8'
 CONTAINER_NAME = 'container_quota_test_container'
 
 
@@ -63,8 +65,8 @@ class ContainerQuotaTest(ObjectStorageFixture):
         'X-Container-Meta-Quota-Count' header and it is set
         properly
         """
-        num_objects = 10
-        hdrs = {'X-Container-Meta-Quota-Count': num_objects}
+        obj_limit = '10'
+        hdrs = {'X-Container-Meta-Quota-Count': obj_limit}
         self.client.create_container(CONTAINER_NAME, headers=hdrs)
         response = self.client.get_container_metadata(CONTAINER_NAME)
 
@@ -74,7 +76,7 @@ class ContainerQuotaTest(ObjectStorageFixture):
                  " header returned status code {0}").format(
                 response.status_code))
 
-        expected = str(num_objects)
+        expected = obj_limit
         received = response.headers.get('x-container-meta-quota-count')
 
         self.assertEqual(
@@ -82,3 +84,206 @@ class ContainerQuotaTest(ObjectStorageFixture):
             received,
             msg=("x-container-meta-quota-count header was not set properly."
                  " expected: {0} received: {1}").format(expected, received))
+
+    def test_bad_container_quota_headers(self):
+        """
+        Scenario:
+        Set bad quota headers
+
+        Expected Results:
+        bad request status is returned
+        """
+        # create a container and set a bad quota count header
+        hdrs = {'X-Container-Meta-Quota-Count': 'drok'}
+        response = self.client.create_container(CONTAINER_NAME, headers=hdrs)
+
+        self.assertEqual(response.status_code, 400)
+
+        # create a container and set a bad quota bytes header
+        hdrs = {'X-Container-Meta-Quota-Bytes': 'drok'}
+        response = self.client.create_container(CONTAINER_NAME, headers=hdrs)
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_container_quota_count(self):
+        """
+        Scenario:
+        'X-Container-Meta-Quota-Count' is set on a container.
+        objects are created in the container == the container limit.
+        an object is created to exceed the container limit.
+
+        Expected Results:
+        objects created <= the container limit function properly reflecting
+        the correct count in the 'x-container-object-count' header and the
+        actual container listing.
+
+        objects created > the container limit will send the correct error
+        status code. the object will not be created and the
+        'x-container-object-count' will not be incremented.
+        """
+        # create a container and set the obj limit
+        obj_limit = 10
+        hdrs = {'X-Container-Meta-Quota-Count': str(obj_limit)}
+        self.client.create_container(CONTAINER_NAME, headers=hdrs)
+
+        # create objects == the set limit
+        headers = \
+            {'Content-Length': str(len(self.behaviors.VALID_OBJECT_DATA)),
+             'Content-Type': CONTENT_TYPE_TEXT}
+
+        for num in range(obj_limit):
+            response = self.client.create_object(
+                CONTAINER_NAME,
+                "obj_{0}".format(str(num)),
+                headers=headers,
+                data=self.behaviors.VALID_OBJECT_DATA)
+            self.assertTrue(
+                response.status_code,
+                msg=("obj_{0} was not created. obj creation returned"
+                     " status code {1}").format(str(num),
+                                                str(response.status_code)))
+
+        # check the obj count in the header and body of the container listing
+        response = self.client.list_objects(CONTAINER_NAME)
+
+        self.assertEqual(
+            str(obj_limit),
+            response.headers.get('x-container-object-count'),
+            msg="x-container-object-count header expected: {0} received"
+                "{1}".format(
+                str(obj_limit),
+                str(response.headers.get('x-container-object-count'))))
+
+        obj_count = 0
+        if response.entity:
+            obj_count = len(response.entity)
+
+        self.assertEqual(
+            obj_limit,
+            obj_count,
+            msg="container list expected: {0} received {1}".format(
+                str(obj_limit),
+                str(obj_count)))
+
+        # create an object to exceed the container obj limit
+        response = self.client.create_object(
+            CONTAINER_NAME,
+            "obj_{0}".format(str(obj_limit)),
+            headers=headers,
+            data=self.behaviors.VALID_OBJECT_DATA)
+
+        # check to see that the obj creation returns the correct status code
+        self.assertEqual(
+            413,
+            response.status_code,
+            msg="obj creation expected status code: {0} received {1}".format(
+                '413',
+                str(response.status_code)))
+
+        # check the obj count in the header and body of the container listing
+        response = self.client.list_objects(CONTAINER_NAME)
+
+        self.assertEqual(
+            str(obj_limit),
+            response.headers.get('x-container-object-count'),
+            msg="x-container-object-count header expected: {0} received"
+                "{1}".format(
+                str(obj_limit),
+                str(response.headers.get('x-container-object-count'))))
+
+        obj_count = 0
+        if response.entity:
+            obj_count = len(response.entity)
+
+        self.assertEqual(
+            obj_limit,
+            obj_count,
+            msg="container list expected: {0} received {1}".format(
+                str(obj_limit),
+                str(obj_count)))
+
+    def test_container_quota_bytes(self):
+        """
+        Scenario:
+        'X-Container-Meta-Quota-Bytes' is set on a container.
+        an obj is created in the container == the byte limit of the container.
+        an object is created to exceed the byte limit of the container.
+
+        Expected Results:
+        objects created <= the container limit function properly reflecting
+        the correct count in the 'x-container-object-count' header and the
+        actual container listing.
+
+        objects created > the container byte limit will send the correct error
+        status code. the object will not be created and the
+        'x-container-object-count' will not be incremented.
+        'x-container-object-bytes' will not be incremented.
+        """
+        data_string = "0123456789"
+
+        # create a container and set the obj limit
+        hdrs = {'X-Container-Meta-Quota-Bytes': str(len(data_string))}
+        self.client.create_container(CONTAINER_NAME, headers=hdrs)
+
+        # create object == the set limit
+        headers = \
+            {'Content-Length': str(len(data_string)),
+             'Content-Type': CONTENT_TYPE_TEXT}
+
+        response = self.client.create_object(
+            CONTAINER_NAME,
+            "obj_1",
+            headers=headers,
+            data=data_string)
+
+        # check to see that the obj creation returns the correct status code
+        self.assertTrue(
+            response.ok,
+            msg=("obj_1 was not created. obj creation returned"
+                 " status code {0}").format(str(response.status_code)))
+
+        # this sleep is for CF eventual consistency
+        sleep(60)
+
+        response = self.client.create_object(
+            CONTAINER_NAME,
+            "obj_2",
+            headers=headers,
+            data=data_string)
+
+        # check to see that the obj creation returns the correct failure code
+        self.assertEqual(
+            413,
+            response.status_code,
+            msg="obj creation expected status code: {0} received {1}".format(
+                '413',
+                str(response.status_code)))
+
+        response = self.client.list_objects(CONTAINER_NAME)
+
+        self.assertEqual(
+            '1',
+            response.headers.get('x-container-object-count'),
+            msg="x-container-object-count header expected: {0} received"
+                "{1}".format(
+                '1',
+                str(response.headers.get('x-container-object-count'))))
+
+        obj_count = 0
+        if response.entity:
+            obj_count = len(response.entity)
+
+        self.assertEqual(
+            1,
+            obj_count,
+            msg="container list expected: {0} received {1}".format(
+                '1',
+                str(obj_count)))
+
+        self.assertEqual(
+            str(len(data_string)),
+            response.headers.get('x-container-bytes-used'),
+            msg="x-container-bytes-used header expected: {0} received"
+                "{1}".format(
+                str(len(data_string)),
+                str(response.headers.get('x-container-object-count'))))
