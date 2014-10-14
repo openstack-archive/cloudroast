2f755731693e3615a1afb51a618f388eb4c305b1
Leonardo Maycotte <leonardo.maycotte@rackspace.com>
2014-10-14 16:57:14 -0500

    Subnet create tests and minor network test updates
    - Adding subnets POST tests
    - Updating fixture assert methods to check partial
      names (starts with)
    - Updating network tests setup to instantiate the expected
      network object
    - Updating network test data admin_state_up param
    - Updating network tests negative assertions
    - Updating networks and subnets data to fixture
    - Adding create_test_network and add_subnet_to_network
      fixture methods for tests set up methods
    - Adding to fixture get data methods for dns_nameservers,
      allocation_pools and host_routes
    
    Change-Id: I9462ffc907041af0f86046c80ab97c5872ef73ee

--- cloudroast/networking/networks/fixtures.py
+++ cloudroast/networking/networks/fixtures.py
@@ -19,6 +19,12 @@ import netaddr
 from cafe.drivers.unittest.fixtures import BaseTestFixture
 from cloudcafe.common.resources import ResourcePool
 from cloudcafe.compute.composites import ComputeComposite
+from cloudcafe.networking.networks.common.constants \
+    import NeutronResponseCodes
+from cloudcafe.networking.networks.common.models.response.network \
+    import Network
+from cloudcafe.networking.networks.common.models.response.subnet \
+    import Subnet
 from cloudcafe.networking.networks.composites import NetworkingComposite
 from cloudcafe.networking.networks.config import NetworkingSecondUserConfig
 
@@ -76,7 +82,7 @@ class NetworkingFixture(BaseTestFixture):
         """
 
         cls.fixture_log.info('networkingCleanUp ....')
-        if not cls.ports.config.keep_resources:
+        if not cls.ports.config.keep_resources and cls.delete_ports:
             if cls.ports.config.keep_resources_on_failure:
                 cls.fixture_log.info('Keeping failed ports...')
                 for failed_port in cls.failed_ports:
@@ -85,7 +91,7 @@ class NetworkingFixture(BaseTestFixture):
             cls.fixture_log.info('Deleting ports...')
             cls.ports.behaviors.clean_ports(ports_list=cls.delete_ports)
 
-        if not cls.subnets.config.keep_resources:
+        if not cls.subnets.config.keep_resources and cls.delete_subnets:
             if cls.subnets.config.keep_resources_on_failure:
                 cls.fixture_log.info('Keeping failed subnets...')
                 for failed_subnet in cls.failed_subnets:
@@ -95,7 +101,7 @@ class NetworkingFixture(BaseTestFixture):
             cls.subnets.behaviors.clean_subnets(
                 subnets_list=cls.delete_subnets)
 
-        if not cls.networks.config.keep_resources:
+        if not cls.networks.config.keep_resources and cls.delete_networks:
             if cls.networks.config.keep_resources_on_failure:
                 cls.fixture_log.info('Keeping failed networks...')
                 for failed_network in cls.failed_networks:
@@ -105,8 +111,79 @@ class NetworkingFixture(BaseTestFixture):
             cls.networks.behaviors.clean_networks(
                 networks_list=cls.delete_networks)
 
+    def create_test_network(self, expected_network, set_up=True):
+        """
+        @summary: creating a test network
+        @param expected_network: network object with expected params
+        @type expected_network: models.response.network.Network
+        @param set_up: flag for raising an assertClassSetupFailure if network
+            is not created as expected
+        @type setup: bool
+        @return: network entity or None if set_up flag set to False
+        @rtype: models.response.network.Network or None
+        """
+        network = None
+        resp = self.networks.behaviors.create_network(
+            name=expected_network.name)
+        if (resp.response.status_code == NeutronResponseCodes.CREATE_NETWORK
+                and resp.response.entity):
+            network = resp.response.entity
+            self.delete_networks.append(network.id)
+
+            # Check the Network response
+            self.assertNetworkResponse(expected_network=expected_network,
+                                       network=network, check_exact_name=False)
+        elif set_up:
+            msg = ('Unable to create test network status code {0}, '
+                   'failures:{1}'.format(resp.response.status_code,
+                                         resp.failures))
+            self.assertClassSetupFailure(msg)
+        return network
+
+    def add_subnet_to_network(self, expected_subnet, set_up=True):
+        """
+        @summary: creating and adding a test subnet to a test network
+        @param expected_subnet: subnet object with expected params
+        @type expected_subnet: models.response.subnet.Subnet
+        @param set_up: flag for raising an assertClassSetupFailure if subnet
+            is not created as expected
+        @type setup: bool
+        @return: subnet entity or None if set_up flag set to False
+        @rtype: models.response.subnet.Subnet or None
+        """
+        subnet = None
+        resp = self.subnets.behaviors.create_subnet(
+            network_id=expected_subnet.network_id,
+            name=expected_subnet.name,
+            ip_version=expected_subnet.ip_version,
+            cidr=expected_subnet.cidr,
+            allocation_pools=expected_subnet.allocation_pools,
+            raise_exception=False)
+        if (resp.response.status_code == NeutronResponseCodes.CREATE_SUBNET
+                and resp.response.entity):
+            subnet = resp.response.entity
+            self.delete_subnets.append(subnet.id)
+
+            if expected_subnet.ip_version == 6:
+                # Need to format IPv6 allocation pools response for assertion
+                subnet.allocation_pools = (
+                    self.subnets.behaviors.format_allocation_pools(
+                    subnet.allocation_pools))
+
+            # Check the created subnet is as expected
+            self.assertSubnetResponse(
+                expected_subnet=expected_subnet, subnet=subnet,
+                check_exact_name=False)
+        elif set_up:
+            msg = ('Unable to create test IPv{0} subnet {1} status code {2}, '
+                'failures:{3}'.format(
+                expected_subnet.ip_version, expected_subnet.name,
+                resp.response.status_code, resp.failures))
+            self.assertClassSetupFailure(msg)
+        return subnet
+
     def assertNegativeResponse(self, resp, status_code, msg, delete_list=None,
-                               entity=None):
+                               entity=None, error_type=None):
         """
         @summary: negative test response assertion
         @param resp: networking response
@@ -119,10 +196,12 @@ class NetworkingFixture(BaseTestFixture):
         @type msg: string
         @param entity: if entity should be None it should not be set
         @type entity: None or expected entity
+        @param error_type: Neutron error type at common/constants
+        @type error_type: string
         """
         # Just in case there is a resource that should be deleted
         if (delete_list is not None and resp.response.entity
-            and hasattr(resp.response.entity, 'id')):
+                and hasattr(resp.response.entity, 'id')):
             delete_list.append(resp.response.entity.id)
         message = ('{msg}: unexpected HTTP response {resp_status} instead of '
                    'the expected {status}'.format(
@@ -140,22 +219,35 @@ class NetworkingFixture(BaseTestFixture):
         else:
             self.assertFalse(resp.response.entity, entity_msg)
 
-    def assertNetworkResponse(self, expected_network, network):
+        # Neutron error type check
+        if error_type:
+            error_msg_check = error_type in resp.failures[0]
+            msg = ('Expected {0} error type in failure msg: {1}').format(
+                error_type, resp.failures[0])
+            self.assertTrue(error_msg_check, msg)
+
+    def assertNetworkResponse(self, expected_network, network,
+                              check_exact_name=True):
         """
         @summary: compares two network entity objects
         """
         self.fixture_log.info('asserting Network response ...')
-        print network
         msg = 'Expected {0} instead of {1}'
+        if check_exact_name:
+            self.assertEqual(
+                expected_network.name, network.name,
+                msg.format(expected_network.name, network.name))
+        else:
+            start_name_msg = 'Expected {0} name to start with {1}'.format(
+                network.name, expected_network.name)
+            self.assertTrue(network.name.startswith(expected_network.name),
+                            start_name_msg)
         self.assertEqual(
             expected_network.status, network.status,
             msg.format(expected_network.status, network.status))
         self.assertEqual(
             expected_network.subnets, network.subnets,
             msg.format(expected_network.subnets, network.subnets))
-        self.assertEqual(
-            expected_network.name, network.name,
-            msg.format(expected_network.name, network.name))
         self.assertEqual(
             expected_network.admin_state_up, network.admin_state_up,
             msg.format(expected_network.admin_state_up,
@@ -174,17 +266,22 @@ class NetworkingFixture(BaseTestFixture):
                 network.kwargs)
             self.assertFalse(network.kwargs, msg)
 
-    def assertSubnetResponse(self, expected_subnet, subnet):
+    def assertSubnetResponse(self, expected_subnet, subnet,
+                             check_exact_name=True):
         """
         @summary: compares two network entity objects
         """
         self.fixture_log.info('asserting Subnet response ...')
-        print expected_subnet
-        print subnet
         msg = 'Expected {0} instead of {1}'
-        self.assertEqual(
-            expected_subnet.name, subnet.name,
-            msg.format(expected_subnet.name, subnet.name))
+        if check_exact_name:
+            self.assertEqual(
+                expected_subnet.name, subnet.name,
+                msg.format(expected_subnet.name, subnet.name))
+        else:
+            start_name_msg = 'Expected {0} name to start with {1}'.format(
+                subnet.name, expected_subnet.name)
+            self.assertTrue(subnet.name.startswith(expected_subnet.name),
+                            start_name_msg)
         self.assertEqual(
             expected_subnet.enable_dhcp, subnet.enable_dhcp,
             msg.format(expected_subnet.enable_dhcp, subnet.enable_dhcp))
@@ -232,11 +329,118 @@ class NetworkingAPIFixture(NetworkingFixture):
     def setUpClass(cls):
         super(NetworkingAPIFixture, cls).setUpClass()
 
+        # Data for creating networks and asserting responses
+        cls.network_data = dict(
+            status='ACTIVE', subnets=[],
+            name='test_api_net', admin_state_up=True,
+            tenant_id=cls.net.networking_auth_composite().tenant_id,
+            shared=False)
+
+        # Data for creating subnets and asserting responses
+        cls.subnet_data = dict(
+            name='test_api_subnet',
+            tenant_id=cls.net.networking_auth_composite().tenant_id,
+            enable_dhcp=None, dns_nameservers=[], gateway_ip=None,
+            host_routes=[])
+
         # Getting second user data for negative testing
         cls.alt_user = NetworkingSecondUserConfig()
 
         # Using the networkingCleanup method
         cls.addClassCleanup(cls.networkingCleanUp)
+
+    @classmethod
+    def get_expected_network_data(cls):
+        """Network object with default data"""
+        expected_network = Network(**cls.network_data)
+        return expected_network
+
+    @classmethod
+    def get_expected_ipv4_subnet_data(cls):
+        """Subnet object with default data"""
+        expected_subnet = Subnet(**cls.subnet_data)
+        expected_subnet.ip_version = 4
+        cidr = cls.subnets.behaviors.create_ipv4_cidr()
+        expected_subnet.cidr = cidr
+        allocation_pool = cls.subnets.behaviors.get_allocation_pool(cidr)
+        expected_subnet.allocation_pools = [allocation_pool]
+        return expected_subnet
+
+    @classmethod
+    def get_expected_ipv6_subnet_data(cls):
+        """Subnet object with default data"""
+        expected_subnet = Subnet(**cls.subnet_data)
+        expected_subnet.ip_version = 6
+        cidr = cls.subnets.behaviors.create_ipv6_cidr()
+        expected_subnet.cidr = cidr
+        allocation_pool = cls.subnets.behaviors.get_allocation_pool(cidr)
+        expected_subnet.allocation_pools = [allocation_pool]
+        return expected_subnet
+
+    def get_ipv4_dns_nameservers_data(self):
+        """IPv4 dns nameservers test data (quota is 2)"""
+                # IPv4 dns_nameservers test data
+        ipv4_dns_nameservers = ['0.0.0.0', '0.0.1.0']
+        return ipv4_dns_nameservers
+
+    def get_ipv6_dns_nameservers_data(self):
+        """IPv6 dns nameservers test data (quota is 2)"""
+        dns_cidr = self.subnets.behaviors.create_ipv6_cidr()
+        dns_1 = self.subnets.behaviors.get_ip(cidr=dns_cidr, increment=1)
+        dns_2 = self.subnets.behaviors.get_ip(cidr=dns_cidr, increment=2)
+        ipv6_dns_nameservers = [dns_1, dns_2]
+        return ipv6_dns_nameservers
+
+    def get_allocation_pools_data(self, cidr, start_increment,
+                                  ip_range, interval, n):
+        """Generates allocation pools subnet data"""
+        allocation_pools = []
+        for _ in range(n):
+            end_increment = start_increment + ip_range
+            allocation_pool = self.subnets.behaviors.get_allocation_pool(
+                cidr=cidr, start_increment=start_increment,
+                end_increment=end_increment)
+            allocation_pools.append(allocation_pool)
+            start_increment = end_increment + interval
+        return allocation_pools
+
+    def get_ipv4_host_route_data(self, num=1):
+        """IPv4 host routes test data (num host routes, quota is 3)"""
+        host_route_cidrv6 = self.subnets.behaviors.create_ipv6_cidr()
+        host_route_cidrv4 = self.subnets.behaviors.create_ipv4_cidr()
+
+        # Minus 1 since it is added afterwards with an IPv6 nexthop
+        num -= 1
+        host_routes = []
+        if num:
+            ipv4_ips = self.subnets.behaviors.get_ips(host_route_cidrv4, num)
+            ipv4_host_routes = self.subnets.behaviors.get_host_routes(
+                cidr=host_route_cidrv4, ips=ipv4_ips)
+            host_routes.extend(ipv4_host_routes)
+        nexthop_ipv6 = self.subnets.behaviors.get_ip(host_route_cidrv6)
+        ipv6_host_route = self.subnets.behaviors.get_host_routes(
+            cidr=host_route_cidrv6, ips=[nexthop_ipv6])
+        host_routes.extend(ipv6_host_route)
+        return host_routes
+
+    def get_ipv6_host_route_data(self, num=1):
+        """IPv6 host routes test data (n host routes, quota is 3)"""
+        host_route_cidrv6 = self.subnets.behaviors.create_ipv6_cidr()
+        host_route_cidrv4 = self.subnets.behaviors.create_ipv4_cidr()
+
+        # Minus 1 since it is added afterwards with an IPv4 nexthop
+        num -= 1
+        host_routes = []
+        if num:
+            ipv6_ips = self.subnets.behaviors.get_ips(host_route_cidrv6, num)
+            ipv6_host_routes = self.subnets.behaviors.get_host_routes(
+                cidr=host_route_cidrv6, ips=ipv6_ips)
+            host_routes.extend(ipv6_host_routes)
+        nexthop_ipv4 = self.subnets.behaviors.get_ip(host_route_cidrv4)
+        ipv4_host_route = self.subnets.behaviors.get_host_routes(
+            cidr=host_route_cidrv4, ips=[nexthop_ipv4])
+        host_routes.extend(ipv4_host_route)
+        return host_routes
 
 
 class NetworkingComputeFixture(NetworkingFixture):
