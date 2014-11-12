b71d0f2f62ece32f4d96cad87efd66aa090326c2
Leonardo Maycotte <leonardo.maycotte@rackspace.com>
2014-11-12 12:21:06 -0600

    adding network get tests
    Change-Id: I128dc32eba803df21f17bc0d351d562ed0003bdd

---
+++ cloudroast/networking/networks/api/networks/test_networks_get.py
@@ -0,0 +1,137 @@
+"""
+Copyright 2014 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from cafe.drivers.unittest.decorators import tags
+from cloudcafe.networking.networks.common.constants \
+    import NeutronResponseCodes, NeutronErrorTypes
+from cloudroast.networking.networks.fixtures import NetworkingAPIFixture
+
+
+class NetworkGetTest(NetworkingAPIFixture):
+
+    @classmethod
+    def setUpClass(cls):
+        """Setting up test data"""
+        super(NetworkGetTest, cls).setUpClass()
+
+        # Setting up the network
+        cls.expected_network = cls.get_expected_network_data()
+        cls.expected_network.name = 'test_network_get'
+
+        # Setting up the Subnets data
+        cls.expected_ipv4_subnet = cls.get_expected_ipv4_subnet_data()
+        cls.expected_ipv6_subnet = cls.get_expected_ipv6_subnet_data()
+
+        # Setting up the Port data
+        cls.expected_ipv4_port = cls.get_expected_port_data()
+        cls.expected_ipv6_port = cls.get_expected_port_data()
+
+    def setUp(self):
+        self.network = self.create_test_network(self.expected_network)
+        self.expected_network.id = self.network.id
+
+    def tearDown(self):
+        self.networkingCleanUp()
+
+    @tags(type='smoke', rbac='observer')
+    def test_network_list(self):
+        """
+        @summary: Get networks test
+        """
+        expected_network = self.network
+
+        # Get network
+        resp = self.networks.behaviors.list_networks()
+
+        # Fail the test if any failure is found
+        self.assertFalse(resp.failures)
+
+    @tags(type='smoke', rbac='observer')
+    def test_network_get(self):
+        """
+        @summary: Get network test
+        """
+        expected_network = self.network
+
+        # Get network
+        resp = self.networks.behaviors.get_network(
+            network_id=expected_network.id)
+
+        # Fail the test if any failure is found
+        self.assertFalse(resp.failures)
+        network = resp.response.entity
+
+        # Check the network response
+        self.assertNetworkResponse(expected_network, network)
+
+    @tags(type='smoke', rbac='observer')
+    def test_network_w_subnets_get(self):
+        """
+        @summary: Get network with subnets test
+        """
+        expected_network = self.network
+
+        # Creating subnets
+        self.expected_ipv4_subnet.network_id = expected_network.id
+        self.expected_ipv6_subnet.network_id = expected_network.id
+        ipv4_subnet = self.add_subnet_to_network(self.expected_ipv4_subnet)
+        ipv6_subnet = self.add_subnet_to_network(self.expected_ipv6_subnet)
+        expected_network.subnets.append(ipv4_subnet.id)
+        expected_network.subnets.append(ipv6_subnet.id)
+
+        # Get network
+        resp = self.networks.behaviors.get_network(
+            network_id=expected_network.id)
+
+        # Fail the test if any failure is found
+        self.assertFalse(resp.failures)
+        network = resp.response.entity
+
+        # Check the network response
+        expected_network.subnets.sort()
+        network.subnets.sort()
+        self.assertNetworkResponse(expected_network, network)
+
+    @tags(type='smoke', rbac='observer')
+    def test_network_w_ports_get(self):
+        """
+        @summary: Get network with ports test
+        """
+        expected_network = self.network
+
+        # Creating subnets and ports
+        self.expected_ipv4_subnet.network_id = expected_network.id
+        self.expected_ipv6_subnet.network_id = expected_network.id
+        self.expected_ipv4_port.network_id = expected_network.id
+        self.expected_ipv6_port.network_id = expected_network.id
+        ipv4_subnet = self.add_subnet_to_network(self.expected_ipv4_subnet)
+        ipv6_subnet = self.add_subnet_to_network(self.expected_ipv6_subnet)
+        ipv4_port = self.add_port_to_network(self.expected_ipv4_port)
+        ipv6_port = self.add_port_to_network(self.expected_ipv6_port)
+        expected_network.subnets.append(ipv4_subnet.id)
+        expected_network.subnets.append(ipv6_subnet.id)
+
+        # Get network
+        resp = self.networks.behaviors.get_network(
+            network_id=expected_network.id)
+
+        # Fail the test if any failure is found
+        self.assertFalse(resp.failures)
+        network = resp.response.entity
+
+        # Check the network response
+        expected_network.subnets.sort()
+        network.subnets.sort()
+        self.assertNetworkResponse(expected_network, network)
