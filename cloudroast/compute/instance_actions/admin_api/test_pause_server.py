fd260647843c5ca5cfec0888f833758f86330552
ivo5307 <ivo.vasev@RACKSPACE.COM>
2014-11-24 11:07:14 -0600

    Pause Test cases for Boot from Volume
    * Added new test case for reboot while instance is paused
    * Refactored address for ssh from only 4 used to use config option
    * Fixed typo in tags
    * Fixed review comments - extracted ip parse function and typos
    * Added ping check after reboot
    * Fixed minor pep8 issue
    
    Change-Id: I97c489f48a79abebf3b7557ccec963f47cf8a149

--- cloudroast/compute/instance_actions/admin_api/test_pause_server.py
+++ cloudroast/compute/instance_actions/admin_api/test_pause_server.py
@@ -1,5 +1,5 @@
 """
-Copyright 2013 Rackspace
+Copyright 2014 Rackspace
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,31 +14,28 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import unittest
-
 from cafe.drivers.unittest.decorators import tags
+
 from cloudcafe.compute.common.types import NovaServerStatusTypes \
     as ServerStates
+from cloudcafe.compute.composites import ComputeAdminComposite
 from cloudcafe.compute.common.clients.ping import PingClient
+from cloudcafe.compute.common.types import NovaServerRebootTypes
+from cloudcafe.compute.common.exceptions import Forbidden
 
-from cloudroast.compute.fixtures import ComputeAdminFixture
+from cloudroast.compute.fixtures import ServerFromImageFixture
 
-@unittest.skip("Failing due to RM7040")
-class PauseServerTests(ComputeAdminFixture):
 
-    @classmethod
-    def setUpClass(cls):
-        super(PauseServerTests, cls).setUpClass()
-        cls.server = cls.server_behaviors.create_active_server().entity
-        cls.resources.add(cls.server.id, cls.servers_client.delete_server)
+class PauseServerTests(object):
 
-        cls.ping_ip = cls.server.addresses.get_by_name(
-            cls.servers_config.network_for_ssh).ipv4
-        cls.verify_server_reachable(cls.ping_ip)
-
-    @tags(type='smoke', net='no')
+    @tags(type='smoke', net='yes')
     def test_pause_unpause_server(self):
         """Verify that a server can be paused and then unpaused successfully"""
+
+        self.ping_ip = self.parse_ip_address_for_communication(self.server)
+
+        self.admin_server_behaviors.wait_for_server_status(
+            self.server.id, ServerStates.ACTIVE)
 
         response = self.admin_servers_client.pause_server(self.server.id)
         self.assertEqual(response.status_code, 202)
@@ -62,3 +59,41 @@ class PauseServerTests(ComputeAdminFixture):
             self.server, self.servers_config),
             "Unable to connect to active server {0} after unpausing".format(
                 self.server.id))
+
+
+class NegativePauseServerTests(object):
+
+    @tags(type='smoke', net='yes')
+    def test_pause_reboot_server(self):
+        """Verify that a server reboot during pause does not restore it"""
+
+        self.ping_ip = self.parse_ip_address_for_communication(self.server)
+
+        response = self.admin_servers_client.pause_server(self.server.id)
+        self.assertEqual(response.status_code, 202)
+
+        self.admin_server_behaviors.wait_for_server_status(
+            self.server.id, ServerStates.PAUSED)
+
+        PingClient.ping_until_unreachable(
+            self.ping_ip, timeout=60, interval_time=5)
+
+        with self.assertRaises(Forbidden):
+            self.servers_client.reboot(self.server.id,
+                                       NovaServerRebootTypes.HARD)
+
+        PingClient.ping_until_unreachable(
+            self.ping_ip, timeout=60, interval_time=5)
+
+
+class ServerFromImagePauseTests(ServerFromImageFixture,
+                                PauseServerTests,
+                                NegativePauseServerTests):
+
+    @classmethod
+    def setUpClass(cls):
+        super(ServerFromImagePauseTests, cls).setUpClass()
+        cls.compute_admin = ComputeAdminComposite()
+        cls.admin_servers_client = cls.compute_admin.servers.client
+        cls.admin_server_behaviors = cls.compute_admin.servers.behaviors
+        cls.create_server()
