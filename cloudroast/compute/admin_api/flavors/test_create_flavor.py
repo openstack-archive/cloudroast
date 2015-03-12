d4b4a4af5743a94e65544ff30604f348afd8a741
Anna Eilering <anna.eilering@rackspace.com>
2015-03-12 09:38:07 -0500

    Added and amended Doc strings for compute/admin_api/flavors
    Added doc strings for test modules in compute/admin_api/flavors
    to allow programmatically generation of test documentation.
    
    - Changed test expectations from the negative("This test will fail
      if...") to the positive ("This test will succeed if..."
    - Removed class level docstrings for this patch as the best template
      for these is still under consideration.
    - Added single line summaries to comply with pep 257
    
    Change-Id: I3a53652883ec5a6f0efccb1cab0c6f3d68e2cdce

--- cloudroast/compute/admin_api/flavors/test_create_flavor.py
+++ cloudroast/compute/admin_api/flavors/test_create_flavor.py
@@ -1,5 +1,5 @@
 """
-Copyright 2013 Rackspace
+Copyright 2015 Rackspace
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -24,6 +24,13 @@ class CreateFlavorTest(ComputeAdminFixture):
 
     @classmethod
     def setUpClass(cls):
+        """
+        Perform actions that setup the neccesary resources for testing
+
+        The following resources are created during this setup:
+            - A public flavor with a name starting with 'flavor', 64MB of RAM,
+              1 vcpu, 10GB disk space
+        """
         super(CreateFlavorTest, cls).setUpClass()
         cls.flavor_name = rand_name('flavor')
         cls.flavor = cls.admin_flavors_client.create_flavor(
@@ -32,11 +39,26 @@ class CreateFlavorTest(ComputeAdminFixture):
 
     @classmethod
     def tearDownClass(cls):
+        """
+        Perform actions that allow for the cleanup of any generated resources
+
+        The following resources are deleted during this tear down:
+            - The public flavor created in the setup
+        """
         super(CreateFlavorTest, cls).tearDownClass()
         cls.admin_flavors_client.delete_flavor(cls.flavor.id)
 
     @tags(type='positive', net='no')
     def test_create_server_from_new_flavor(self):
+        """
+        Test that a newly created flavor can be used to create an instance
+
+        Create an instance using the flavor id created during Setup. Wait for
+        that instance to become active.
+        This test will be successful if:
+            - The instance successfully builds in less than configured timeout
+              time
+        """
         resp = self.server_behaviors.create_active_server(
             flavor_ref=self.flavor.id)
         server = resp.entity
@@ -44,6 +66,14 @@ class CreateFlavorTest(ComputeAdminFixture):
 
     @tags(type='negative', net='no')
     def test_create_flavor_with_duplicate_id(self):
+        """
+        Test that a new flavor cannot be created with an in use flavor id
+
+        Validate that you receive an 'Action in Progress' error when a user
+        attempts to create a flavor with a previously used ID value.
+        This test will be successful if:
+            - The create flavor request raises a 'Action in Progress' error
+        """
         with self.assertRaises(ActionInProgress):
             self.admin_flavors_client.create_flavor(
                 name=self.flavor_name, ram='64', vcpus='1', disk='10',
