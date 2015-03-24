7ec1066769c10cf9fd5ae4e987887b9d7d1ecd97
Anna Eilering <anna.eilering@rackspace.com>
2015-03-23 19:44:27 -0500

    Added and amended Doc strings for /compute/instance_actions/api
    Added the doc strings for the soft and hard reboot test modules
    and the delete server test modules in
    /compute/instance_actions/api.
    
    Change-Id: I3f1ba534298d42df74a06062aca2fdb0116e81ab

--- cloudroast/compute/instance_actions/api/test_reboot_server_hard.py
+++ cloudroast/compute/instance_actions/api/test_reboot_server_hard.py
@@ -1,5 +1,5 @@
 """
-Copyright 2013 Rackspace
+Copyright 2015 Rackspace
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -26,7 +26,22 @@ class RebootServerHardTests(object):
 
     @tags(type='smoke', net='yes')
     def test_reboot_server_hard(self):
-        """ The server should be power cycled """
+        """
+        A server should be able to successfully be hard rebooted
+
+        Get a remote instance client for the server identified during test set
+        up. Get the uptime value for the OS on the server. Request a hard
+        reboot of the server. Time the reboot. Once the reboot completes get a
+        new remote instance client for the server and use it to get the uptime
+        of the server after the hard reboot. Validate that the uptime of the
+        server post soft reboot is less than the uptime prior to the reboot
+        plus the time it took for the reboot to complete.
+
+        The following assertions will occur:
+            - The value of uptime after the hard reboot will be less that the
+              uptime of the uptime from before the reboot plus the amount of
+              time it takes for the reboot to occur.
+        """
         remote_instance = self.server_behaviors.get_remote_instance_client(
             self.server, config=self.servers_config, key=self.key.private_key)
         uptime_start = remote_instance.get_uptime()
@@ -46,6 +61,14 @@ class ServerFromImageRebootServerHardTests(ServerFromImageFixture,
 
     @classmethod
     def setUpClass(cls):
+        """
+        Perform actions that setup the neccesary resources for testing
+
+        The following resources are created during this setup:
+            - A keypair with a random name starting with 'key'
+            - A server using the created keypair and values from the test
+              configuration
+        """
         super(ServerFromImageRebootServerHardTests, cls).setUpClass()
         cls.key = cls.keypairs_client.create_keypair(rand_name("key")).entity
         cls.resources.add(cls.key.name,
