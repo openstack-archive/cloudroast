ece271a73f222c66b78702a8dc0bcb9e4d2f5efa
peterhowells <pete.howells@rackspace.com>
2015-03-28 16:23:56 -0400

    Documetation for testing migrating a server from the admin api version 2
    Change-Id: I3daab70db3f55df551d08e621e5f9eb7e5cadc9e

--- cloudroast/compute/integration/volumes/boot_from_volume/admin_api/v2/test_migrate_server.py
+++ cloudroast/compute/integration/volumes/boot_from_volume/admin_api/v2/test_migrate_server.py
@@ -1,5 +1,5 @@
 """
-Copyright 2013 Rackspace
+Copyright 2015 Rackspace
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -25,6 +25,12 @@ class ServerFromVolumeV2MigrateTests(ServerFromVolumeV2Fixture,
 
     @classmethod
     def setUpClass(cls):
+        """
+        Perform actions that setup the necessary resources for testing.
+
+        The following resources are created during this setup:
+            - Create an active server.
+        """
         super(ServerFromVolumeV2MigrateTests, cls).setUpClass()
         cls.compute_admin = ComputeAdminComposite()
         cls.admin_servers_client = cls.compute_admin.servers.client
