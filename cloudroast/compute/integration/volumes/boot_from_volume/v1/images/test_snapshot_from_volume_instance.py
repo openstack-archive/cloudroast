f7f7f6fad2b5c47d6d79626a7ec8b36d001c823a
peterhowells <pete.howells@rackspace.com>
2015-03-31 19:05:03 -0400

    Documetation for testing takeing a snapshot form a volume version 1.
    Change-Id: Iac979c83f1a76f92c7f19e622380df7987a5fde2

--- cloudroast/compute/integration/volumes/boot_from_volume/v1/images/test_snapshot_from_volume_instance.py
+++ cloudroast/compute/integration/volumes/boot_from_volume/v1/images/test_snapshot_from_volume_instance.py
@@ -1,5 +1,5 @@
 """
-Copyright 2013 Rackspace
+Copyright 2015 Rackspace
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -25,6 +25,12 @@ class ServerFromVolumeV1CreateImageTests(ServerFromVolumeV1Fixture):
 
     @classmethod
     def setUpClass(cls):
+        """
+        Perform actions that setup the necessary resources for testing.
+
+        The following resources are created during this setup:
+            - Creates an active server.
+        """
         super(ServerFromVolumeV1CreateImageTests, cls).setUpClass()
         cls.create_server()
         cls.image_name = rand_name('image')
@@ -33,7 +39,15 @@ class ServerFromVolumeV1CreateImageTests(ServerFromVolumeV1Fixture):
 
     @tags(type='smoke', net='no')
     def test_verify_create_glance_snapshot_is_disabled(self):
-        """Verify a user can not take glance snapshot on BFV instance"""
+        """
+        Verify a user can not take glance snapshot on BFV instance.
+
+        Will try to create an image from the server and is expecting a
+        "Forbidden" exception to be raised.
+
+        The following assertions occur:
+            - Expecting the Forbidden Exception to be raised.
+        """
         with self.assertRaises(Forbidden):
             self.servers_client.create_image(
                 self.server.id, self.image_name, metadata=self.metadata)
