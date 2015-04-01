50c5bfd7b832c9e6efee3bafc85ff97c5116429a
Anna Eilering <anna.eilering@rackspace.com>
2015-04-01 09:00:45 -0500

    Added and amended doc strings for remaining config drive tests
    Added doc strings for the second half of the config drive tests
    in /compute/api/config_drive to allow automated generation of
    test documentation.
    
    Change-Id: I738e9c4c5a9b70008de707a347de082e59ae8376

--- cloudroast/compute/api/config_drive/test_config_drive_rescue_server.py
+++ cloudroast/compute/api/config_drive/test_config_drive_rescue_server.py
@@ -1,5 +1,5 @@
 """
-Copyright 2013 Rackspace
+Copyright 2015 Rackspace
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -25,7 +25,53 @@ class ConfigDriveRescueTests(ComputeFixture):
 
     @classmethod
     def setUpClass(cls):
+        """
+        Perform actions that setup the necessary resources for testing
 
+        The following data is generated during this set up:
+            - A dictionary of metadata with the values:
+                {'meta_key_1': 'meta_value_1',
+                 'meta_key_2': 'meta_value_2'}
+            - A list of files containing a file with the path '/test.txt' and
+              the contents 'This is a config drive test file.'
+            - User data contents 'My user data'
+
+        The following resources are created during this set up:
+            - A keypair with a random name starting with 'key'
+            - A server with the following settings:
+                - The config_drive is set to True
+                - The keypair previously created
+                - Files to be injected at server creation including the
+                 '/test.txt' data previously generated
+                - The user data previously generated
+                - The metadata previously created
+                - Remaining values required for creating a server will come
+                  from test configuration.
+
+        The following actions are performed during this set up:
+            - A remote instant client is set up for the previously created
+              server
+            - The config drive is mounted at the base path set during test
+              configuration on the previously created server
+            - The details of the mounted config drive are retrieved and the
+              config drive user data, size of the config drive and the open
+              stack metadata prior to the server rebuild are recorded.
+            - The OpenStack metadata of the previously created server is
+              recorded prior to rebuild
+            - The previously created server is rescued and enters state 'RESCUE'
+            - The previously resued server is unrescued and enters state
+              'ACTIVE'
+            - A fresh remote instant client is set up for the rescued and
+              unrescued server
+            - The config drive is mounted at the base path set during test
+              configuration on the rebuilt server
+            - Using the remote instance client, it is determined whether the
+              directory '/openstack/content' is present at the base path to
+              mount set during test configuration
+            - The details of the mounted config drive are retrieved and the
+              config drive user data, size of the config drive and the open
+              stack metadata after the server rebuild are recorded.
+        """
         super(ConfigDriveRescueTests, cls).setUpClass()
 
         cls.metadata = {'meta_key_1': 'meta_value_1',
@@ -109,17 +155,65 @@ class ConfigDriveRescueTests(ComputeFixture):
         cls.openstack_meta_after_rescue = test_values[2]
 
     def test_unresecue_response(self):
+        """
+        Verify the reponse status code for the unrescue request is correct
+
+        Validate that the response status code for the unrescue request is
+        a 202.
+
+        The following assertions occur:
+            - The status code for the unrescue request from test set
+              up is equal to 202
+        """
         self.assertEqual(self.unrescue_response.status_code, 202)
 
     def test_directory_present_after_rescue(self):
-        # verify that the directory exists after the reboot and remount
+        """
+        Verify that the 'openstack' directory is present after a server rescue
+
+        Validate that the variable showing whether the directory of OpenStack
+        content is present after the server rescued and unrescued during
+        test set up is True.
+
+        The following assertions occur:
+            - The 'dir_openstack_content_present' variable is equal to True
+        """
         self.assertTrue(self.dir_openstack_content_present,
-                        msg="Directory Openstack is not present")
+                        msg="Directory openstack is not present")
 
     def test_openstack_metadata(self):
         message = "Expected {0} to be {1}, was {2}."
-        # Verify that the metadata before the rescue
-        # matches the metadata after the rescue
+        """
+        OpenStack metadata should remaining consistent through a server rescue
+
+        Get the OpenStack metadata of the server rescued and unrescued
+        during test setup. Validate that the metadata after unrescue matches the
+        metadata from before the rescue that was recorded during test set up.
+        Validate that the metadata contains select key value pairs.
+
+        The following assertions occur:
+            - The metadata recorded during test set up prior to the rescue is
+              equal to the metadata found on the server after the unrescue.
+            - The admin password value in the OpenStack metadata after unrescue
+              is equal to the admin password value in the OpenStack metadata
+              prior to rescue
+            - The availability_zone value in the OpenStack metadata is not None
+            - The hostname value in the OpenStack metadata is not None
+            - The launch index value in the OpenStack metadata is not None
+            - The server name value in the OpenStack metadata after unrescue is
+              equal to the server name value in the OpenStack metadata prior to
+              rescue
+            - The value of 'meta_key_1' in the OpenStack metadata is equal to
+              'meta_value_1'
+            - The value of 'meta_key_2' in the OpenStack metadata is equal to
+              'meta_value_2'
+            - The public key value in the OpenStack metadata after unrescue is
+              equal to the public key value in the OpenStack metadata prior to
+              rescue
+            - The uuid value in the OpenStack metadata after unrescue is
+              equal to the uuid value in the OpenStack metadata prior to
+              rescue
+        """
         self.assertEqual(
             self.openstack_meta_after_rescue,
             self.openstack_meta_before_rescue,
