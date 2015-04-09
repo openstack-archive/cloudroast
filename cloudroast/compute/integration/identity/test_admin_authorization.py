f45f71f19cf7d6beca2e22246ca39b0ec684f02b
peterhowells <pete.howells@rackspace.com>
2015-04-09 18:36:19 -0400

    Documetation for negative testing as a non-admin user.
    fixed some bad descriptions of calls being made.
    
    Change-Id: Ieb1fb5bd9f0e49b1b749605c599e96bb3388b3cd

--- cloudroast/compute/integration/identity/test_admin_authorization.py
+++ cloudroast/compute/integration/identity/test_admin_authorization.py
@@ -1,5 +1,5 @@
 """
-Copyright 2013 Rackspace
+Copyright 2015 Rackspace
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -24,78 +24,181 @@ class AdminAuthorizationTest(ComputeFixture):
 
     @classmethod
     def setUpClass(cls):
+        """
+        Perform actions that setup the necessary resources for testing.
+
+        The following resources are created during this setup:
+            - Creates an active server.
+        """
         super(AdminAuthorizationTest, cls).setUpClass()
         cls.server = cls.server_behaviors.create_active_server().entity
         cls.resources.add(cls.server.id, cls.servers_client.delete_server)
 
     @tags(type='negative', net='no')
     def test_lock_server_fails_as_user(self):
-        """A lock request should fail when not made by an admin"""
+        """
+        A lock request should fail when not made by an admin.
+
+        Will call lock server expecting a "Forbidden" exception to be raised
+        because the call is being executed as a non-admin.
+
+        The following assertions occur:
+            - Expecting a Forbidden exception to be raised.
+        """
         with self.assertRaises(Forbidden):
             self.servers_client.lock_server(self.server.id)
 
     @tags(type='negative', net='no')
     def test_unlock_server_fails_as_user(self):
-        """An unlock request should fail when not made by an admin"""
+        """
+        An unlock request should fail when not made by an admin.
+
+        Will call unlock server expecting a "Forbidden" exception to be raised
+        because the call is being executed as a non-admin.
+
+        The following assertions occur:
+            - Expecting a Forbidden exception to be raised.
+        """
         with self.assertRaises(Forbidden):
             self.servers_client.unlock_server(self.server.id)
 
     @tags(type='negative', net='no')
     def test_migrate_server_fails_as_user(self):
-        """A migrate request should fail when not made by an admin"""
+        """
+        A migrate request should fail when not made by an admin.
+
+        Will call migrate server expecting a "Forbidden" exception to be
+        raised because the call is being executed as a non-admin.
+
+        The following assertions occur:
+            - Expecting a Forbidden exception to be raised.
+        """
         with self.assertRaises(Forbidden):
             self.servers_client.migrate_server(self.server.id)
 
     @tags(type='negative', net='no')
     def test_live_migrate_server_fails_as_user(self):
-        """A live migrate request should fail when not made by an admin"""
+        """
+        A live migrate request should fail when not made by an admin.
+
+        Will call live migrate server expecting a "Forbidden" exception to be
+        raised because the call is being executed as a non-admin.
+
+        The following assertions occur:
+            - Expecting a Forbidden exception to be raised.
+        """
         with self.assertRaises(Forbidden):
             self.servers_client.live_migrate_server(self.server.id)
 
     @tags(type='negative', net='no')
     def test_stop_server_fails_as_user(self):
-        """A stop request should fail when not made by an admin"""
+        """
+        A stop request should fail when not made by an admin.
+
+        Will call stop server expecting a "Forbidden" exception to be
+        raised because the call is being executed as a non-admin.
+
+        The following assertions occur:
+            - Expecting a Forbidden exception to be raised.
+        """
         with self.assertRaises(BadRequest):
             self.servers_client.stop_server(self.server.id)
 
     @tags(type='negative', net='no')
     def test_start_server_fails_as_user(self):
-        """A start request should fail when not made by an admin"""
+        """
+        A start request should fail when not made by an admin.
+
+        Will call start server expecting a "Forbidden" exception to be
+        raised because the call is being executed as a non-admin.
+
+        The following assertions occur:
+            - Expecting a Forbidden exception to be raised.
+        """
         with self.assertRaises(BadRequest):
             self.servers_client.start_server(self.server.id)
 
     @tags(type='negative', net='no')
     def test_suspend_server_fails_as_user(self):
-        """A suspend request should fail when not made by an admin"""
+        """
+        A suspend request should fail when not made by an admin.
+
+        Will call suspend server expecting a "Forbidden" exception to be
+        raised because the call is being executed as a non-admin.
+
+        The following assertions occur:
+            - Expecting a Forbidden exception to be raised.
+        """
         with self.assertRaises(Forbidden):
             self.servers_client.suspend_server(self.server.id)
 
     @tags(type='negative', net='no')
     def test_resume_server_fails_as_user(self):
-        """A resume request should fail when not made by an admin"""
+        """
+        A resume request should fail when not made by an admin.
+
+        Will call resume server expecting a "Forbidden" exception to be
+        raised because the call is being executed as a non-admin.
+
+        The following assertions occur:
+            - Expecting a Forbidden exception to be raised.
+        """
         with self.assertRaises(Forbidden):
             self.servers_client.resume_server(self.server.id)
 
     @tags(type='negative', net='no')
     def test_pause_server_fails_as_user(self):
-        """A pause request should fail when not made by an admin"""
+        """
+        A pause request should fail when not made by an admin.
+
+        Will call pause server expecting a "Forbidden" exception to be
+        raised because the call is being executed as a non-admin.
+
+        The following assertions occur:
+            - Expecting a Forbidden exception to be raised.
+        """
         with self.assertRaises(Forbidden):
             self.servers_client.pause_server(self.server.id)
 
     @tags(type='negative', net='no')
     def test_unpause_server_fails_as_user(self):
-        """An unpause request should fail when not made by an admin"""
+        """
+        An unpause request should fail when not made by an admin.
+
+        Will call unpause server expecting a "Forbidden" exception to be
+        raised because the call is being executed as a non-admin.
+
+        The following assertions occur:
+            - Expecting a Forbidden exception to be raised.
+        """
         with self.assertRaises(Forbidden):
             self.servers_client.unpause_server(self.server.id)
 
     @tags(type='negative', net='no')
     def test_reset_server_state_fails_as_user(self):
-        """A reset state request should fail when not made by an admin"""
+        """
+        A reset server state request should fail when not made by an admin.
+
+        Will call reset server sate expecting a "Forbidden" exception to be
+        raised because the call is being executed as a non-admin.
+
+        The following assertions occur:
+            - Expecting a Forbidden exception to be raised.
+        """
         with self.assertRaises(Forbidden):
             self.servers_client.reset_state(self.server.id)
 
     @tags(type='negative', net='no')
     def test_create_flavor_fails_as_user(self):
+        """
+        A create flavor request should fail when not made by an admin.
+
+        Will call create flavor expecting a "Forbidden" exception to be
+        raised because the call is being executed as a non-admin.
+
+        The following assertions occur:
+            - Expecting a Forbidden exception to be raised.
+        """
         with self.assertRaises(ItemNotFound):
             self.flavors_client.create_flavor(
                 name='test555', ram='128', vcpus='1', disk='10', id='100',
@@ -103,5 +206,14 @@ class AdminAuthorizationTest(ComputeFixture):
 
     @tags(type='negative', net='no')
     def test_delete_flavor_fails_as_user(self):
+        """
+        A delete flavor request should fail when not made by an admin.
+
+        Will call delete flavor expecting a "Forbidden" exception to be
+        raised because the call is being executed as a non-admin.
+
+        The following assertions occur:
+            - Expecting a Forbidden exception to be raised.
+        """
         with self.assertRaises(ItemNotFound):
             self.flavors_client.delete_flavor(self.flavor_ref)
