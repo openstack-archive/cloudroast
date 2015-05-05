0d58974558fb3686ef96bef52d4b74f8abe7cd8a
Luke Wollney <luke.wollney@rackspace.com>
2015-05-05 15:02:12 -0500

    Update image sharing operations tests continued
    *Updated image_sharing_test
    -Added test_shared_deactivated_image_accessibility
    -Added test_shared_reactivated_image_accessibility
    *Updated list_image_members_test
    -Added test_list_image_members_using_deactivated_image
    -Added test_list_image_members_using_reactivated_image
    *Updated update_image_member_test
    -Added test_update_image_member_using_deactivated_image
    -Added test_update_image_member_using_reactivated_image
    
    Change-Id: I055bc43199438fa518cb8412fdd35ce63e179d1d

--- cloudroast/glance/regression/image_sharing_operations/update_image_member_test.py
+++ cloudroast/glance/regression/image_sharing_operations/update_image_member_test.py
@@ -34,7 +34,7 @@ class UpdateImageMember(ImagesFixture):
         # Count set to number of images required for this module
         created_images = cls.images.behaviors.create_images_via_task(
             image_properties={'name': rand_name('update_image_member')},
-            count=2)
+            count=4)
 
         cls.shared_image = created_images.pop()
         resp = cls.images.client.create_image_member(
@@ -45,6 +45,19 @@ class UpdateImageMember(ImagesFixture):
         resp = cls.images.client.create_image_member(
             cls.alt_shared_image.id_, cls.member_id)
         cls.alt_created_member = resp.entity
+
+        cls.deactivated_image = created_images.pop()
+        resp = cls.images.client.create_image_member(
+            cls.deactivated_image.id_, cls.member_id)
+        cls.deactivated_member = resp.entity
+        cls.images_admin.client.deactivate_image(cls.deactivated_image.id_)
+
+        cls.reactivated_image = created_images.pop()
+        resp = cls.images.client.create_image_member(
+            cls.reactivated_image.id_, cls.member_id)
+        cls.reactivated_member = resp.entity
+        cls.images_admin.client.deactivate_image(cls.reactivated_image.id_)
+        cls.images_admin.client.reactivate_image(cls.reactivated_image.id_)
 
     @classmethod
     def tearDownClass(cls):
@@ -97,6 +110,58 @@ class UpdateImageMember(ImagesFixture):
             msg=('Unexpected error received for image {0}. '
                  'Expected: No errors '
                  'Received: {1}').format(self.shared_image.id_, errors))
+
+    def test_update_image_member_using_deactivated_image(self):
+        """
+        @summary: Update image member using deactivated image
+
+        1) Update image member as a member of the image
+        2) Verify that the response code is 200
+        3) Verify that the returned image member's properties are as expected
+        generically
+        """
+
+        resp = self.images_alt_one.client.update_image_member(
+            self.deactivated_image.id_, self.member_id,
+            ImageMemberStatus.ACCEPTED)
+        self.assertEqual(
+            resp.status_code, 200,
+            Messages.STATUS_CODE_MSG.format(200, resp.status_code))
+        updated_member = resp.entity
+
+        errors = self.images.behaviors.validate_image_member(updated_member)
+
+        self.assertEqual(
+            errors, [],
+            msg=('Unexpected error received for image {0}. '
+                 'Expected: No errors '
+                 'Received: {1}').format(self.deactivated_image.id_, errors))
+
+    def test_update_image_member_using_reactivated_image(self):
+        """
+        @summary: Update image member using reactivated image
+
+        1) Update image member as a member of the image
+        2) Verify that the response code is 200
+        3) Verify that the returned image member's properties are as expected
+        generically
+        """
+
+        resp = self.images_alt_one.client.update_image_member(
+            self.reactivated_image.id_, self.member_id,
+            ImageMemberStatus.ACCEPTED)
+        self.assertEqual(
+            resp.status_code, 200,
+            Messages.STATUS_CODE_MSG.format(200, resp.status_code))
+        updated_member = resp.entity
+
+        errors = self.images.behaviors.validate_image_member(updated_member)
+
+        self.assertEqual(
+            errors, [],
+            msg=('Unexpected error received for image {0}. '
+                 'Expected: No errors '
+                 'Received: {1}').format(self.reactivated_image.id_, errors))
 
     def test_update_image_member_as_image_owner_forbidden(self):
         """
@@ -182,66 +247,63 @@ class UpdateImageMember(ImagesFixture):
             resp.status_code, 404,
             Messages.STATUS_CODE_MSG.format(404, resp.status_code))
 
-    @unittest.skip('Redmine bug #11535')
+    @unittest.skip('Launchpad bug #1442320')
     def test_update_image_member_using_blank_member_id(self):
         """
         @summary: Update image member using blank member id
 
         1) Update image member using blank member id
-        2) Verify that the response code is 404
+        2) Verify that the response code is 400
         """
 
         resp = self.images_alt_one.client.update_image_member(
-            image_id=self.shared_image, member_id='',
+            image_id=self.shared_image.id_, member_id='',
             status=ImageMemberStatus.ACCEPTED)
         self.assertEqual(
-            resp.status_code, 404,
-            Messages.STATUS_CODE_MSG.format(404, resp.status_code))
+            resp.status_code, 400,
+            Messages.STATUS_CODE_MSG.format(400, resp.status_code))
 
-    @unittest.skip('Redmine bug #11535')
     def test_update_image_member_using_invalid_member_id(self):
         """
         @summary: Update image member using invalid member id
 
         1) Update image member using invalid member id
-        2) Verify that the response code is 404
+        2) Verify that the response code is 403
         """
 
         resp = self.images_alt_one.client.update_image_member(
-            image_id=self.shared_image, member_id='invalid',
+            image_id=self.shared_image.id_, member_id='invalid',
             status=ImageMemberStatus.ACCEPTED)
         self.assertEqual(
-            resp.status_code, 404,
-            Messages.STATUS_CODE_MSG.format(404, resp.status_code))
+            resp.status_code, 403,
+            Messages.STATUS_CODE_MSG.format(403, resp.status_code))
 
-    @unittest.skip('Redmine bug #11535')
     def test_update_image_member_using_blank_status(self):
         """
         @summary: Update image member using blank status
 
         1) Update image member using blank status
-        2) Verify that the response code is 404
+        2) Verify that the response code is 400
         """
 
         resp = self.images_alt_one.client.update_image_member(
-            image_id=self.shared_image, member_id=self.member_id,
+            image_id=self.shared_image.id_, member_id=self.member_id,
             status='')
         self.assertEqual(
-            resp.status_code, 404,
-            Messages.STATUS_CODE_MSG.format(404, resp.status_code))
+            resp.status_code, 400,
+            Messages.STATUS_CODE_MSG.format(400, resp.status_code))
 
-    @unittest.skip('Redmine bug #11535')
     def test_update_image_member_using_invalid_status(self):
         """
         @summary: Update image member using invalid status
 
         1) Update image member using invalid status
-        2) Verify that the response code is 404
+        2) Verify that the response code is 400
         """
 
         resp = self.images_alt_one.client.update_image_member(
-            image_id=self.shared_image, member_id=self.member_id,
+            image_id=self.shared_image.id_, member_id=self.member_id,
             status='invalid')
         self.assertEqual(
-            resp.status_code, 404,
-            Messages.STATUS_CODE_MSG.format(404, resp.status_code))
+            resp.status_code, 400,
+            Messages.STATUS_CODE_MSG.format(400, resp.status_code))
