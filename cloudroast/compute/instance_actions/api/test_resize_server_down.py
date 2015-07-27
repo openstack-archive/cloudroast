83cbc8e6d364915f11da5ad133c4f3f8b809e18b
ivo5307 <ivo.vasev@RACKSPACE.COM>
2015-07-27 13:13:26 -0500

    Resize down added option and renaming resize_enabled to resize up
    * adds skipping of tests in case resize down is not supported for desired flavor or deployment
    * renaming resize_enabled for resize_up_enabled, because those tests are using resize up
    
    Change-Id: I0e43bcaf4e7a4573d0f9fe08740def38d3a3739c

--- cloudroast/compute/instance_actions/api/test_resize_server_down.py
+++ cloudroast/compute/instance_actions/api/test_resize_server_down.py
@@ -28,10 +28,10 @@ compute_config = ComputeConfig()
 hypervisor = compute_config.hypervisor.lower()
 
 flavors_config = FlavorsConfig()
-resize_enabled = flavors_config.resize_enabled
+resize_down_enabled = flavors_config.resize_down_enabled
 
 can_resize = (
-    resize_enabled
+    resize_down_enabled
     and hypervisor not in [ComputeHypervisors.IRONIC,
                            ComputeHypervisors.LXC_LIBVIRT])
 
