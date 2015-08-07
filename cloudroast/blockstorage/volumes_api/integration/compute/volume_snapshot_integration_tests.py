a10f358795e686840ecde7ccec724b60e48791f3
Jose Idar <jose.idar@rackspace.com>
2015-08-07 09:28:28 -0500

    Adding more checks to common attach test logic   * Moving attach logic to common function in fixture   * Porting functionality into snapshot integration test
    Change-Id: Ia3bf0c723644cd4108425aaa03d2a4723031c2f5

--- cloudroast/blockstorage/volumes_api/integration/compute/volume_snapshot_integration_tests.py
+++ cloudroast/blockstorage/volumes_api/integration/compute/volume_snapshot_integration_tests.py
@@ -13,6 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import time
+
 from cloudroast.blockstorage.volumes_api.integration.compute.fixtures \
     import ComputeIntegrationTestFixture
 
@@ -33,7 +35,62 @@ class VolumeSnapshotIntegrationSmokeTests(ComputeIntegrationTestFixture):
         cls.test_attachment = cls.attach_volume_and_get_device_info(
             cls.server_conn, cls.test_server.id, cls.test_volume.id_)
 
+    @classmethod
+    def attach_volume_and_get_device_info(
+            cls, server_connection, server_id, volume_id):
+
+        # Get list of devices with original volume attached
+        original_devices = server_connection.get_all_disk_details()
+        cls.fixture_log.debug(
+            "Pre-attach devices: {}".format(original_devices))
+
+        attachment = \
+            cls.volume_attachments.behaviors.attach_volume_to_server(
+                server_id, volume_id)
+
+        assert attachment, "Could not attach volume {0} to server {1}".format(
+            volume_id, server_id)
+
+        # Wait for device to show up on VM.  Currently this is expected to
+        # be fairly instantaneous; the wait was added to help catch rare
+        # erroneous behavior.
+        volume_details = None
+        current_devices = None
+        cls.fixture_log.debug("Waiting for device to show up on server")
+        end = time.time() + 120
+        while end > time.time():
+            current_devices = server_connection.get_all_disk_details()
+            new_devices = [
+                d for d in current_devices if d not in original_devices]
+            if len(new_devices) > 0:
+                print new_devices
+                volume_details = new_devices
+                break
+            else:
+                cls.fixture_log.debug(
+                    "New device hasn't shown up yet, waiting 5 seconds")
+                time.sleep(5)
+        else:
+            cls.fixture_log.debug(
+                "Visible devices: {}".format(current_devices))
+            raise Exception("Could not verify attached volume assigned device")
+
+        cls.fixture_log.debug("Device found on server")
+        cls.fixture_log.debug("Visible devices: {}".format(current_devices))
+        setattr(attachment, 'os_disk_details', volume_details)
+
+        os_disk_device_name = \
+            volume_details[0].get('Number') or "/dev/{0}".format(
+                volume_details[0].get('name'))
+
+        assert os_disk_device_name, (
+            "Could not get a unique device name from the OS")
+        setattr(attachment, 'os_disk_device_name', os_disk_device_name)
+
+        return attachment
+
     def test_restore_snapshot_of_written_volume_and_verify_data(self):
+
         # Format Volume
         self.format_attached_volume(
             self.server_conn, self.test_attachment.os_disk_device_name)
@@ -64,7 +121,7 @@ class VolumeSnapshotIntegrationSmokeTests(ComputeIntegrationTestFixture):
 
         # Snapshot unattached used volume
         self.test_snapshot = self.new_snapshot(
-            self.test_volume.id_, add_cleanup=False)
+            self.test_volume.id_, add_cleanup=True)
         self.addCleanup(
             self.volumes.behaviors.delete_snapshot_confirmed,
             self.test_snapshot.id_)
@@ -72,8 +129,10 @@ class VolumeSnapshotIntegrationSmokeTests(ComputeIntegrationTestFixture):
         # Restore snapshot to new volume
         self.restored_snapshot_volume = \
             self.volumes.behaviors.create_available_volume(
-                self.test_volume.size, self.test_volume.volume_type,
-                self.random_snapshot_name(), source_volid=self.test_volume.id_)
+                self.test_volume.size,
+                self.test_volume.volume_type,
+                self.random_snapshot_name(),
+                snapshot_id=self.test_snapshot.id_)
         self.addCleanup(
             self.volumes.behaviors.delete_volume_confirmed,
             self.restored_snapshot_volume.id_)
