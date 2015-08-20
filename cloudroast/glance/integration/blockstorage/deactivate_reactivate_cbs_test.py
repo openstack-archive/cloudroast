667146a9a693473dd4477d808062850ecb9d44d2
Luke Wollney <luke.wollney@rackspace.com>
2015-08-20 14:48:56 -0500

    Update Glance CBS integration tests
    *Updated test doc strings for consistency
    
    Change-Id: I88105f3f7ac929558659b1c7c7c1fdafa2d4b49f

--- cloudroast/glance/integration/blockstorage/deactivate_reactivate_cbs_test.py
+++ cloudroast/glance/integration/blockstorage/deactivate_reactivate_cbs_test.py
@@ -16,7 +16,10 @@ limitations under the License.
 
 from unittest.suite import TestSuite
 
+from cloudcafe.blockstorage.volumes_api.common.models.statuses import (
+    Volume as VolumeStatuses)
 from cloudcafe.compute.composites import ComputeIntegrationComposite
+from cloudcafe.glance.common.constants import Messages
 
 from cloudroast.glance.fixtures import ImagesIntegrationFixture
 
@@ -24,9 +27,9 @@ from cloudroast.glance.fixtures import ImagesIntegrationFixture
 def load_tests(loader, standard_tests, pattern):
     suite = TestSuite()
     suite.addTest(DeactivateReactivateCBS(
-        'test_create_volume_from_deactivated_image_invalid'))
+        "test_create_volume_from_deactivated_image_invalid"))
     suite.addTest(DeactivateReactivateCBS(
-        'test_create_volume_from_reactivated_image'))
+        "test_create_volume_from_reactivated_image"))
     return suite
 
 
@@ -35,7 +38,7 @@ class DeactivateReactivateCBS(ImagesIntegrationFixture):
     @classmethod
     def setUpClass(cls):
         """
-        Perform actions that setup the necessary resources for testing.
+        Perform actions that setup the necessary resources for testing
 
         The following resources are created during this setup:
             - A server with defaults defined in server behaviors
@@ -44,6 +47,7 @@ class DeactivateReactivateCBS(ImagesIntegrationFixture):
         The following data is generated during this set up:
             - Get compute integration composite
         """
+
         super(DeactivateReactivateCBS, cls).setUpClass()
         cls.server = (
             cls.compute.servers.behaviors.create_active_server().entity)
@@ -57,32 +61,76 @@ class DeactivateReactivateCBS(ImagesIntegrationFixture):
         cls.volume_size = int(cls.volumes.config.min_volume_from_image_size)
 
     def test_create_volume_from_deactivated_image_invalid(self):
-        """Volume should not be created from deactivated image"""
+        """
+        Verify that a volume cannot be created from a deactivated image
+
+        Attempt to create a volume using a deactivated image
+
+        This test will be successful if:
+            - The response code received for deactivate image is a 204
+            - The response code received for create volume is 200
+            - The response code received for get volume is a 200
+            - The volume status is error
+        """
+
         # Deactivate Image
         self.resp = self.images_admin.client.deactivate_image(self.image.id)
         self.assertEqual(204, self.resp.status_code)
-        # Trying to create bootable volume
-        resp = self.volumes.behaviors.create_available_volume(
+        # Attempt to create bootable volume
+        resp = self.volumes.client.create_volume(
             size=self.volume_size,
             volume_type=self.volumes.config.default_volume_type,
             image_ref=self.image.id)
-        if resp.ok:
-            self.volumes.behaviors.delete_volume_confirmed(
-                resp.entity.id_,
-                size=self.volume_size)
-            self.fail('The create volume request should fail with disabled'
-                      ' image, but it received response code in 2xx range')
-        self.assertEqual(400, resp.status_code)
+        self.assertEqual(
+            resp.status_code, 200,
+            Messages.STATUS_CODE_MSG.format(200, resp.status_code))
+        created_volume = resp.entity
+        self.verify_volume_build_has_errored(
+            created_volume.id_, self.volumes.config.volume_create_max_timeout)
+        # Verify volume status is error
+        resp = self.volumes.client.get_volume_info(created_volume.id_)
+        self.assertEqual(
+            resp.status_code, 200,
+            Messages.STATUS_CODE_MSG.format(200, resp.status_code))
+        get_volume = resp.entity
+        self.assertEqual(
+            get_volume.status, VolumeStatuses.ERROR,
+            msg=('Unexpected status for volume {0}. '
+                 'Expected: {1} Received: '
+                 '{2}').format(created_volume.id_,
+                               VolumeStatuses.ERROR, get_volume.status))
 
     def test_create_volume_from_reactivated_image(self):
-        """Volume should be created from reactivated image"""
+        """
+        Verify that a volume can be created from a reactivated image
+
+        Create a volume using a reactivated image
+
+        This test will be successful if:
+            - The response code received for reactivate image is a 204
+            - The response code received for get volume is 200
+            - The volume status is available
+        """
+
         # Reactivate Image
         resp = self.images_admin.client.reactivate_image(self.image.id)
         self.assertEqual(204, resp.status_code)
-
-        resp = self.volumes.behaviors.create_available_volume(
+        # Create bootable volume
+        created_volume = self.volumes.behaviors.create_available_volume(
             size=self.volume_size,
             volume_type=self.volumes.config.default_volume_type,
             image_ref=self.image.id)
         self.resources.add(
-            resp.id_, self.volumes.client.delete_volume)
+            created_volume.id_, self.volumes.client.delete_volume)
+        # Verify volume status is available
+        resp = self.volumes.client.get_volume_info(created_volume.id_)
+        self.assertEqual(
+            resp.status_code, 200,
+            Messages.STATUS_CODE_MSG.format(200, resp.status_code))
+        get_volume = resp.entity
+        self.assertEqual(
+            get_volume.status, VolumeStatuses.AVAILABLE,
+            msg=('Unexpected status for volume {0}. '
+                 'Expected: {1} Received: '
+                 '{2}').format(created_volume.id_,
+                               VolumeStatuses.AVAILABLE, get_volume.status))
