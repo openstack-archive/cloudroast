dc7713dba755ca072e1481753bf3c76ccfabaa8b
Luke Wollney <luke.wollney@rackspace.com>
2015-08-20 13:55:28 -0500

    Update Glance integration naming conventions
    *Updated class and test naming conventions to be
    consistent with the other tests in the Glance
    package
    
    Change-Id: I00c76b3cc6dc41b5ef5535b5e58d99b363ae765e

--- cloudroast/glance/integration/compute/servers/deactivate_reactivate_servers_test.py
+++ cloudroast/glance/integration/compute/servers/deactivate_reactivate_servers_test.py
@@ -23,18 +23,18 @@ from cloudroast.glance.fixtures import ImagesIntegrationFixture
 
 def load_tests(loader, standard_tests, pattern):
     suite = TestSuite()
-    suite.addTest(ServerIntegration(
-        "test_deactivated_image"))
-    suite.addTest(ServerIntegration(
-        "test_can_create_server_from_deactivated_image"))
-    suite.addTest(ServerIntegration(
-        "test_reactivate_image"))
-    suite.addTest(ServerIntegration(
-        "test_can_create_server_from_reactivated_image"))
+    suite.addTest(DeactivateReactivateServers(
+        'test_deactivate_snapshot_image'))
+    suite.addTest(DeactivateReactivateServers(
+        'test_create_server_from_deactivated_image_invalid'))
+    suite.addTest(DeactivateReactivateServers(
+        'test_reactivate_snapshot_image'))
+    suite.addTest(DeactivateReactivateServers(
+        'test_create_server_from_reactivated_image'))
     return suite
 
 
-class ServerIntegration(ImagesIntegrationFixture):
+class DeactivateReactivateServers(ImagesIntegrationFixture):
 
     @classmethod
     def setUpClass(cls):
@@ -45,7 +45,7 @@ class ServerIntegration(ImagesIntegrationFixture):
             - A server with defaults defined in server behaviors
             - An image from the newly created server
         """
-        super(ServerIntegration, cls).setUpClass()
+        super(DeactivateReactivateServers, cls).setUpClass()
         cls.server = (
             cls.compute.servers.behaviors.create_active_server().entity)
         cls.image = cls.compute.images.behaviors.create_active_image(
@@ -54,7 +54,7 @@ class ServerIntegration(ImagesIntegrationFixture):
             cls.server.id, cls.compute.servers.client.delete_server)
         cls.resources.add(cls.image.id, cls.images_client.delete_image)
 
-    def test_deactivated_image(self):
+    def test_deactivate_snapshot_image(self):
         """
         Verify that a V1 image can be deactivated
 
@@ -66,7 +66,7 @@ class ServerIntegration(ImagesIntegrationFixture):
         self.resp = self.images_admin.client.deactivate_image(self.image.id)
         self.assertEqual(204, self.resp.status_code)
 
-    def test_create_server_from_deactivated_image(self):
+    def test_create_server_from_deactivated_image_invalid(self):
         """
         Verify that a instance can not be build from deactivated image
 
@@ -81,7 +81,7 @@ class ServerIntegration(ImagesIntegrationFixture):
             image_ref=self.image.id)
         self.assertEqual(400, resp.status_code)
 
-    def test_reactivate_image(self):
+    def test_reactivate_snapshot_image(self):
         """
         Verify that a V1 image can be reactivated
 
