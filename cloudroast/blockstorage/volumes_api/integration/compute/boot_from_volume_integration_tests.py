2cc04520b8d737562ad936437dede02ef100c17f
Jose Idar <jose.idar@rackspace.com>
2015-09-02 13:21:30 -0500

    Added server check in new bootable volume test
      * Added a mew test to check that server booted from a
        bootable volume is reachable and that the OS disk is
        writable.
      * Moved BFV datasets to new datasets.py
      * Fixed os_type bug in fixtures.py (sometimes os_type metadata
        is unavailable, so now it can be inspected and assigned
        separately.
    
    Change-Id: Ia307ff69a7301215b2d1b1a2b95785788b780460

--- cloudroast/blockstorage/volumes_api/integration/compute/boot_from_volume_integration_tests.py
+++ cloudroast/blockstorage/volumes_api/integration/compute/boot_from_volume_integration_tests.py
@@ -15,82 +15,74 @@ limitations under the License.
 """
 from cafe.drivers.unittest.decorators import (
     data_driven_test, DataDrivenFixture)
-from cloudcafe.blockstorage.datasets import (
-    BlockstorageDatasets, ComputeIntegrationDatasets)
 from cloudcafe.common.tools.datagen import random_string
+from cloudcafe.blockstorage.datasets import BlockstorageDatasets
 
 from cloudroast.blockstorage.volumes_api.integration.compute.fixtures \
     import ComputeIntegrationTestFixture
-
-
-class test_datasets(object):
-    # Create tagged Image dataset
-    images = ComputeIntegrationDatasets.images()
-    single = ComputeIntegrationDatasets.images(
-        max_datasets=1, randomize=True)
-    configured = ComputeIntegrationDatasets.configured_images()
-    images.apply_test_tags("bfv-exhaustive")
-    configured.apply_test_tags("bfv-configured")
-    single.apply_test_tags("bfv-single-random")
-    images.merge_dataset_tags(configured, single)
-
-    # Create tagged Images by VolumeType dataset
-    images_by_volume = \
-        ComputeIntegrationDatasets.images_by_volume_type()
-    single = ComputeIntegrationDatasets.images_by_volume_type(
-        max_datasets=1, randomize=True)
-    configured = ComputeIntegrationDatasets.configured_images_by_volume_type()
-    images_by_volume.apply_test_tags("bfv-exhaustive")
-    configured.apply_test_tags("bfv-configured")
-    single.apply_test_tags("bfv-single-random")
-    images_by_volume.merge_dataset_tags(configured, single)
-
-    # Create tagged Images by Flavor dataset
-    images_by_flavor = \
-        ComputeIntegrationDatasets.images_by_flavor()
-    single = ComputeIntegrationDatasets.images_by_flavor(
-        max_datasets=1, randomize=True)
-    configured = ComputeIntegrationDatasets.configured_images_by_flavor()
-    images_by_flavor.apply_test_tags("bfv-exhaustive")
-    configured.apply_test_tags("bfv-configured")
-    single.apply_test_tags("bfv-single-random")
-    images_by_flavor.merge_dataset_tags(configured, single)
+from cloudroast.blockstorage.volumes_api.integration.compute.datasets \
+    import bfv_datasets
 
 
 @DataDrivenFixture
 class BootFromVolumeIntegrationTests(ComputeIntegrationTestFixture):
 
-    @data_driven_test(test_datasets.images)
+    @data_driven_test(bfv_datasets.images)
     def ddtest_non_asserting_min_disk_check_for_image(self, image):
         """Check if the image has min disk attribute set, print a
         message if it doesn't.  This test will only fail on Error"""
         self.check_if_minimum_disk_size_is_set(image)
 
-    @data_driven_test(test_datasets.images)
+    @data_driven_test(bfv_datasets.images)
     def ddtest_min_disk_is_set_for_image(self, image):
         """Verify that image has min disk attribute set"""
         self.assertMinDiskSizeIsSet(image)
 
-    @data_driven_test(test_datasets.images_by_volume)
+    @data_driven_test(bfv_datasets.images_by_volume)
     def ddtest_create_basic_bootable_volume_from(self, volume_type, image):
         """Create a single volume_type volume from image"""
         self.create_volume_from_image_test(volume_type, image)
 
-    @data_driven_test(test_datasets.images_by_flavor)
+    @data_driven_test(bfv_datasets.images_by_flavor)
     def ddtest_create_bootable_volume_from_a_snapshot_of_a_server(
             self, image, flavor,
             volume_type=BlockstorageDatasets.default_volume_type_model()):
         self.create_bootable_volume_from_server_snapshot(
             image, flavor, volume_type)
 
-    @data_driven_test(test_datasets.images_by_flavor)
+    @data_driven_test(bfv_datasets.images_by_flavor)
     def ddtest_create_bootable_volume_from_last_of_3_snapshots_of_a_server(
             self, image, flavor,
             volume_type=BlockstorageDatasets.default_volume_type_model()):
         self.create_bootable_volume_from_third_snapshot_of_server_test(
             image, flavor, volume_type)
 
-    @data_driven_test(test_datasets.images_by_flavor)
+    @data_driven_test(bfv_datasets.flavors_by_images_by_volume_type)
+    def ddtest_boot_a_server_from_a_volume(
+            self, image, flavor, volume_type):
+
+        os_type = self.get_image_os_type(image.id)
+
+        # Create a bootable volume
+        volume = self.create_volume_from_image_test(volume_type, image)
+
+        # Create block device mapping
+        bdm = self.compute.servers.behaviors.create_block_device_mapping_v1(
+            volume.id_, True, 'vda', volume.size, volume_type.id_)
+
+        # Boot a server from the volume
+        server = self.servers.behaviors.create_active_server(
+            name=self.random_server_name(), flavor_ref=flavor.id,
+            block_device_mapping=bdm).entity
+
+        # Connect to server
+        self.server_conn = self.connect_to_instance(server, os_type=os_type)
+
+        # Write data to the root disk
+        resp = self.server_conn.create_large_file(multiplier=0.1)
+        self.assertTrue(resp, "Unable to write data to bootable OS volume")
+
+    @data_driven_test(bfv_datasets.images_by_flavor)
     def ddtest_verify_data_on_custom_snapshot_after_copy_to_volume(
             self, image, flavor,
             volume_type=BlockstorageDatasets.default_volume_type_model()):
