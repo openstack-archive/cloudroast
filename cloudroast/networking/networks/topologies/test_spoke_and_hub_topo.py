37ef766b67c7709b26ccaf65258fbeb6eb3d6315
Christopher Hunt <chris.hunt@RACKSPACE.COM>
2016-05-17 09:53:19 -0500

    Initial import of basic spoke-and-hub test
    * Simple test to verify connectivity
    
    Change-Id: I6410153df1add73499279e68a5023cad81e82113

---
+++ cloudroast/networking/networks/topologies/test_spoke_and_hub_topo.py
@@ -0,0 +1,31 @@
+import time
+
+from cafe.drivers.unittest.decorators import tags
+from cloudroast.networking.networks.topologies.spoke_and_hub_fixture \
+    import SpokeAndHubFixture
+
+
+class TestSpokeAndHubTopology(SpokeAndHubFixture):
+    NUM_OF_SPOKES = 3
+    DEBUG = False
+
+    @tags('spoke_and_hub', 'topology', 'connectivity')
+    def test_basic_spoke_and_hub_connectivity(self):
+        """ Basic topology connectivity verification. """
+
+        # The validation of the topology is actually done as part of the
+        # topology setup, but this will allow a basic connectivity test in
+        # the setup to occur. If there is an issue with connectivity, the setup
+        # will error report the issues.
+        pass
+
+    def debug_topology_routine(self):
+        """
+        For debugging only. If the class DEBUG flag is set, and connectivity
+        fails, this routine will be invoked by the setup routine.
+
+        """
+        delay_minutes = 15
+        print ("Connectivity Issues detected!\n You have {delay} minutes to "
+               "debug. GO!".format(delay=delay_minutes))
+        time.sleep(delay_minutes * 60)
